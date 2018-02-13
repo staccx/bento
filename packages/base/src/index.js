511ca62d67946e146eb55618079877bdda740ff5
Marius Hauken <marius.hauken@gmail.com>
2018-02-13 12:51:46 +0100

    Moved theme outside of components

--- packages/base/src/index.js
+++ packages/base/src/index.js
@@ -5,5 +5,5 @@
 import Input from "./components/Input/Input"
 import { default as Button, ButtonUnstyled } from "./components/Button/Button"
 import { default as extend } from "./functions/extend"
-import { default as cssResets } from './components/Theme'
+import { default as cssResets } from './Theme/cssResets'
 export { Input, Button, ButtonUnstyled, extend, cssResets }
--- packages/base/styleguide.config.js
+++ packages/base/styleguide.config.js
@@ -1,7 +1,7 @@
 const path = require("path")
 const { createConfig, babel } = require("webpack-blocks")
 
-const wrapperPath = path.join(__dirname, "src/components/Theme/ThemeWrapper.js")
+const wrapperPath = path.join(__dirname, "src/Theme/ThemeWrapper.js")
 console.log(wrapperPath)
 module.exports = {
   webpackConfig: createConfig([babel()]),
