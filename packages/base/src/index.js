12e62ef612b6e0adcab2d40c9c21c63fb583b8b3
robinsandborg <robin@netliferesearch.com>
2018-02-13 13:56:18 +0100

    Add resets to export

--- packages/base/src/index.js
+++ packages/base/src/index.js
@@ -7,10 +7,11 @@ import SplitList from "./components/Lists/SplitList/SplitList"
 import SplitListItem from "./components/Lists/SplitList/SplitList.Item"
 import { default as Button, ButtonUnstyled } from "./components/Button/Button"
 import { default as extend } from "./functions/extend"
-import { default as cssResets } from './Theme/cssResets'
+import { default as cssResets } from "./Theme/cssResets"
 import { default as ThemeProvider } from "./components/Theme/ThemeWrapper"
 
 export {
+  cssResets,
   SplitList,
   SplitListItem,
   Input,
