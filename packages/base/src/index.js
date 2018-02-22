0f6d1a8f8ebf9846269b1fa730c19698a89346cf
Espen Th Sæverud <espensaeverud@gmail.com>
2018-02-22 20:54:04 +0100

    chore(CompanyInput): ADded to export

--- packages/base/src/index.js
+++ packages/base/src/index.js
@@ -5,6 +5,7 @@
 import Input from "./components/Forms/Input/Input"
 import CurrencyInput from "./components/Forms/Input/CurrencyInput/CurrencyInput"
 import PhoneInput from "./components/Forms/Input/PhoneInput/PhoneInput"
+import CompanyInput from "./components/Forms/Input/CompanyInput/CompanyInput"
 import Label from "./components/Forms/Label/Label"
 import CheckBox from "./components/Forms/CheckBox/CheckBox"
 import RadioButton from "./components/Forms/RadioButton/RadioButton"
@@ -47,6 +48,7 @@ export {
   Button,
   CheckBox,
   CheckGroup,
+  CompanyInput,
   cssResets,
   CurrencyInput,
   ExpandListItem,
