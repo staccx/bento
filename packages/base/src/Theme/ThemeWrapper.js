4742dff5f145b378ea847184dc3a565bcd029264
robinsandborg <robin@netliferesearch.com>
2018-03-01 13:16:47 +0100

    Merge themes

--- packages/base/src/Theme/ThemeWrapper.js
+++ packages/base/src/Theme/ThemeWrapper.js
@@ -1,16 +1,19 @@
 import React, { Component } from "react"
 import { ThemeProvider } from "styled-components"
 import PropTypes from "prop-types"
-import theme from "./baseTheme"
+import baseTheme from "./baseTheme"
 import cssReset from "../Styles/cssResets"
 
 export default class ThemeWrapper extends Component {
   render() {
     cssReset()
+    const theme = Object.assign({}, baseTheme, this.props.theme)
+
     return <ThemeProvider theme={theme}>{this.props.children}</ThemeProvider>
   }
 }
 
 ThemeWrapper.propTypes = {
-  children: PropTypes.object.isRequired
+  children: PropTypes.object.isRequired,
+  theme: PropTypes.object
 }
