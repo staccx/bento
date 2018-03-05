98902f179fe285ea7b08a3ba1408955b7c9583f4
Marius Hauken <marius.hauken@gmail.com>
2018-03-05 11:29:25 +0100

    Position-adjustment portfolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,21 +1,14 @@
-import React, {Component} from 'react';
-import {inject, observer} from 'mobx-react'
-import styled from 'styled-components'
-import PieChart from "./PieChart";
-import PortfolioExpand from "./PortfolioExpand";
+import React, { Component } from "react"
+import { inject, observer } from "mobx-react"
+import styled from "styled-components"
+import PieChart from "./PieChart"
+import PortfolioExpand from "./PortfolioExpand"
 
 const explodeAmount = 20
 
-const PortfolioWrapper = styled.div`
-  display: flex;
-  > canvas {
-    max-width: 50%;
-  }
-`
-
-@inject("apiStore", "uiStore") @observer
+@inject("apiStore", "uiStore")
+@observer
 class Portfolio extends Component {
-
   componentDidMount() {
     this.pieChart = this.pieChartInjector.wrappedInstance
     this.updatePie(this.props.uiStore.selectedInstrument, 1000)
@@ -42,20 +35,45 @@ class Portfolio extends Component {
   }
 
   render() {
-    const {selectedInstrument, setInstrument} = this.props.uiStore
+    const { selectedInstrument, setInstrument } = this.props.uiStore
     return (
       <PortfolioWrapper>
-        <PieChart ref={node => this.pieChartInjector = node} padding={30} explodeAmount={explodeAmount}/>
+        <PieChart
+          ref={node => (this.pieChartInjector = node)}
+          padding={30}
+          explodeAmount={explodeAmount}
+        />
         <PortfolioExpand
           funds={this.props.apiStore.recommendedPortfolio}
           selectedIndex={selectedInstrument}
           onClick={setInstrument}
         />
       </PortfolioWrapper>
-    );
+    )
   }
 }
 
-Portfolio.propTypes = {};
+Portfolio.propTypes = {}
 
-export default Portfolio;
+const PortfolioWrapper = styled.div`
+  padding-top: ${p => p.theme.spacing.huge};
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: ${p => p.theme.spacing.large};
+  grid-template-areas: "left right";
+  @media (max-width: 735px) {
+    grid-gap: ${p => p.theme.spacing.medium};
+  }
+  @media (max-width: 710px) {
+    grid-template-columns: 100%;
+    grid-template-areas: "one";
+  }
+
+  > canvas {
+    margin-top: ${p => p.theme.spacing.medium};
+    width: 100% !important;
+    height: auto !important;
+  }
+`
+
+export default Portfolio
