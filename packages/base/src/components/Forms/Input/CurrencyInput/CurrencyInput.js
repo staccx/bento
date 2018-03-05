1601a7934d2aeafc2c5c9a902b35a2e7f48222aa
Espen Th Sæverud <espensaeverud@gmail.com>
2018-03-05 18:41:35 +0100

    fix: CurrencyInput now fixed

--- packages/base/src/components/Forms/Input/CurrencyInput/CurrencyInput.js
+++ packages/base/src/components/Forms/Input/CurrencyInput/CurrencyInput.js
@@ -6,19 +6,18 @@ import {
 } from "../../../../functions/Currency/currency"
 
 const CurrencyInput = ({ ...props }) => {
-  const createMask = createCurrencyMask({ ...props })
+  const mask = createCurrencyMask({ ...props })
   Object.keys(currencyMaskOptions).forEach(key => {
     delete props[key]
   })
   delete props["mask"]
-  const value = props.value ? props.value.toString() : '0'
-  const mask = createMask(value)
   return <Input type="number" mask={mask} {...props} />
 }
 
 CurrencyInput.propTypes = InputPropTypes
 CurrencyInput.defaultProps = {
-  ...InputDefaultProps
+  ...InputDefaultProps,
+  value: 0
 }
 
 export default CurrencyInput
