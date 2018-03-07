7d47c69c191d2684e3f508e37269a2561f197823
Marius Hauken <marius.hauken@gmail.com>
2018-03-07 11:08:19 +0100

    risk-wave take 1

--- src/Questions/Risk.js
+++ src/Questions/Risk.js
@@ -1,15 +1,14 @@
 import React from "react"
-import TestQuestion from "../components/TestQuestion"
+import styled, { css } from "styled-components"
 
 const content = {
-  title:
-    "What do you think when you see newspaper pages where the stock markets plummet?",
+  title: "What do you do if there is a strong market decline?",
+  lead:
+    "Funds and stocks are affected by fluctuations in the market globally and nationally. A strong market decline is a fall of more than 20%, which has happened seven times on Oslo Stock Exchange over the past 20 years. In the two most extreme cases, the IT bubble and the financial crisis, Oslo Børs had a decline of over 40%.",
   answers: [
     {
       id: "fsdfds2",
-      heading: "I get scared",
-      body:
-        "When I read such headlines, it affects night sleep. I want to sell my investments",
+      heading: "I sell everything",
       value: "very-low"
     },
     {
@@ -33,7 +32,97 @@ const content = {
     }
   ]
 }
+class Risk extends React.Component {
+  render() {
+    const waves = 100
+    const waveArray = [...Array(waves)]
+    return (
+      <Space>
+        <LabelWrapper>
+          {content.answers.map(e => <Label key={e.id}>{e.heading}</Label>)}
+        </LabelWrapper>
+        <WaveWrapper>
+          {waveArray.map((e, index) => (
+            <WaveHover href="#" index={index}>
+              <WaveElement />
+            </WaveHover>
+          ))}
+        </WaveWrapper>
+      </Space>
+    )
+  }
+}
 
-const Risk = () => <TestQuestion content={content} />
+const Space = styled.div`
+  margin-top: 48px;
+  margin-bottom: 48px;
+`
+const Label = styled.label`
+  font-weight: bold;
+  color: #2f80ed;
+`
+
+const LabelWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: ${p => p.theme.spacing.medium};
+`
+
+const WaveWrapper = styled.div`
+  display: flex;
+  justify-content: stretch;
+`
+
+const WaveThing = css`
+  width: 3px;
+  background-color: #2f80ed;
+  height: 32px;
+  border-radius: 1px;
+  display: block;
+`
+
+const WaveElement = styled.i`
+  ${WaveThing};
+  transition: transform 0.4s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+`
+
+const WavePseudo = css`
+  content: "";
+  position: absolute;
+  left: -100%;
+  top: 0;
+  z-index: -1;
+  transition: 0.6s transform cubic-bezier(0.175, 0.885, 0.32, 1.275);
+`
+
+const WaveHover = styled.a`
+  position: relative;
+  display: block;
+  flex-grow: 1;
+  cursor: pointer;
+
+  &::before {
+    ${WaveThing};
+    ${WavePseudo};
+    left: -100%;
+  }
+  &::after {
+    ${WaveThing};
+    ${WavePseudo};
+    left: 100%;
+  }
+  &:hover,
+  &:active,
+  &:focus {
+    outline: none;
+    ${WaveElement} {
+      transform: scaleY(1.8);
+    }
+    &:before,
+    &:after {
+      transform: scaleY(1.3);
+    }
+  }
+`
 
 export default Risk
