a146b6573ccfdc6bff424f2228f16a5e06e5d944
Marius Hauken <marius.hauken@gmail.com>
2018-03-07 12:34:29 +0100

    Opacity on slideIn-animations

--- packages/base/src/Styles/animations.js
+++ packages/base/src/Styles/animations.js
@@ -25,20 +25,24 @@ export const SlideInFromTop = keyframes`
 
 export const SlideInFromLeft = keyframes`
   from {
+    opacity: 0;
     transform: translateX(-100%);
   }
 
   to {
+    opacity: 1;
     transform: translateX(0);
   }
 `
 
 export const SlideInFromRight = keyframes`
   from {
+    opacity: 0;
     transform: translateX(100%);
   }
 
   to {
+    opacity: 1;
     transform: translateX(0);
   }
 `
