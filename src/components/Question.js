6cebe0d9c0803a960bb25b5bd835b416a4137281
robinsandborg <robin@netliferesearch.com>
2018-03-12 15:23:10 +0100

    Bouncing dot

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,35 +1,95 @@
-import React from "react"
-import styled from "styled-components"
+import React, { Component } from "react"
+import styled, { keyframes } from "styled-components"
 import { rgba } from "polished"
 import { CheckGroup, RadioButton, Wrapper } from "@staccx/base"
 import QuestionLead from "./QuestionLead"
 
-const Question = ({ content, onChange, selected }) => (
-  <Wrapper size="medium">
-    <QuestionLead question={content.title}>{content.lead}</QuestionLead>
-    <CheckGroup group={"experiencealternatives"} onChange={() => null}>
-      {content.answers.map(answer => (
-        <AnswerBox
-          selected={answer.value === selected}
-          key={answer.id}
-          htmlFor={answer.id}
-        >
-          <Image>{answer.icon}</Image>
-          <AdvisorRadio
-            id={answer.id}
-            value={answer.value}
-            selected={answer.value === selected}
-            name={"experiencealternatives"}
-            onChange={() => onChange(answer.value)}
-          >
-            <Heading>{answer.heading}</Heading>
-            <span>{answer.body}</span>
-          </AdvisorRadio>
-        </AnswerBox>
-      ))}
-    </CheckGroup>
-  </Wrapper>
-)
+class Question extends Component {
+  constructor(props, context) {
+    super(props, context)
+    this.state = {
+      selectedRow: null
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange = (index, value) => {
+    this.setState({
+      selectedRow: index
+    })
+    this.props.onChange(value)
+  }
+
+  render() {
+    const { content, selected } = this.props
+    const { selectedRow } = this.state
+
+    return (
+      <Wrapper size="medium">
+        <QuestionLead question={content.title}>{content.lead}</QuestionLead>
+        <Inner totalRows={content.answers.length} currentRow={selectedRow}>
+          {content.answers.map((answer, index) => (
+            <AnswerBox
+              selected={answer.value === selected}
+              key={answer.id}
+              htmlFor={answer.id}
+            >
+              <Image>{answer.icon}</Image>
+              <AdvisorRadio
+                id={answer.id}
+                value={answer.value}
+                selected={answer.value === selected}
+                name={"experiencealternatives"}
+                onChange={() => this.handleChange(index, answer.value)}
+                group={"experiencealternatives"}
+              >
+                <Heading>{answer.heading}</Heading>
+                <span>{answer.body}</span>
+              </AdvisorRadio>
+            </AnswerBox>
+          ))}
+        </Inner>
+      </Wrapper>
+    )
+  }
+}
+
+const dot0 = keyframes`30%, 70% { transform:scale(0.5) translate(-52px, -50%); }`
+const dot1 = keyframes`29%, 70% { transform:scale(0.4999) translate(-52px, -50%); }`
+const dot2 = keyframes`31%, 70% { transform:scale(0.5001) translate(-52px, -50%); }`
+const dot3 = keyframes`28%, 70% { transform:scale(0.4998) translate(-52px, -50%); }`
+const dot4 = keyframes`32%, 70% { transform:scale(0.5002) translate(-52px, -50%); }`
+
+const Inner = styled.div`
+  position: relative;
+  display: grid;
+  grid-auto-flow: row;
+  grid-auto-rows: 150px;
+
+  &:after {
+    content: "";
+    opacity: ${p => (p.currentRow !== null ? 1 : 0)};
+    position: absolute;
+    top: calc(${p => p.currentRow / p.totalRows * 100 + "%"} + 75px);
+    right: 0;
+    display: block;
+    width: 16px;
+    height: 16px;
+    border-radius: 50%;
+    background: rebeccapurple;
+    transition: top 0.3s ease-out;
+    transform: translate(-52px, -50%);
+    animation-name: ${p =>
+      p.currentRow === 0
+        ? dot0
+        : p.currentRow === 1
+          ? dot1
+          : p.currentRow === 2 ? dot2 : p.currentRow === 3 ? dot3 : dot4};
+    animation-timing-function: ease-in-out;
+    animation-duration: 0.3s;
+    animation-fill-mode: forwards;
+  }
+`
 
 const Heading = styled.h4`
   color: ${p => p.theme.color.primary};
@@ -42,7 +102,6 @@ const AnswerBox = styled.label`
   align-items: center;
   position: relative;
   border-radius: ${p => p.theme.borderRadius};
-  margin-bottom: ${p => p.theme.spacing.large};
   padding: 1px;
   transition: box-shadow 0.2s ease;
 
@@ -92,7 +151,7 @@ const AdvisorRadio = styled(RadioButton)`
 
   > input:checked ~ label {
     &::after {
-      transform: translate(-4px, -50%) scale(1);
+      transform: translate(-4px, -50%) scale(0);
     }
   }
 
