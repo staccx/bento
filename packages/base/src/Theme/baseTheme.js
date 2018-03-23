d074826aff0a94db93541c72377a72885c438b88
robinsandborg <robin.sandborg@gmail.com>
2018-03-23 14:56:33 +0100

    New base theme

--- packages/base/src/Theme/baseTheme.js
+++ packages/base/src/Theme/baseTheme.js
@@ -1,59 +1,122 @@
-export const theme = {
-  name: "StaccXBase",
-  spacing: {
-    huge: "96px",
-    large: "48px",
-    mediumPlus: "32px",
-    medium: "24px",
-    small: "12px",
-    tiny: "6px",
-    micro: "3px"
-  },
-  wrapper: {
-    small: "420px",
-    medium: "640px",
-    large: "1024px",
-    breakout: "640px"
-  },
-  color: {
-    white: "#fff",
-    bg: "#fff",
-    subtleHover: "#F7F7FF",
-    bgGray: "#f7f7ff",
-    line: "#EBEBF2",
-    gray: "#767676",
-    wcag: "#777",
-    disabled: "#BABABA",
-    primary: "#00a190",
-    secondary: "#F5A623",
-    text: "#2b2b2b",
-    negative: "#ea526f",
-    positive: "#4A90E2",
-    warning: "#c54059",
-    black: "#000",
-    green: "#00a190" // for toggle eks
-  },
-  font: {
-    size: {
-      h1: "36px",
-      h2: "26px",
-      h3: "22px",
-      h4: "18px",
-      h5: "16px",
-      h6: "14px",
-      base: "14px",
-      tiny: "12px",
-      input: "16px",
-      huge: "44px"
-    },
-    heading: "sans-serif",
-    body: "sans-serif"
-  },
-  targetSize: {
-    small: "36px",
-    normal: "44px"
-  },
-  borderRadius: "2px"
-}
+import Styles from "../index"
 
-export default theme
+export const theme = {
+  name: "FunduTheme",
+  globals: {
+    spacing: {
+      huge: "96px",
+      large: "48px",
+      mediumPlus: "32px",
+      medium: "24px",
+      small: "12px",
+      tiny: "6px",
+      micro: "3px"
+    },
+    wrapper: {
+      small: "420px",
+      medium: "640px",
+      large: "1024px",
+      breakout: "640px"
+    },
+    color: {
+      white: "#fff",
+      bg: "#fff",
+      subtleHover: "#F7F7FF",
+      bgGray: "#f7f7ff",
+      line: "#EBEBF2",
+      gray: "#767676",
+      wcag: "#777",
+      disabled: "#BABABA",
+      primary: "#00a190",
+      secondary: "#F5A623",
+      text: "#2b2b2b",
+      negative: "#ea526f",
+      positive: "#4A90E2",
+      warning: "#c54059",
+      black: "#000",
+      green: "#00a190" // for toggle eks
+    },
+    font: {
+      size: {
+        h1: "36px",
+        h2: "26px",
+        h3: "22px",
+        h4: "18px",
+        h5: "16px",
+        h6: "14px",
+        base: "14px",
+        tiny: "12px",
+        input: "16px",
+        huge: "44px"
+      },
+      heading: "sans-serif",
+      body: "sans-serif"
+    },
+    targetSize: {
+      small: "36px",
+      normal: "44px"
+    },
+    borderRadius: "2px"
+  },
+  Button: {
+    base: {
+      style: Styles.button
+    }
+  },
+  Slider: {
+    base: {
+      style: Styles.slider
+    }
+  },
+  SliderKeyboardInput: {
+    base: {},
+    SliderWrapper: {
+      base: {
+        style: Styles.sliderKeyboardInput.sliderWrapper
+      }
+    },
+    HiddenLabel: {
+      base: {
+        style: Styles.sliderKeyboardInput.hiddenLabel
+      }
+    },
+    SliderInput: {
+      base: {
+        style: Styles.sliderKeyboardInput.sliderInput
+      }
+    }
+  },
+  Odometer: {
+    base: {},
+    OdometerAnimating: {
+      base: {
+        style: Styles.odometer.odometerAnimating
+      }
+    },
+    OdometerStatic: {
+      base: {
+        style: Styles.odometer.odometerStatic
+      }
+    },
+    OdometerContainer: {
+      base: {
+        style: Styles.odometer.odometerContainer
+      }
+    },
+    OdometerWrapper: {
+      base: {
+        style: Styles.odometer.odometerWrapper
+      }
+    },
+    DigitWrapper: {
+      base: {
+        style: Styles.odometer.digitWrapper
+      }
+    },
+    DigitContainer: {
+      base: {
+        style: Styles.odometer.digitContainer
+      }
+    }
+  }
+}
