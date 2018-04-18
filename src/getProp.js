c1b4f3a22f269151be240a768678b1f1deba456c
Espen Th Sæverud <espensaeverud@gmail.com>
2018-04-18 13:13:47 +0200

    feat: Added function 'wrapper' to fetch theme prop wrapper. Removed logging

--- src/getProp.js
+++ src/getProp.js
@@ -1,15 +1,9 @@
 import deepfind from "./deepfind";
 
 export default (prop, field) => ({ theme }) => {
-
-  if(prop === "color") {
-    console.log(prop, field)
-  }
   if (!deepfind(theme, [prop, field].join("."))) {
     console.warn("Could not find ", prop, field);
     return null;
   }
-  const value = theme[prop][field]
-  console.log(value)
-  return value
+  return theme[prop][field];
 };
