f2c14d01ed1f97b5b78425b6803702d7ce3551eb
Espen Th Sæverud <espensaeverud@gmail.com>
2018-04-23 13:38:46 +0200

    feat: added convenience for color and font

--- src/color.js
+++ src/color.js
@@ -1,3 +1,22 @@
 import getProp from "./getProp";
 
-export default color => getProp("color", color);
+const color = c => getProp("color", c);
+
+color.white = () => color("white")
+color.bg = () => color("bg")
+color.subtleHover = () => color("subtleHover")
+color.bgGray = () => color("bgGray")
+color.line = () => color("line")
+color.gray = () => color("gray")
+color.wcag = () => color("wcag")
+color.disabled = () => color("disabled")
+color.primary = () => color("primary")
+color.secondary = () => color("secondary")
+color.text = () => color("text")
+color.negative = () => color("negative")
+color.positive = () => color("positive")
+color.warning = () => color("warning")
+color.black = () => color("black")
+color.green = () => color("green")
+
+export default color
--- src/font.js
+++ src/font.js
@@ -1,6 +1,6 @@
 import deepfind from "./deepfind";
 
-export default (name, type = "size") => ({ theme }) => {
+const font = (name, type = "size") => ({ theme }) => {
   const deep = ["font", type, name].join(".");
   if (!deepfind(theme, deep)) {
     console.warn("Could not find font", deep, theme);
@@ -8,3 +8,16 @@ export default (name, type = "size") => ({ theme }) => {
   }
   return theme.font[type][name];
 };
+
+font.h1 = () => font("h1")
+font.h2 = () => font("h2")
+font.h3 = () => font("h3")
+font.h4 = () => font("h4")
+font.h5 = () => font("h5")
+font.h6 = () => font("h6")
+font.base = () => font("base")
+font.tiny = () => font("tiny")
+font.input = () => font("input")
+font.huge = () => font("huge")
+
+export default font
