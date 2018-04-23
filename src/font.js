a06ed767730b07c87083d496531cf658badd95fb
Espen Th Sæverud <espensaeverud@gmail.com>
2018-04-23 13:46:05 +0200

    feat: Convenience methods for wrapper

--- src/font.js
+++ src/font.js
@@ -9,15 +9,15 @@ const font = (name, type = "size") => ({ theme }) => {
   return theme.font[type][name];
 };
 
-font.h1 = () => font("h1")
-font.h2 = () => font("h2")
-font.h3 = () => font("h3")
-font.h4 = () => font("h4")
-font.h5 = () => font("h5")
-font.h6 = () => font("h6")
-font.base = () => font("base")
-font.tiny = () => font("tiny")
-font.input = () => font("input")
-font.huge = () => font("huge")
+font.h1 = () => font("h1");
+font.h2 = () => font("h2");
+font.h3 = () => font("h3");
+font.h4 = () => font("h4");
+font.h5 = () => font("h5");
+font.h6 = () => font("h6");
+font.base = () => font("base");
+font.tiny = () => font("tiny");
+font.input = () => font("input");
+font.huge = () => font("huge");
 
-export default font
+export default font;
--- src/wrapper.js
+++ src/wrapper.js
@@ -1,3 +1,10 @@
 import getProp from "./getProp";
 
-export default size => getProp("wrapper", size);
+const wrapper = size => getProp("wrapper", size);
+
+wrapper.small = () => wrapper("small")
+wrapper.medium = () => wrapper("medium")
+wrapper.large = () => wrapper("large")
+wrapper.breakout = () => wrapper("breakout")
+
+export default wrapper
