947495604125540cea462bf501a5fe96e271eb32
Espen Th Sæverud <espensaeverud@gmail.com>
2018-04-24 11:32:20 +0200

    feat: Select is now themable

--- packages/base/src/components/Forms/Select/Select.js
+++ packages/base/src/components/Forms/Select/Select.js
@@ -1,37 +1,35 @@
-import React from "react"
-import PropTypes from "prop-types"
-import styled from "styled-components"
-import Downshift from "downshift"
-import { multiplyPixelValue } from "@staccx/math"
-import Label from "../Label/Label"
-import Input from "../Input/Input"
-import Caret from "../../Icons/Caret"
-import Close from "../../Icons/Close"
-import {
-  targetSize,
-  spacing,
-  borderRadius,
-  font,
-  color,
-  themify
-} from "@staccx/theme"
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import Downshift from 'downshift'
+import { multiplyPixelValue } from '@staccx/math'
+import {ScaleIn} from "@staccx/animations"
+import Label from '../Label/Label'
+import Input from '../Input/Input'
+import Caret from '../../Icons/Caret'
+import Close from '../../Icons/Close'
+import { color, spacing, targetSize, ThemeComponent, themify } from '@staccx/theme'
 
+export const SELECT_DEFAULT_OPTION_ELEMENT_WRAPPER = 'SELECT_DEFAULT_OPTION_ELEMENT_WRAPPER'
 const DefaultOptionElementWrapper = styled.div`
   border: 1px solid ${color.line};
-  border-top-width: 0;
+  border-top-width: 0;,
+  ${themify(SELECT_DEFAULT_OPTION_ELEMENT_WRAPPER)}
 `
 
+export const SELECT_WRAPPER = 'SELECT_WRAPPER'
 const SelectedWrapper = styled.div`
   position: relative;
+  ${themify(SELECT_WRAPPER)}
 `
-
+export const SELECT_ICON_BUTTON = 'SELECT_ICON_BUTTON'
 const IconButton = styled.button`
   position: absolute;
   right: ${spacing.small()};
   bottom: ${p => {
-    const value = targetSize.normal()(p)
-    return multiplyPixelValue(value, 0.5)
-  }};
+  const value = targetSize.normal()(p)
+  return multiplyPixelValue(value, 0.5)
+}};
   width: ${targetSize.small};
   height: ${targetSize.small};
   border: 0;
@@ -51,24 +49,41 @@ const IconButton = styled.button`
       fill: ${color.primary};
     }
   }
+    ${themify(SELECT_ICON_BUTTON)}
+`
+
+export const COMPONENT_SELECT_CARET_ICON = 'COMPONENT_SELECT_CARET_ICON'
+const CaretComp = ({...props}) => (
+  <ThemeComponent tagName={COMPONENT_SELECT_CARET_ICON} fallback={Caret} {...props} />
+)
+const CaretIcon = styled(CaretComp)`
+  transition: transform 0.3s ease-out;
+  transform: ${p => (p.isExpanded ? 'rotate(180deg)' : 'rotate(0)')};
+`
+
+export const COMPONENT_SELECT_CLOSE_ICON = 'COMPONENT_SELECT_CLOSE_ICON'
+const CloseComp = ({...props}) => (
+  <ThemeComponent tagName={COMPONENT_SELECT_CLOSE_ICON} fallback={Close} {...props} />
+)
+const CloseIcon = styled(CloseComp)`
+  transform: scale(0);
+  animation: ${ScaleIn} 0.3s ease-in forwards 1;
 `
 
 const DefaultPlaceholderElement = Input
 
 const Select = ({
-  children,
-  onChange,
-  label,
-  renderSelectedElement,
-  selectedElement,
-  renderOptionsWrapperElement,
-  optionsWrapperElement,
-  renderPlaceHolderElement,
-  placeHolderElement,
-  placeHolderLabel,
-  iconCaret,
-  iconClose
-}) => {
+                  children,
+                  onChange,
+                  label,
+                  renderSelectedElement,
+                  selectedElement,
+                  renderOptionsWrapperElement,
+                  optionsWrapperElement,
+                  renderPlaceHolderElement,
+                  placeHolderElement,
+                  placeHolderLabel,
+                }) => {
   const Selected = renderSelectedElement || selectedElement
   const OptionsWrapper =
     renderOptionsWrapperElement ||
@@ -76,24 +91,22 @@ const Select = ({
     DefaultOptionElementWrapper
   const Placeholder =
     renderPlaceHolderElement || placeHolderElement || DefaultPlaceholderElement
-  const CaretIcon = iconCaret || Caret
-  const CloseIcon = iconClose || Close
 
   return (
     <Downshift
       onChange={onChange}
-      itemToString={item => (item ? item.text : "")}
+      itemToString={item => (item ? item.text : '')}
       render={({
-        getInputProps,
-        getButtonProps,
-        getItemProps,
-        isOpen,
-        inputValue,
-        selectedItem,
-        highlightedIndex,
-        toggleMenu,
-        clearSelection
-      }) => (
+                 getInputProps,
+                 getButtonProps,
+                 getItemProps,
+                 isOpen,
+                 inputValue,
+                 selectedItem,
+                 highlightedIndex,
+                 toggleMenu,
+                 clearSelection
+               }) => (
         <div>
           <Label>{label}</Label>
           {selectedItem ? (
@@ -101,24 +114,24 @@ const Select = ({
               <Selected
                 onClick={() => toggleMenu()}
                 selectedItem={selectedItem}
-                buttonProps={{ ...getButtonProps() }}
-                inputProps={{ ...getInputProps() }}
+                buttonProps={{...getButtonProps()}}
+                inputProps={{...getInputProps()}}
                 toggleMenu={toggleMenu}
               />
               <IconButton onClick={() => clearSelection()}>
-                <CloseIcon />
+                <CloseIcon/>
               </IconButton>
             </SelectedWrapper>
           ) : (
             <SelectedWrapper>
               <Placeholder
                 {...getInputProps({
-                  placeholder: placeHolderLabel || ""
+                  placeholder: placeHolderLabel || ''
                 })}
                 onClick={() => toggleMenu()}
               />
               <IconButton onClick={() => toggleMenu()}>
-                <CaretIcon />
+                <CaretIcon isExpanded={isOpen}/>
               </IconButton>
             </SelectedWrapper>
           )}
@@ -127,7 +140,7 @@ const Select = ({
               {children.map((child, index) => {
                 const item = child.props.data
                 return React.cloneElement(child, {
-                  ...getItemProps({ item }),
+                  ...getItemProps({item}),
                   ...child.props,
                   highlighted: highlightedIndex === index,
                   selected: selectedItem === child
@@ -165,7 +178,10 @@ Select.propTypes = {
   placeHolderLabel: PropTypes.string,
   renderPlaceHolderElement: PropTypes.func,
   placeHolderElement: PropTypes.func,
-  iconCaret: PropTypes.element,
-  iconClose: PropTypes.element
+  _themeVars: PropTypes.oneOf([SELECT_DEFAULT_OPTION_ELEMENT_WRAPPER,
+    SELECT_ICON_BUTTON,
+    SELECT_WRAPPER,
+    COMPONENT_SELECT_CARET_ICON,
+    COMPONENT_SELECT_CLOSE_ICON])
 }
 export default Select
