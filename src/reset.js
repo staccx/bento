23398895f81008e78d51fa21a6efee9291d13640
Marius Hauken <marius.hauken@gmail.com>
2018-04-30 12:31:28 +0200

    Hack for reset.css

--- src/reset.js
+++ src/reset.js
@@ -1,1 +1,9 @@
-export default ``
+export default `
+  h1,h2,h3,h4,h5,h6 {
+    font-family: 'IBM Plex Sans';
+  }
+  html {
+    font-family: 'IBM Plex Sans';
+    background-color: #F8F8F8;
+  }
+`;
