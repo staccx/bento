c010b01a6ff244400d932c90880fc945a4af15dd
robinsandborg <robin.sandborg@gmail.com>
2018-04-30 13:22:41 +0200

    Add some basic styles to TransactionList

--- src/components/TransactionList/TransactionList.item.js
+++ src/components/TransactionList/TransactionList.item.js
@@ -1,18 +1,48 @@
 import React from "react"
 import PropTypes from "prop-types"
 import styled from "styled-components"
+import { spacing, color } from "@staccx/theme"
+import { formatCurrency } from "@staccx/formatting"
 
 const TransactionListItem = ({ heading, date, amount }) => (
   <Item>
-    {heading}
-    <div>{date}</div>
-    <div>{amount}</div>
+    <Icon type={amount > 0 ? "deposit" : "withdrawal"} />
+    <Body>
+      <h4>{heading}</h4>
+      <div>{date}</div>
+    </Body>
+    <Amount>{formatCurrency(amount)}</Amount>
   </Item>
 )
 
 const Item = styled.li`
-  padding: 0;
+  display: flex;
+  align-items: center;
+  padding: 0 ${spacing.small} ${spacing.small};
+  border-bottom: 1px solid ${color.line};
 `
+
+const Icon = styled.div`
+  width: 32px;
+  height: 32px;
+  margin-right: ${spacing.small};
+  background-color: ${props =>
+    props.type === "withdrawal" ? color.negative : color.positive};
+  border-radius: 50%;
+  text-align: center;
+  line-height: 32px;
+  color: white;
+
+  &:after {
+    content: "${props => (props.type === "withdrawal" ? "-" : "+")}";
+  }
+`
+
+const Body = styled.div`
+  flex-grow: 1;
+`
+
+const Amount = styled.div``
 
 TransactionListItem.propTypes = {
   children: PropTypes.arrayOf(PropTypes.element).isRequired
--- src/components/TransactionList/TransactionList.js
+++ src/components/TransactionList/TransactionList.js
@@ -1,11 +1,17 @@
 import React from "react"
 import PropTypes from "prop-types"
 import styled from "styled-components"
+import { spacing } from "@staccx/theme"
+import { Box } from "../Box"
 
 const TransactionList = ({ children }) => <List>{children}</List>
 
-const List = styled.ol`
+const List = styled(Box)`
+  display: grid;
+  grid-auto-flow: row;
+  grid-gap: ${spacing.small};
   list-style-type: none;
+  padding: ${spacing.small} 0 0;
 `
 
 TransactionList.propTypes = {
