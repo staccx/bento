4740640e397a05ad1bd758562cf752e98d9ac527
robinsandborg <robin.sandborg@gmail.com>
2018-05-03 12:55:37 +0200

    Some whitespace in Ad

--- src/components/Ad.js
+++ src/components/Ad.js
@@ -1,21 +1,37 @@
 import React from "react"
+import { font } from "@staccx/theme"
+import { Button, Box, Layout, LayoutItem } from "@staccx/base"
+import styled from "styled-components"
 import Pack from "./Pack"
-import { Button, Box } from "@staccx/base"
 
 const Ad = () => (
   <Box variant="adBox">
-    <img src="/tripletex.svg" width="200" alt="" />
-    <h3>Bruker du Tripletex?</h3>
-    <p>
-      Skaff deg tilgang til “selg faktura”-knappen i Tripletex. <br />
-      Det tar kun et par minutter.
-    </p>
-    <Pack>
-      <Button>Jeg vil ha fakturasalg!</Button>
-      <Button>Nei takk</Button>
-      <Button>3dje</Button>
-    </Pack>
+    <Layout grid="rows">
+      <LayoutItem>
+        <img src="/tripletex.svg" width="200" alt="" />
+      </LayoutItem>
+      <LayoutItem>
+        <AdHeading>Bruker du Tripletex?</AdHeading>
+      </LayoutItem>
+      <LayoutItem>
+        <p>
+          Skaff deg tilgang til “selg faktura”-knappen i Tripletex. <br />
+          Det tar kun et par minutter.
+        </p>
+      </LayoutItem>
+      <LayoutItem>
+        <Pack>
+          <Button>Jeg vil ha fakturasalg!</Button>
+          <Button>Nei takk</Button>
+          <Button>3dje</Button>
+        </Pack>
+      </LayoutItem>
+    </Layout>
   </Box>
 )
+
+const AdHeading = styled.h3`
+  font-size: ${font.h3};
+`
 
 export default Ad
