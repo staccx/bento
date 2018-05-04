94d873eb4385e5e3b189fbbb14101a91e6571948
Marius Hauken <marius.hauken@gmail.com>
2018-05-04 16:10:47 +0200

    Start on animation

--- src/Theme/Aprila/Theme.Modal.js
+++ src/Theme/Aprila/Theme.Modal.js
@@ -1,9 +1,10 @@
 import { css } from "styled-components"
-import { registerStyle, wrapper, spacing } from "@staccx/theme"
+import { registerStyle, wrapper, spacing, VARIANT_DEFAULT } from "@staccx/theme"
 import { ModalStyles } from "@staccx/base"
 
 export default registerStyle(
   {
+    [VARIANT_DEFAULT]: css``,
     small: css`
       max-width: ${wrapper.small};
       padding-top: ${spacing.medium};
