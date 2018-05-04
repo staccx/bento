39e71555bcbc0063b3a2f1d2b4c0bfd382bc44de
robinsandborg <robin.sandborg@gmail.com>
2018-05-04 11:44:42 +0200

    Remove Fonts from resets

--- src/Theme/Aprila/reset.js
+++ src/Theme/Aprila/reset.js
@@ -1,10 +1,5 @@
 export default `
   h1,h2,h3,h4,h5,h6 {
-    font-family: 'Fira Sans';
-    font-weight: 600;
-  }
-  html {
-    font-family: 'Fira Sans';
-    background-color: #fff;
+    font-weight: 300;
   }
 `
--- src/Theme/Norfjell/reset.js
+++ src/Theme/Norfjell/reset.js
@@ -1,9 +1,2 @@
 export default `
-  h1,h2,h3,h4,h5,h6 {
-    font-family: 'IBM Plex Sans';
-  }
-  html {
-    font-family: 'IBM Plex Sans';
-    background-color: #F8F8F8;
-  }
 `
