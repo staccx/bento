9efef39c5e958be955c25c6841c6138e4ee88c1b
Espen Th Sæverud <espensaeverud@gmail.com>
2018-05-06 18:40:55 +0200

    fix: Fixed issue with @staccx/animations not working with rest

--- src/Theme/Aprila/Theme.Modal.js
+++ src/Theme/Aprila/Theme.Modal.js
@@ -1,10 +1,14 @@
 import { css } from "styled-components"
 import { registerStyle, wrapper, spacing, VARIANT_DEFAULT } from "@staccx/theme"
 import { ModalStyles } from "@staccx/base"
+import { ScaleInY } from "@staccx/animations"
 
 export default registerStyle(
   {
-    [VARIANT_DEFAULT]: css``,
+    [VARIANT_DEFAULT]: css`
+      transform: scaleY(0);
+      animation: 0.2s ${ScaleInY} ease-out forwards 1;
+    `,
     small: css`
       max-width: ${wrapper.small};
       padding-top: ${spacing.medium};
