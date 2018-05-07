c6a2ebbcf60011c0973ba15a227e3ea0112d0800
Marius Hauken <marius.hauken@gmail.com>
2018-05-07 15:08:31 +0200

    Appear-animasjon på NorFjell

--- src/Theme/Norfjell/Theme.Modal.js
+++ src/Theme/Norfjell/Theme.Modal.js
@@ -1,10 +1,21 @@
 import { css } from "styled-components"
-import { registerStyle, spacing, wrapper } from "@staccx/theme"
+import { registerStyle, spacing, wrapper, VARIANT_DEFAULT } from "@staccx/theme"
 import { ModalStyles } from "@staccx/base"
+import { BounceIn } from "@staccx/animations"
+
+const boxAnimation = css`
+  opacity: 0;
+  transform: translateY(-12px);
+  animation: 0.4s ${BounceIn} cubic-bezier(0.175, 0.885, 0.32, 1.275) forwards 1;
+`
 
 export default registerStyle(
   {
+    [VARIANT_DEFAULT]: css`
+      ${boxAnimation};
+    `,
     small: css`
+      ${boxAnimation};
       max-width: ${wrapper.small};
       padding-top: ${spacing.medium};
     `
