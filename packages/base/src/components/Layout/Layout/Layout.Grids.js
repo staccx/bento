3e238687272cf9da79b7c1ff8bb4b95cba35db68
Marius Hauken <marius.hauken@gmail.com>
2018-05-07 15:40:16 +0200

    Responsive adjustments layout

--- packages/base/src/components/Layout/Layout/Layout.Grids.js
+++ packages/base/src/components/Layout/Layout/Layout.Grids.js
@@ -1,25 +1,24 @@
-import { wrapper, spacing } from "@staccx/theme"
+import { wrapper } from "@staccx/theme"
 import { css } from "styled-components"
 
 export default {
   stonehenge: css`
-    grid-template-columns: 0.5fr minmax(320px, 410px) minmax(320px, 410px) 0.5fr;
+    grid-template-columns: 0.5fr minmax(320px, 450px) minmax(320px, 450px) 0.5fr;
     grid-template-rows: auto auto auto 1fr;
     grid-template-areas:
       "header  header  header  header"
       ".  main  main  ."
       ".  body  aside  ."
       "footer  footer  footer  footer";
-
-    @media screen and (max-width: 1010px) {
+    @media only screen and (max-width: 730px) {
       grid-template-columns: 12px 1fr 12px;
       grid-template-rows: auto auto auto auto;
       grid-template-areas:
-        ".  header  ."
-        ".  main  ."
-        ".  body  ."
-        ".  aside  ."
-        ".  footer  .";
+        ". header ."
+        ". main ."
+        ". body ."
+        ". aside ."
+        ". footer .";
       grid-column-gap: 0;
     }
   `,
@@ -32,6 +31,11 @@ export default {
       ".  aside  ."
       ".  footer  .";
     grid-auto-flow: column;
+    @media only screen and (max-width: 500px) {
+      grid-template-columns: 12px 1fr 12px;
+      grid-template-rows: auto auto auto auto;
+      grid-column-gap: 0;
+    }
   `,
   rows: css`
     grid-auto-flow: row;
