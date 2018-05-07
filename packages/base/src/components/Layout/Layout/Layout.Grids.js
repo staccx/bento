e7be0f69b6d47272db84c84df7f49d5891323f48
robinsandborg <robin.sandborg@gmail.com>
2018-05-07 08:39:19 +0200

    Basic responsive stonehenge

--- packages/base/src/components/Layout/Layout/Layout.Grids.js
+++ packages/base/src/components/Layout/Layout/Layout.Grids.js
@@ -3,13 +3,25 @@ import { css } from "styled-components"
 
 export default {
   stonehenge: css`
-    grid-template-columns: 0.5fr minmax(320px, 520px) minmax(320px, 520px) 0.5fr;
+    grid-template-columns: 0.5fr minmax(320px, 410px) minmax(320px, 410px) 0.5fr;
     grid-template-rows: auto auto auto 1fr;
     grid-template-areas:
       "header  header  header  header"
       ".  main  main  ."
       ".  body  aside  ."
       "footer  footer  footer  footer";
+
+    @media screen and (max-width: 1010px) {
+      grid-template-columns: 12px 1fr 12px;
+      grid-template-rows: auto auto auto auto;
+      grid-template-areas:
+        ".  header  ."
+        ".  main  ."
+        ".  body  ."
+        ".  aside  ."
+        ".  footer  .";
+      grid-column-gap: 0;
+    }
   `,
   columnNarrow: css`
     grid-template-columns: 1fr ${wrapper.small} 1fr;
