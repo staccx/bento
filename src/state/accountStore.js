328019e766ae07d69711150cef3b7a6059e75017
robinsandborg <robin.sandborg@gmail.com>
2018-05-08 11:43:50 +0200

    Increase deposit, decrease interestAmount

--- src/state/accountStore.js
+++ src/state/accountStore.js
@@ -1,25 +1,30 @@
-import { action, observable } from 'mobx'
-import { getAccount, getTransactions } from './api'
+import { action, observable } from "mobx"
+import { getAccount, getTransactions } from "./api"
 
 class AccountStore {
   @observable transactions = []
 
   @observable selectedAccount = {}
-  @observable earned = 17822.45
+  @observable earned = 2776.944
 
-  @action setSelectedAccount = accountId => getAccount(accountId)
-    .then(account => {
-      this.selectedAccount = account
-      return accountId
-    })
-    .then(this.setTransactions)
+  @action
+  setSelectedAccount = accountId =>
+    getAccount(accountId)
+      .then(account => {
+        this.selectedAccount = account
+        return accountId
+      })
+      .then(this.setTransactions)
 
-  @action setTransactions = accountId => getTransactions(accountId).then(transactions => this.transactions = transactions)
+  @action
+  setTransactions = accountId =>
+    getTransactions(accountId).then(
+      transactions => (this.transactions = transactions)
+    )
 
-  constructor () {
-    this.setSelectedAccount('34551524578')
+  constructor() {
+    this.setSelectedAccount("34551524578")
   }
-
 }
 
 export default AccountStore
