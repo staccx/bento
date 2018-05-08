9f5162bafad184f920d251c7ca4ab3c8e9230b77
Marius Hauken <marius.hauken@gmail.com>
2018-05-08 15:40:18 +0200

    Remove console.log

--- src/pages/Inbox.js
+++ src/pages/Inbox.js
@@ -4,27 +4,24 @@ import { Modal, Layout, LayoutItem, ExpandListItem, List } from "@staccx/base"
 import { color, spacing, font, fontWeight } from "@staccx/theme"
 import inboxData from "./Inbox.data"
 
-const Deposit = () => {
-  console.log(inboxData)
-  return (
-    <Modal variant="small" isOpen>
-      <Layout grid="rows">
-        <LayoutItem>
-          <Heading>Innboks</Heading>
-        </LayoutItem>
-        <LayoutItem>
-          <List>
-            {inboxData.map(element => (
-              <ExpandListItem title={element.label} key={element._id}>
-                {element.content}
-              </ExpandListItem>
-            ))}
-          </List>
-        </LayoutItem>
-      </Layout>
-    </Modal>
-  )
-}
+const Deposit = () => (
+  <Modal variant="small" isOpen>
+    <Layout grid="rows">
+      <LayoutItem>
+        <Heading>Innboks</Heading>
+      </LayoutItem>
+      <LayoutItem>
+        <List>
+          {inboxData.map(element => (
+            <ExpandListItem title={element.label} key={element._id}>
+              {element.content}
+            </ExpandListItem>
+          ))}
+        </List>
+      </LayoutItem>
+    </Layout>
+  </Modal>
+)
 
 const Heading = styled.h1`
   font-size: ${font.h4};
