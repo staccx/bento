3d9fd5488a41d7a9d8b24000bedf8732d45c7eb1
robinsandborg <robin.sandborg@gmail.com>
2018-05-08 11:21:08 +0200

    Add ExpandIcon and switcheroo in/out icons

--- src/Theme/Shared/Transaction.ExpandButton.js
+++ src/Theme/Shared/Transaction.ExpandButton.js
@@ -3,17 +3,18 @@ import PropTypes from "prop-types"
 import styled from "styled-components"
 import { spacing, color, font } from "@staccx/theme"
 import { formatCurrency } from "@staccx/formatting"
+import { Caret } from "@staccx/base"
 
-const ExpandListBtn = ({ heading, date, amount, ...rest }) => (
+const ExpandListBtn = ({ heading, date, amount, isExpanded, ...rest }) => (
   <Item {...rest}>
     <Icon type={amount > 0 ? "deposit" : "withdrawal"}>
       {amount > 0 ? (
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-          <path d="M12.2 24a1.24 1.24 0 1 1 0-2.47 1.24 1.24 0 0 1 0 2.47zm0-3.7a1.24 1.24 0 1 1 0-2.48 1.24 1.24 0 0 1 0 2.47zm-.03-4.94a1.24 1.24 0 0 1-1.21-1.25V4.73L8.12 7.57a1.24 1.24 0 1 1-1.75-1.75L12.2 0l5.82 5.82a1.24 1.24 0 1 1-1.75 1.75l-2.83-2.84v9.38a1.24 1.24 0 0 1-1.26 1.25z" />
+          <path d="M12.2 0a1.24 1.24 0 1 0 0 2.47 1.24 1.24 0 0 0 0-2.47zm0 3.7a1.24 1.24 0 1 0 0 2.48 1.24 1.24 0 0 0 0-2.47zm-.03 4.94c-.68 0-1.22.57-1.21 1.25v9.38l-2.84-2.84a1.24 1.24 0 1 0-1.75 1.75L12.2 24l5.82-5.82a1.24 1.24 0 1 0-1.75-1.75l-2.83 2.84V9.89a1.24 1.24 0 0 0-1.26-1.25z" />
         </svg>
       ) : (
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-          <path d="M12.2 0a1.24 1.24 0 1 0 0 2.47 1.24 1.24 0 0 0 0-2.47zm0 3.7a1.24 1.24 0 1 0 0 2.48 1.24 1.24 0 0 0 0-2.47zm-.03 4.94c-.68 0-1.22.57-1.21 1.25v9.38l-2.84-2.84a1.24 1.24 0 1 0-1.75 1.75L12.2 24l5.82-5.82a1.24 1.24 0 1 0-1.75-1.75l-2.83 2.84V9.89a1.24 1.24 0 0 0-1.26-1.25z" />
+          <path d="M12.2 24a1.24 1.24 0 1 1 0-2.47 1.24 1.24 0 0 1 0 2.47zm0-3.7a1.24 1.24 0 1 1 0-2.48 1.24 1.24 0 0 1 0 2.47zm-.03-4.94a1.24 1.24 0 0 1-1.21-1.25V4.73L8.12 7.57a1.24 1.24 0 1 1-1.75-1.75L12.2 0l5.82 5.82a1.24 1.24 0 1 1-1.75 1.75l-2.83-2.84v9.38a1.24 1.24 0 0 1-1.26 1.25z" />
         </svg>
       )}
     </Icon>
@@ -22,6 +23,7 @@ const ExpandListBtn = ({ heading, date, amount, ...rest }) => (
       <div>{date}</div>
     </Body>
     <Amount>{formatCurrency(amount || 0)}</Amount>
+    <ExpandIcon isExpanded={isExpanded} />
   </Item>
 )
 
@@ -71,6 +73,13 @@ const Amount = styled.div``
 
 const TransactionHeading = styled.h4`
   font-size: ${font("transactionHeading")};
+`
+
+const ExpandIcon = styled(Caret)`
+  margin-left: ${spacing.small};
+  fill: ${color.primary};
+  transition: transform 0.3s ease-out;
+  transform: ${p => (p.isExpanded ? "rotate(180deg)" : "rotate(0)")};
 `
 
 ExpandListBtn.propTypes = {
