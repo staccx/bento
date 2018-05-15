7c23e0258e8489a65f0c7a09d6888c59fd1923ed
Espen Th Sæverud <espensaeverud@gmail.com>
2018-05-15 13:00:19 +0200

    fix: Fixed formatting not building bc of jest

--- packages/internal/formatting/src/currency.js
+++ packages/internal/formatting/src/currency.js
@@ -1,17 +1,17 @@
-import accounting from "accounting";
-import createNumberMask from "text-mask-addons/dist/createNumberMask";
+import accounting from "accounting"
+const createNumberMask = require("text-mask-addons/dist/createNumberMask")
 
-const prefix = "";
-const suffix = "";
-const includeThousandsSeparator = true;
-const thousandsSeparatorSymbol = " ";
-const allowDecimal = true;
-const decimalSymbol = ".";
-const decimalLimit = 0;
-const requireDecimal = false;
-const allowNegative = false;
-const allowLeadingZeroes = false;
-const integerLimit = null;
+const prefix = ""
+const suffix = ""
+const includeThousandsSeparator = true
+const thousandsSeparatorSymbol = " "
+const allowDecimal = true
+const decimalSymbol = "."
+const decimalLimit = 0
+const requireDecimal = false
+const allowNegative = false
+const allowLeadingZeroes = false
+const integerLimit = null
 
 export const currencyMaskOptions = {
   prefix,
@@ -25,9 +25,9 @@ export const currencyMaskOptions = {
   allowNegative,
   allowLeadingZeroes,
   integerLimit
-};
+}
 
-const format = "%v%s";
+const format = "%v%s"
 
 const currencyOptions = {
   symbol: suffix,
@@ -35,14 +35,14 @@ const currencyOptions = {
   thousand: thousandsSeparatorSymbol,
   decimal: decimalSymbol,
   format
-};
+}
 
 export const formatCurrency = (number, options = {}) => {
   if (typeof number !== "number") {
-    throw new TypeError(`Expected a number, got ${typeof number}`);
+    throw new TypeError(`Expected a number, got ${typeof number}`)
   }
-  const opts = { ...currencyOptions, ...options };
-  const { symbol, precision, thousand, decimal, format } = opts;
+  const opts = { ...currencyOptions, ...options }
+  const { symbol, precision, thousand, decimal, format } = opts
   return accounting.formatMoney(
     number,
     symbol,
@@ -50,37 +50,34 @@ export const formatCurrency = (number, options = {}) => {
     thousand,
     decimal,
     format
-  );
-};
+  )
+}
 
 export const createCurrencyMask = (options = {}) => {
-  options = Object.assign({}, currencyMaskOptions, options);
-  return createNumberMask(options);
-};
+  options = Object.assign({}, currencyMaskOptions, options)
+  return createNumberMask(options)
+}
 
 export const abbreviateCurrency = value => {
-  let newValue = value;
+  let newValue = value
   if (value >= 1000) {
-    const suffixes = ["", "k", "m", "b", "t"];
-    const suffixNum = Math.floor(("" + value).length / 3);
-    let shortValue = "";
+    const suffixes = ["", "k", "m", "b", "t"]
+    const suffixNum = Math.floor(("" + value).length / 3)
+    let shortValue = ""
     for (let precision = 2; precision >= 1; precision--) {
       shortValue = parseFloat(
         (suffixNum !== 0
           ? value / Math.pow(1000, suffixNum)
           : value
         ).toPrecision(precision)
-      );
-      const dotLessShortValue = (shortValue + "").replace(
-        /[^a-zA-Z 0-9]+/g,
-        ""
-      );
+      )
+      const dotLessShortValue = (shortValue + "").replace(/[^a-zA-Z 0-9]+/g, "")
       if (dotLessShortValue.length <= 2) {
-        break;
+        break
       }
     }
-    if (shortValue % 1 != 0) shortValue = shortValue.toFixed(1);
-    newValue = shortValue + suffixes[suffixNum];
+    if (shortValue % 1 != 0) shortValue = shortValue.toFixed(1)
+    newValue = shortValue + suffixes[suffixNum]
   }
-  return newValue;
-};
+  return newValue
+}
--- packages/internal/formatting/src/date.test.js
+++ packages/internal/formatting/src/date.test.js
@@ -1,61 +1,61 @@
-import { formatDate, formatDateDistance } from "./date";
-import { addDays } from "date-fns";
-import { enUS } from "date-fns/esm/locale";
+import { formatDate, formatDateDistance } from "./date"
+import { addDays } from "date-fns"
+import { enUS } from "date-fns/esm/locale"
 
 describe("Date", () => {
   describe("Format", () => {
     it("Should format ISO 8601 with defaults", () => {
-      const formatted = formatDate("2018-04-25T00:00:00");
-      expect(formatted).toBe("25 april 2018");
-    });
+      const formatted = formatDate("2018-04-25T00:00:00")
+      expect(formatted).toBe("25 april 2018")
+    })
     it("Should format ISO 8601 with other formats", () => {
-      const format = f => formatDate("2018-04-25T00:00:00", f);
+      const format = f => formatDate("2018-04-25T00:00:00", f)
 
-      expect(format("YYYY")).toBe("2018");
-      expect(format("MM/YYYY")).toBe("04/2018");
-      expect(format("DD/MM")).toBe("25/04");
-      expect(format("DD/MM")).toBe("25/04");
-      expect(format("HH:mm")).toBe("00:00");
-      expect(format("MMMM")).toBe("april");
-    });
+      expect(format("YYYY")).toBe("2018")
+      expect(format("MM/YYYY")).toBe("04/2018")
+      expect(format("DD/MM")).toBe("25/04")
+      expect(format("DD/MM")).toBe("25/04")
+      expect(format("HH:mm")).toBe("00:00")
+      expect(format("MMMM")).toBe("april")
+    })
     it("Should take Date object", () => {
-      const date = new Date(1525968057000);
-      expect(formatDate(date)).toBe("10 mai 2018");
-    });
-  });
+      const date = new Date(1525968057000)
+      expect(formatDate(date)).toBe("10 mai 2018")
+    })
+  })
   describe("FormatDistance", () => {
     it("Should format dates with defaults", () => {
-      const date = new Date(1525910400000);
-      const old = new Date(1524614400000);
+      const date = new Date(1525910400000)
+      const old = new Date(1524614400000)
 
-      const formatted = formatDateDistance(old, date);
-      expect(formatted).toBe("15 dager");
-    });
+      const formatted = formatDateDistance(old, date)
+      expect(formatted).toBe("15 dager")
+    })
 
     it("Should format ISO 8601 with defaults", () => {
       const formatted = formatDateDistance(
         "2018-04-25T00:00:00",
         "2018-05-10T00:00:00"
-      );
-      expect(formatted).toBe("15 dager");
-    });
+      )
+      expect(formatted).toBe("15 dager")
+    })
 
     it("Should format mixed inputs with defaults", () => {
-      const date = new Date(1524614400000);
-      const date2 = new Date(1525910400000);
+      const date = new Date(1524614400000)
+      const date2 = new Date(1525910400000)
 
-      expect(formatDateDistance(date, "2018-05-10T00:00:00")).toBe("15 dager");
-      expect(formatDateDistance("2018-04-25T00:00:00", date2)).toBe("15 dager");
-    });
+      expect(formatDateDistance(date, "2018-05-10T00:00:00")).toBe("15 dager")
+      expect(formatDateDistance("2018-04-25T00:00:00", date2)).toBe("15 dager")
+    })
 
     it("Should get distance only one input with defaults", () => {
-      const date = addDays(new Date(), 10);
-      expect(formatDateDistance(date)).toBe("10 dager");
-    });
+      const date = addDays(new Date(), 10)
+      expect(formatDateDistance(date)).toBe("10 dager")
+    })
 
     it("Should be able to override locale", () => {
-      const date = addDays(new Date(), 10);
-      expect(formatDateDistance(date, new Date(), enUS)).toBe("10 days");
-    });
-  });
-});
+      const date = addDays(new Date(), 10)
+      expect(formatDateDistance(date, new Date(), enUS)).toBe("10 days")
+    })
+  })
+})
