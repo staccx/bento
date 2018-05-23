fff73892d5a0bd88196cfe5301a42bb39b32fc1c
Marius Hauken <marius.hauken@gmail.com>
2018-05-23 10:19:29 +0200

    Support for both enum-schema-types

--- packages/internal/base/src/components/Forms/JsonSchema/ArrayFieldTemplate.js
+++ packages/internal/base/src/components/Forms/JsonSchema/ArrayFieldTemplate.js
@@ -2,31 +2,41 @@ import React from "react"
 import Label from "../Label/Label"
 import CheckBox from "../CheckBox/CheckBox"
 
-const ArrayFieldTemplate = props => {
-  console.log(props)
-  return (
-    <div className={props.className}>
-      <Label>{props.title}</Label>
-      {props.schema.items.enumNames
-        ? props.schema.items.enumNames.map((element, index) => {
-            console.log(element)
+const ArrayFieldTemplate = props => (
+  <div className={props.className}>
+    <Label>{props.title}</Label>
+    {props.schema.items.enumNames
+      ? props.schema.items.enumNames.map((element, index) => {
+          return (
+            <CheckBox
+              group={props.title}
+              id={props.title + index}
+              onChange={() => null}
+              key={element + index}
+              value={props.schema.items.enum[index]}
+            >
+              {element}
+            </CheckBox>
+          )
+        })
+      : props.schema.items.anyOf
+        ? props.schema.items.anyOf.map((element, index) => {
             return (
               <CheckBox
                 group={props.title}
                 id={props.title + index}
                 onChange={() => null}
-                key={element + index}
-                value={props.schema.items.enum[index]}
+                key={element.title + element.enum}
+                value={element.enum[0]}
               >
-                {element}
+                {element.title}
               </CheckBox>
             )
           })
         : console.error(
-            "Enum doesn't have corresponding enumNames: https://github.com/mozilla-services/react-jsonschema-form#custom-labels-for-enum-fields"
+            "Your enum doesn't follow the pattern for https://github.com/mozilla-services/react-jsonschema-form#alternative-json-schema-compliant-approach or is missin enumNames: https://github.com/mozilla-services/react-jsonschema-form#custom-labels-for-enum-fields"
           )}
-    </div>
-  )
-}
+  </div>
+)
 
 export default ArrayFieldTemplate
