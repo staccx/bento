2c5715a36256aa65effc5823294645929033f1a2
Marius Hauken <marius.hauken@gmail.com>
2018-05-25 13:40:17 +0200

    Adjustments on onboarding [Grid, inputs]

--- packages/apps/onboarding-pm/src/App.js
+++ packages/apps/onboarding-pm/src/App.js
@@ -30,18 +30,20 @@ class App extends Component {
     return (
       <ThemeProxyProvider theme={theme}>
         <Form size="small">
-          <Heading level="2">Personalia</Heading>
-          <PhoneInput label="Mobiltelefon" id="dlkn2" />
-          <Input label="E-post" placeholder="mail@mail.com" id="mail" />
-          <div>
+          <Fieldset>
+            <Heading level="2">Personalia</Heading>
+            <PhoneInput label="Mobilnummer" id="dlkn2" />
+            <Input label="E-post" placeholder="mail@mail.com" id="mail" />
+          </Fieldset>
+          <Fieldset>
             <Heading level="2">Om din sparing</Heading>
             <Paragraph>
               Vi er pliktig til å innhente informasjon om ditt kundeforhold i
               henhold til hvitvaskingsloven og ber derfor om at du svarer på
               spørsmålene nedenfor.
             </Paragraph>
-          </div>
-          <fieldset>
+          </Fieldset>
+          <Fieldset>
             <Legend>Hva er opprinnelsen til midlene som spares?</Legend>
             <CheckGroup onChange={handleOriginChange} group="origin">
               <CheckBox
@@ -71,22 +73,33 @@ class App extends Component {
               >
                 Salg av eiendom
               </CheckBox>
+              <CheckBox
+                group="opprinnelse"
+                id="other"
+                value="other"
+                key="test4"
+              >
+                Annet
+              </CheckBox>
             </CheckGroup>
-          </fieldset>
-          <fieldset>
+          </Fieldset>
+          <Fieldset>
             <Legend>Huk av kun dersom dette gjelder deg</Legend>
             <CheckGroup onChange={handleUtenlandsChange} group="utenlands">
               <CheckBox id="231432" value="foreignCitizen">
                 Jeg har utenlandsk statsborgerskap og/eller skatteplikt utenfor
                 Norge.
               </CheckBox>
+              <CheckBox id="dfkjlsdf" value="foreignCitizenExposed">
+                Jeg er eller har vært politisk eksponert person i utlandet.
+              </CheckBox>
               <CheckBox id="5436457" value="notOnlyPrivatePurpose">
                 Kontoen er ikke til privat bruk.
               </CheckBox>
             </CheckGroup>
-          </fieldset>
+          </Fieldset>
 
-          <fieldset>
+          <Fieldset>
             <Heading level="2">Samtykke</Heading>
             <CheckGroup onChange={handleSamtykkeChange} group="samtykke">
               <CheckBox
@@ -102,10 +115,10 @@ class App extends Component {
                 value="newsAndAdvertisment"
               >
                 Jeg samtykker til mottak av av tilbud og nyheter om andre
-                produkter fra Nordsjøbanken.
+                produkter.
               </CheckBox>
             </CheckGroup>
-          </fieldset>
+          </Fieldset>
         </Form>
       </ThemeProxyProvider>
     )
@@ -116,8 +129,13 @@ const Form = styled(Wrapper)`
   padding-top: ${spacing.large};
   display: grid;
   grid-template-columns: 1fr;
-  grid-column-gap: 24px;
-  grid-row-gap: 24px;
+  grid-row-gap: 48px;
+`
+
+const Fieldset = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-row-gap: 12px;
 `
 
 export default App
