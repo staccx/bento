39fc5f8f274306da311278e772026d8216efd088
Espen Th Sæverud <espensaeverud@gmail.com>
2018-06-01 13:20:16 +0200

    fix(loan-process): Now uses new theme syntax

--- packages/apps/loan-process/src/theme.js
+++ packages/apps/loan-process/src/theme.js
@@ -1,9 +1,4 @@
-import { Theme } from "@staccx/theme"
 import StyrkeTheme from "@staccx/styrke-theme"
 import FunduTheme from "@staccx/fundu-theme"
 
-const styrkeTheme = new Theme(StyrkeTheme, {})
-const funduTheme = new Theme(FunduTheme, {})
-
-export const ThemeStyrke = styrkeTheme.get
-export const ThemeFundu = funduTheme.get
+export { StyrkeTheme as ThemeStyrke, FunduTheme as ThemeFundu }
