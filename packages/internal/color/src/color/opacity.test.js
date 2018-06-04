194a2ef66dfc0e26880efc54ccd05b6b726a4945
Espen Th Sæverud <espensaeverud@gmail.com>
2018-06-04 10:25:13 +0200

    fix(color): Fixed test for opacity

--- packages/internal/color/src/color/opacity.test.js
+++ packages/internal/color/src/color/opacity.test.js
@@ -3,6 +3,6 @@ import colorNames from "./colorNames"
 
 describe("Opacity", () => {
   it("Set", () => {
-    expect(opacity(colorNames.white, 0.5)).toBe("rgba(255,255,255,0.5)")
+    expect(opacity(colorNames.white, 0.5)).toBe("rgba(255, 255, 255, 0.5)")
   })
 })
