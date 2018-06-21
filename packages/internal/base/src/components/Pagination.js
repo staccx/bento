079707b3f017aed17f03feb9592e1cfc9374f275
Torgeir Skjøtskift <torgeirs@stacc.com>
2018-06-21 13:50:20 +0200

    fix(base/pagination): fixed nextPage and previousPage

--- packages/internal/base/src/components/Pagination.js
+++ packages/internal/base/src/components/Pagination.js
@@ -9,38 +9,29 @@ class Pagination extends React.Component {
       items: props.items.slice(0, props.itemsPerPage),
       currentIndex: props.startIndex,
       pageCount: Math.ceil(props.items.length / props.itemsPerPage),
-      nextPage: this.resolveItems(props.startIndex + 1),
+      nextPage: () => this.resolveItems(props.startIndex + 1),
       previousPage: null,
       page: props.startIndex + 1
     }
-
-    this.resolveItems = this.resolveItems.bind(this)
   }
 
   resolveItems(currentIndex) {
-    console.log(this)
-    if (currentIndex < 0 || currentIndex >= this.state.pageCount) {
-      return
-    }
-
     const start = currentIndex * this.props.itemsPerPage
     this.setState({
       currentIndex,
       page: currentIndex + 1,
-      items: this.props.items.splice(start, start + this.props.itemsPerPage),
+      items: this.props.items.slice(start, start + this.props.itemsPerPage),
       nextPage:
         currentIndex < this.state.pageCount - 1
-          ? this.resolveItems(currentIndex + 1)
+          ? () => this.resolveItems(currentIndex + 1)
           : null,
       previousPage:
-        currentIndex > 0 ? this.resolveItems(currentIndex - 1) : null
+        currentIndex > 0 ? () => this.resolveItems(currentIndex - 1) : null
     })
   }
 
   render() {
-    return this.props.children({
-      ...this.state
-    })
+    return this.props.children({ ...this.state })
   }
 }
 
