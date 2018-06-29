16af9de852647ffa5afd237c8f4926928e099885
Espen Th Sæverud <espensaeverud@gmail.com>
2018-06-29 13:23:12 +0200

    feat(loan-process): Split calculator and personalia

--- packages/apps/loan-process/src/export.js
+++ packages/apps/loan-process/src/export.js
@@ -1,13 +1,10 @@
 import { default as SetAccountNumber } from "./module/SetAccountNumber"
 import { default as PresentOffer } from "./module/PresentOffer"
-// import { default as Manual } from "./module/Manual"
-// import { default as RejectOffer } from "./module/NoThankYou"
 import { default as ProvideAdditionalInfo } from "./module/ProvideAdditionalInfo"
 import { default as RegisterSigners } from "./module/RegisterSigners"
-// import { default as LoanRejected } from "./module/Rejection"
 import { default as Sign } from "./module/Sign"
 import { default as Calculator } from "./module/Calculator"
-// import { default as TakesTime } from "./module/TakesTime"
+import { default as Personalia } from "./module/Personalia"
 import { default as EndScreen } from "./module/EndScreen"
 import { default as PersonalFinance } from "./module/PersonalFinance"
 import { default as OfferTable } from "./module/OfferTable"
@@ -21,5 +18,6 @@ export {
   EndScreen,
   PersonalFinance,
   OfferTable,
-  Calculator
+  Calculator,
+  Personalia
 }
--- packages/apps/loan-process/src/module/Calculator.js
+++ packages/apps/loan-process/src/module/Calculator.js
@@ -2,15 +2,10 @@ import PropTypes from "prop-types"
 import React from "react"
 import {
   Box,
-  Input,
-  CompanyInput,
-  CurrencyInput,
-  PhoneInput,
   Select,
   SliderKeyboardInput,
   Button,
   Label,
-  RadioButton,
   Odometer,
   Text,
   Wrapper,
@@ -19,7 +14,6 @@ import {
   LayoutItem,
   List,
   SplitListItem,
-  Alert,
   Divider
 } from "@staccx/base"
 import { createCurrencyMask } from "@staccx/formatting"
@@ -46,15 +40,7 @@ class Calculator extends React.Component {
         .min(this.props.minValue, this.props.errorValueTooLow)
         .max(this.props.maxValue, this.props.errorValueTooHigh)
         .required(this.props.errorValueRequired),
-      terms: Yup.number().required(this.props.errorTerms),
-      name: Yup.string().required(this.props.errorNameRequired),
-      company: Yup.object().required(this.props.errorCompanyRequired),
-      revenue: Yup.number().required(this.props.errorRevenueRequired),
-      email: Yup.string()
-        .email(this.props.errorEmailInvalid)
-        .required(this.props.errorEmailRequired),
-      phone: Yup.string().required(this.props.errorPhoneNumberRequired),
-      purpose: Yup.string().required(this.props.errorPurposeRequired)
+      terms: Yup.number().required(this.props.errorTerms)
     })
     this.setState(
       {
@@ -88,13 +74,7 @@ class Calculator extends React.Component {
       <Formik
         initialValues={{
           value: this.state.value,
-          terms: this.state.terms,
-          name: "",
-          company: null,
-          revenue: null,
-          email: "",
-          phone: "",
-          purpose: ""
+          terms: this.state.terms
         }}
         validationSchema={this.state.validateSchema}
         onSubmit={this.props.onValidated}
@@ -249,257 +229,6 @@ class Calculator extends React.Component {
                     </Layout>
                     <Divider height={2} variant="calculator" />
                   </div>
-                  <div>
-                    <Layout variant="formElements" inCalculator>
-                      <LayoutItem>
-                        <Field
-                          name={`name`}
-                          render={({ field }) => (
-                            <Input
-                              id={`name`}
-                              {...field}
-                              placeholder={this.props.namePlaceholder}
-                              label={this.props.nameLabel}
-                            />
-                          )}
-                        />
-                        {touched.name &&
-                          errors.name && (
-                            <Alert variant="error" type="warning">
-                              {errors.name}
-                            </Alert>
-                          )}
-                      </LayoutItem>
-                      <LayoutItem>
-                        <Field
-                          name={`company`}
-                          render={({ field }) => {
-                            const { onChange, ...props } = field
-                            return (
-                              <CompanyInput
-                                label={this.props.companyNameLabel}
-                                placeholder={this.props.companyNamePlaceholder}
-                                onSearch={this.props.companySearchFunc}
-                                mapItem={this.props.mapCompany}
-                                id="company"
-                                onSelect={value =>
-                                  setFieldValue("company", value)
-                                }
-                                {...props}
-                              />
-                            )
-                          }}
-                        />
-                        {touched.company &&
-                          errors.company && (
-                            <Alert variant="error" type="warning">
-                              {errors.company}
-                            </Alert>
-                          )}
-                      </LayoutItem>
-                      <LayoutItem>
-                        <Field
-                          name={`revenue`}
-                          render={({ field }) => (
-                            <CurrencyInput
-                              id={"revenue"}
-                              label={this.props.revenueLabel}
-                              placeholder={this.props.revenuePlaceholder}
-                              locale={"nb"}
-                              {...field}
-                            />
-                          )}
-                        />
-                        {touched.revenue &&
-                          errors.revenue && (
-                            <Alert variant="error" type="warning">
-                              {errors.revenue}
-                            </Alert>
-                          )}
-                      </LayoutItem>
-                      <LayoutItem>
-                        <Field
-                          name={`email`}
-                          render={({ field }) => (
-                            <Input
-                              id={"email"}
-                              label={this.props.emailLabel}
-                              placeholder={this.props.emailPlaceholder}
-                              {...field}
-                            />
-                          )}
-                        />
-                        {touched.email &&
-                          errors.email && (
-                            <Alert variant="error" type="warning">
-                              {errors.email}
-                            </Alert>
-                          )}
-                      </LayoutItem>
-                      <LayoutItem>
-                        <Field
-                          name={`phone`}
-                          render={({ field }) => (
-                            <PhoneInput
-                              label={this.props.phoneLabel}
-                              id={"phone"}
-                              placeholder={this.props.phonePlaceholder}
-                              {...field}
-                            />
-                          )}
-                        />
-                        {touched.phone &&
-                          errors.phone && (
-                            <Alert variant="error" type="warning">
-                              {errors.phone}
-                            </Alert>
-                          )}
-                      </LayoutItem>
-                      <LayoutItem>
-                        <Field
-                          name={`purpose`}
-                          render={({ field }) => {
-                            const { name, value, onBlur } = field
-                            return (
-                              <Select
-                                items={this.props.loanPurposes}
-                                label={this.props.purposeLabel}
-                                placeHolderLabel={this.props.purposePlaceholder}
-                                name={name}
-                                value={value}
-                                onChange={item =>
-                                  setFieldValue("purpose", item)
-                                }
-                                onBlur={onBlur}
-                                variant="loanPurpose"
-                              />
-                            )
-                          }}
-                        />
-                        {touched.purpose &&
-                          errors.purpose && (
-                            <Alert variant="error" type="warning">
-                              {errors.purpose}
-                            </Alert>
-                          )}
-                      </LayoutItem>
-                      <LayoutItem>
-                        <Field
-                          name={`purpose`}
-                          render={({ field }) => {
-                            const { name, value, onBlur } = field
-                            return (
-                              <Select
-                                items={[
-                                  "Nettbetaling",
-                                  "Kortbetaling",
-                                  "Fakturering",
-                                  "Kontantbetaling"
-                                ]}
-                                label={
-                                  "Hvordan betaler som oftest kundene dine deg?"
-                                }
-                                placeHolderLabel={"Velg…"}
-                                name={name}
-                                value={value}
-                                onChange={item => console.log(item)}
-                                onBlur={onBlur}
-                                variant="loanPurpose"
-                              />
-                            )
-                          }}
-                        />
-                        {touched.purpose &&
-                          errors.purpose && (
-                            <Alert variant="error" type="warning">
-                              {errors.purpose}
-                            </Alert>
-                          )}
-                      </LayoutItem>
-                      <LayoutItem>
-                        <Field
-                          name={`paymentsInternational`}
-                          render={({ field }) => {
-                            const { name } = field
-                            return (
-                              <Box variant="radioContainer">
-                                <Label htmlFor={name}>
-                                  Mottar du innbetalinger fra utenfor Norge?
-                                </Label>
-                                <RadioButton
-                                  name={name}
-                                  id="213ewqs"
-                                  onChange={() =>
-                                    console.log("Do you copy? Over.")
-                                  }
-                                >
-                                  Ja
-                                </RadioButton>
-                                <RadioButton
-                                  name={name}
-                                  id="21iehwdaj"
-                                  onChange={() =>
-                                    console.log("Do you copy? Over.")
-                                  }
-                                >
-                                  Nei
-                                </RadioButton>
-                              </Box>
-                            )
-                          }}
-                        />
-                        {touched.purpose &&
-                          errors.purpose && (
-                            <Alert variant="error" type="warning">
-                              {errors.purpose}
-                            </Alert>
-                          )}
-                      </LayoutItem>
-                      <LayoutItem>
-                        <Field
-                          name={`paymentCash`}
-                          render={({ field }) => {
-                            const { name } = field
-                            return (
-                              <Box variant="radioContainer">
-                                <Label htmlFor={name}>
-                                  Tar du i mot kontanter i betaling fra dine
-                                  kunder
-                                </Label>
-                                <RadioButton
-                                  name={name}
-                                  id="3riejwfjnsldk"
-                                  onChange={() =>
-                                    console.log("Do you copy? Over.")
-                                  }
-                                >
-                                  Ja
-                                </RadioButton>
-                                <RadioButton
-                                  name={name}
-                                  id="okwenfs"
-                                  onChange={() =>
-                                    console.log("Do you copy? Over.")
-                                  }
-                                >
-                                  Nei
-                                </RadioButton>
-                              </Box>
-                            )
-                          }}
-                        />
-                        {touched.purpose &&
-                          errors.purpose && (
-                            <Alert variant="error" type="warning">
-                              {errors.purpose}
-                            </Alert>
-                          )}
-                      </LayoutItem>
-                    </Layout>
-                    {isValid && (
-                      <Button type={"submit"}>{this.props.buttonText}</Button>
-                    )}
-                  </div>
                 </Box>
               </Box>
               <Button onClick={this.props.onClick}>Neste</Button>
@@ -513,97 +242,60 @@ class Calculator extends React.Component {
 
 export default Calculator
 
-Calculator.propTypes = {
-  buttonText: PropTypes.string,
-  companyNameLabel: PropTypes.string,
-  companyNamePlaceholder: PropTypes.string,
-  companySearchFunc: PropTypes.func,
-  defaultTerms: PropTypes.number,
-  defaultValue: PropTypes.number,
-  downPaymentPerMonthText: PropTypes.string,
-  emailLabel: PropTypes.string,
-  emailPlaceholder: PropTypes.string,
-  errorCompanyRequired: PropTypes.string,
-  errorEmailInvalid: PropTypes.string,
-  errorEmailRequired: PropTypes.string,
-  errorNameRequired: PropTypes.string,
-  errorPhoneNumberRequired: PropTypes.string,
-  errorPurposeRequired: PropTypes.string,
-  errorRevenueRequired: PropTypes.string,
-  errorTerms: PropTypes.string,
-  errorValueRequired: PropTypes.string,
-  errorValueTooHigh: PropTypes.string,
-  errorValueTooLow: PropTypes.string,
-  headingText: PropTypes.string,
-  interestRate: PropTypes.number,
-  interestRateText: PropTypes.string,
-  priceExampleText: PropTypes.string,
-  loanAmount: PropTypes.number,
-  loanDurationLabel: PropTypes.string,
-  loanPurposes: PropTypes.array,
-  mapCompany: PropTypes.func,
-  maxValue: PropTypes.number,
-  minValue: PropTypes.number,
-  monthlyFeesText: PropTypes.string,
-  nameLabel: PropTypes.string,
-  namePlaceholder: PropTypes.string,
-  onValidated: PropTypes.func.isRequired,
-  phoneLabel: PropTypes.string,
-  phonePlaceholder: PropTypes.string,
-  purposePlaceholder: PropTypes.string,
-  revenueLabel: PropTypes.string,
-  revenuePlaceholder: PropTypes.string,
-  showDownpayment: PropTypes.bool,
-  showMonthlyFees: PropTypes.bool,
-  showTotalMonthly: PropTypes.bool,
-  showInterestRate: PropTypes.bool,
-  startFee: PropTypes.number,
-  termFee: PropTypes.number,
-  termValues: PropTypes.array,
-  totalMonthlyText: PropTypes.string,
-  valueLabel: PropTypes.string,
-  purposeLabel: PropTypes.string,
-  productType: PropTypes.string,
-  creditExplanationText: PropTypes.string,
-  onClick: PropTypes.func
-}
-
 Calculator.defaultProps = {
   buttonText: "Søk om lån",
-  companyNameLabel: "Selskap",
-  companyNamePlaceholder: "Navn eller orgnummer",
+  creditExplanationText:
+    "Du betaler kun påløpte renter av beløpet du har brukt, resten betaler du når du selv ønsker.",
   defaultTerms: 6,
   defaultValue: 250000,
   downPaymentPerMonthText: "Nedbetaling månedlig",
-  emailLabel: "Epost",
-  emailPlaceholder: "mail@mail.com",
-  interestRateText: "Rente",
+  headingText: "Se hva lånet koster",
   interestRate: 12.9,
+  interestRateText: "Rente",
   loanDurationLabel: "Nedbetalingstid",
-  loanPurposes: ["party", "hoverboard"],
   maxValue: 1000000,
   minValue: 10000,
-  monthlyFeesText: "Månedlige gebyr",
-  nameLabel: "Navn",
-  namePlaceholder: "Navn navnesen",
-  phoneLabel: "Telefonnummer",
-  phonePlaceholder: "xxx xx xxx",
-  purposePlaceholder: "Velg...",
-  revenueLabel: "Omsetning siste 12 monads",
+  priceExampleText:
+    "Lånet gis med fastrente i hele lånets løpetid. Priseksempel: 500 000 over 6 mnd, eff. rente 11,56 % kostnader 26 232, totalt 526 232.",
+  productType: "PRODUCT_LOAN",
   showDownpayment: true,
+  showInterestRate: true,
   showMonthlyFees: true,
   showTotalMonthly: true,
-  showInterestRate: true,
   startFee: 0,
   termFee: 3000,
   termValues: [6, 12, 18, 24, 36],
   totalMonthlyText: "Totalt månedlig",
-  valueLabel: "Ønsket lånebeløp",
-  headingText: "Se hva lånet koster",
-  purposeLabel: "Hva skal lånet brukes til?",
-  priceExampleText:
-    "Lånet gis med fastrente i hele lånets løpetid. Priseksempel: 500 000 over 6 mnd, eff. rente 11,56 % kostnader 26 232, totalt 526 232.",
-  productType: "PRODUCT_LOAN",
-  creditExplanationText:
-    "Du betaler kun påløpte renter av beløpet du har brukt, resten betaler du når du selv ønsker."
+  valueLabel: "Ønsket lånebeløp"
+}
+
+Calculator.propTypes = {
+  buttonText: PropTypes.string,
+  creditExplanationText: PropTypes.string,
+  defaultTerms: PropTypes.any,
+  defaultValue: PropTypes.number,
+  downPaymentPerMonthText: PropTypes.string,
+  errorTerms: PropTypes.any,
+  errorValueRequired: PropTypes.any,
+  errorValueTooHigh: PropTypes.any,
+  errorValueTooLow: PropTypes.any,
+  headingText: PropTypes.string,
+  interestRate: PropTypes.number,
+  interestRateText: PropTypes.string,
+  loanDurationLabel: PropTypes.string,
+  maxValue: PropTypes.number,
+  minValue: PropTypes.number,
+  onClick: PropTypes.any,
+  onValidated: PropTypes.any,
+  priceExampleText: PropTypes.string,
+  productType: PropTypes.string,
+  showDownpayment: PropTypes.bool,
+  showInterestRate: PropTypes.bool,
+  showMonthlyFees: PropTypes.bool,
+  showTotalMonthly: PropTypes.bool,
+  startFee: PropTypes.any,
+  termFee: PropTypes.number,
+  termValues: PropTypes.array,
+  totalMonthlyText: PropTypes.string,
+  valueLabel: PropTypes.string
 }
