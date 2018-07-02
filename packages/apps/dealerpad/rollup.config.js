278bf2f5831524fcfb2989ac1dbfb963995772e9
robinsandborg <robin.sandborg@gmail.com>
2018-07-02 15:38:52 +0200

    Add dealerpad

---
+++ packages/apps/dealerpad/rollup.config.js
@@ -0,0 +1,30 @@
+import babel from "rollup-plugin-babel"
+import commonjs from "rollup-plugin-commonjs"
+import resolve from "rollup-plugin-node-resolve"
+import json from "rollup-plugin-json"
+
+import pkg from "./package.json"
+
+export default {
+input: "./index.js",
+output: [
+{
+file: pkg.main,
+format: "cjs"
+},
+{
+file: pkg.module,
+format: "es"
+}
+],
+external: ["react", "react-dom", "prop-types", "styled-components", "@staccx/theme", "@staccx/base"],
+plugins: [
+commonjs(),
+resolve(),
+json(),
+babel({
+exclude: ["node_modules/**"],
+plugins: ["external-helpers"]
+})
+]
+}
