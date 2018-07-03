e3e20f5780e9d48ab0fd102bed9a50145be633e7
robinsandborg <robin.sandborg@gmail.com>
2018-07-03 13:21:46 +0200

    Move news-data

---
+++ packages/apps/dealerpad/src/data/news.js
@@ -0,0 +1,34 @@
+export default () => [
+  {
+    title: "Leasing kampanje",
+    body: "Bacon ipsum dolor amet prosciutto",
+    image: "https://source.unsplash.com/p7tai9P7H-s/1600x900",
+    actionText: "Mer om leasing-kampanjen",
+    actionUrl: "/news/lease",
+    featured: true
+  },
+  {
+    title: "GDPR – Hva betyr det for vårt samarbeid?",
+    body: "Bacon ipsum dolor amet prosciutto",
+    actionText: "Bli kjent med GDPR",
+    actionUrl: "/news/gdpr"
+  },
+  {
+    title: "Nye retningslinjer - gyldig legitimasjon",
+    body: "Bacon ipsum dolor amet prosciutto",
+    actionText: "Les om legitimasjon",
+    actionUrl: "/news/legitimasjon"
+  },
+  {
+    title: "Elektronisk salgsmelding",
+    body: "Bacon ipsum dolor amet prosciutto",
+    actionText: "Selg enda mer",
+    actionUrl: "/news/esalg"
+  },
+  {
+    title: "Utkjøp og avlesning av kilometerstand",
+    body: "Bacon ipsum dolor amet prosciutto",
+    actionText: "Bli en utkjøpsmester",
+    actionUrl: "/news/utkjop"
+  }
+]
--- packages/apps/dealerpad/src/pages/Home.js
+++ packages/apps/dealerpad/src/pages/Home.js
@@ -1,45 +1,12 @@
 import React from "react"
 import { Heading, Button } from "@staccx/base"
 import NewsHero from "../components/News/News.Hero"
-const newsItems = [
-  {
-    title: "Leasing kampanje",
-    body: "Bacon ipsum dolor amet prosciutto",
-    image: "https://source.unsplash.com/p7tai9P7H-s/1600x900",
-    actionText: "Mer om leasing-kampanjen",
-    actionUrl: "/news/lease",
-    featured: true
-  },
-  {
-    title: "GDPR – Hva betyr det for vårt samarbeid?",
-    body: "Bacon ipsum dolor amet prosciutto",
-    actionText: "Bli kjent med GDPR",
-    actionUrl: "/news/gdpr"
-  },
-  {
-    title: "Nye retningslinjer - gyldig legitimasjon",
-    body: "Bacon ipsum dolor amet prosciutto",
-    actionText: "Les om legitimasjon",
-    actionUrl: "/news/legitimasjon"
-  },
-  {
-    title: "Elektronisk salgsmelding",
-    body: "Bacon ipsum dolor amet prosciutto",
-    actionText: "Selg enda mer",
-    actionUrl: "/news/esalg"
-  },
-  {
-    title: "Utkjøp og avlesning av kilometerstand",
-    body: "Bacon ipsum dolor amet prosciutto",
-    actionText: "Bli en utkjøpsmester",
-    actionUrl: "/news/utkjop"
-  }
-]
+import getNewsItems from "../data/news"
 
 const Home = () => (
   <div>
     <header>
-      <NewsHero article={newsItems[0]} />
+      <NewsHero article={getNewsItems()[0]} />
     </header>
 
     <Heading level={1}>Welcome home</Heading>
