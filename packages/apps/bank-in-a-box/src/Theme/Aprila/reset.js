e4962a25bb90fcb80e3de43ccaf2a82c3f42728c
Espen Th Sæverud <espensaeverud@gmail.com>
2018-07-04 09:05:45 +0200

    fix(theme): Fixe registerstyle. Deleted old themes

--- packages/apps/bank-in-a-box/src/Theme/Aprila/reset.js
+++
@@ -1,9 +0,0 @@
-export default `
-  ::selection {
-    background: rgba(255, 126, 104, 0.6);
-    color: #3B2C7C;
-  }
-  h1,h2,h3,h4,h5,h6 {
-    font-weight: 500;
-  }
-`
--- packages/internal/theme/src/registerStyle.js
+++ packages/internal/theme/src/registerStyle.js
@@ -1,4 +1,5 @@
-export default (styles, name) => {
+export default (styles, style) => {
+  const name = style && style.hasOwnProperty("name") ? style.name : style
   return {
     [name]: {
       ...styles
