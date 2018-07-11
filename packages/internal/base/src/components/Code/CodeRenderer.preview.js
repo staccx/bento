59e54379b1c8b8b1636ebe6e08cca8fe1b59afe6
Torgeir Skjøtskift <torgeirs@stacc.com>
2018-07-11 10:23:22 +0200

    feat(styleguide): added CodeRenderer preview

---
+++ packages/internal/base/src/components/Code/CodeRenderer.preview.js
@@ -0,0 +1,25 @@
+import React from "react"
+import CodeRenderer from "./CodeRenderer"
+
+const preview = {
+  title: "CodeRenderer",
+  category: "component",
+  tags: ["component", "code"],
+  component: CodeRenderer,
+  render: props => (
+    <CodeRenderer
+      language="javascript"
+      code={`import React from "react"
+
+class MyApp extends React.Component {
+  render() {
+      return <div>Hello world!</div>
+  }
+} 
+
+export default MyApp`}
+    />
+  )
+}
+
+export default preview
