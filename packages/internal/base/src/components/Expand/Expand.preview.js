55b8286841bf51c6c62721bfcaaa5e8678923c18
Torgeir Skjøtskift <torgeirs@stacc.com>
2018-07-11 10:58:56 +0200

    feat(styleguide): added Expand preview

---
+++ packages/internal/base/src/components/Expand/Expand.preview.js
@@ -0,0 +1,23 @@
+import React from "react"
+import Expand from "./Expand"
+
+const preview = {
+  title: "Expand",
+  category: "component",
+  tags: ["visual", "animated"],
+  component: Expand,
+  render: props => (
+    <Expand {...props}>
+      <span>Expand me</span>
+      <div>
+        <ul>
+          <li>We are all hidden</li>
+          <li>Children of expand</li>
+        </ul>
+        <button onClick={() => console.log("Yo")}>Ikke klikk på meg</button>
+      </div>
+    </Expand>
+  )
+}
+
+export default preview
