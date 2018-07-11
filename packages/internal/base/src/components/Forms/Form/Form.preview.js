574bd07426ec2c7dbb9238136670edc1f3e0aa47
Torgeir Skjøtskift <torgeirs@stacc.com>
2018-07-11 12:33:11 +0200

    feat(styleguide): added Form preview

---
+++ packages/internal/base/src/components/Forms/Form/Form.preview.js
@@ -0,0 +1,31 @@
+import React from "react"
+import Form from "./Form"
+import FormField from "./FormField"
+import Input from "../Input/Input"
+
+const preview = {
+  title: "Form",
+  category: "component",
+  tags: ["component"],
+  component: Form,
+  render: props => (
+    <Form initialValues={{ epost: "" }} buttonText="Søk nå" {...props}>
+      <FormField
+        name="epost"
+        type="email"
+        required
+        renderError={({ error, touched }) => (
+          <span>
+            {error}-{touched}
+          </span>
+        )}
+      >
+        {({ name, field, form }) => (
+          <Input {...field} placeholder="ola@nordmann.no" id={name} />
+        )}
+      </FormField>
+    </Form>
+  )
+}
+
+export default preview
