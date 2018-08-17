a803ade3071dd22bdedb4a6bb344f9861b799f12
Marius Hauken <marius.hauken@gmail.com>
2018-08-17 13:02:25 +0200

    fix(dealerpad): updated location of loading for documents

--- packages/apps/dealerpad/src/pages/Case.js
+++ packages/apps/dealerpad/src/pages/Case.js
@@ -149,17 +149,16 @@ class Case extends Component {
             caseNumber={currentCase.applicationId}
           />
         </LayoutItem>
-
-        {caseStore.loadingCaseDetails ? (
-          <Loading />
-        ) : (
-          <LayoutItem
-            variant="caseDocumentation"
-            hideOnMobile={this.state.currentTab !== "documentation"}
-          >
-            <Heading level="2" variant="subtle">
-              Dokumentasjon
-            </Heading>
+        <LayoutItem
+          variant="caseDocumentation"
+          hideOnMobile={this.state.currentTab !== "documentation"}
+        >
+          <Heading level="2" variant="subtle">
+            Dokumentasjon
+          </Heading>
+          {caseStore.loadingCaseDetails ? (
+            <Loading />
+          ) : (
             <div>
               {currentCase.documents.map(document => (
                 <Documentation
@@ -172,8 +171,8 @@ class Case extends Component {
                 />
               ))}
             </div>
-          </LayoutItem>
-        )}
+          )}
+        </LayoutItem>
 
         <LayoutItem
           variant="caseContact"
