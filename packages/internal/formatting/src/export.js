3db5738b0f7a1fd791f3603792f14653f9b10d43
robinsandborg <robin.sandborg@gmail.com>
2018-08-23 12:58:06 +0200

    fix(internal): fix build-blocking bugs

---
+++ packages/internal/formatting/src/export.js
@@ -0,0 +1,25 @@
+import {
+  formatCurrency,
+  createCurrencyMask,
+  currencyMaskOptions,
+  abbreviateCurrency
+} from "./src/currency"
+import { formatDateDistance, formatDateRelative, formatDate } from "./src/date"
+import { default as removeWhitespace } from "./src/removeWhitespace"
+import { default as formatName } from "./src/formatName"
+import { default as truncate } from "./src/truncate"
+import { default as formatPhone } from "./src/phone"
+
+export {
+  formatCurrency,
+  createCurrencyMask,
+  currencyMaskOptions,
+  abbreviateCurrency,
+  formatDateRelative,
+  formatDateDistance,
+  formatDate,
+  removeWhitespace,
+  formatName,
+  formatPhone,
+  truncate
+}
