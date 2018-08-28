6768a15e923388d57a332b428b49cb034f3323c5
robinsandborg <robin.sandborg@gmail.com>
2018-08-28 15:30:50 +0200

    feat(stacc-webiste): add more content

---
+++ packages/apps/stacc-website/src/components/LogoSalad/LogoSalad.js
@@ -0,0 +1,62 @@
+import React from "react"
+import styled from "styled-components"
+import { color } from "@staccx/theme"
+import { Wrapper, ItemGroup, Heading, Layout, Button } from "@staccx/base"
+import FullWidth from "../FullWidth/FullWidth"
+
+const LogoSalad = ({ children, className }) => (
+  <FullWidth>
+    <Container className={className}>
+      <Wrapper>
+        <Layout paddingTop="large" paddingBottom="large" rowGap="large">
+          <Heading level={3}>
+            We help both established and growing companies achieve their goals
+          </Heading>
+
+          <Salad spacing="large">
+            <div>
+              <img src="/img/banklogos/nordea.svg" alt="Nordea" />
+            </div>
+            <div>
+              <img src="/img/banklogos/sparebankenvest.svg" alt="Nordea" />
+            </div>
+            <div>
+              <img src="/img/banklogos/eika.svg" alt="Nordea" />
+            </div>
+            <div>
+              <img src="/img/banklogos/bankaustria.svg" alt="Nordea" />
+            </div>
+            <div>
+              <img src="/img/banklogos/monobank.svg" alt="Nordea" />
+            </div>
+            <div>
+              <img src="/img/banklogos/easybank.svg" alt="Nordea" />
+            </div>
+            <div>
+              <img src="/img/banklogos/fundu.svg" alt="Nordea" />
+            </div>
+            <div>
+              <img src="/img/banklogos/aprila.svg" alt="Nordea" />
+            </div>
+          </Salad>
+          <div>
+            <Button>Read our success stories</Button>
+          </div>
+        </Layout>
+      </Wrapper>
+    </Container>
+  </FullWidth>
+)
+
+const Container = styled.div`
+  text-align: center;
+  background-color: ${color("g4")};
+`
+
+const Salad = styled(ItemGroup)`
+  > * {
+    justify-content: center;
+  }
+`
+
+export default LogoSalad
