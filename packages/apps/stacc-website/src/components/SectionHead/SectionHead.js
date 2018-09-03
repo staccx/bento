0c8e175ef6475bf990f8c29568d8caad18b24a07
robinsandborg <robin.sandborg@gmail.com>
2018-09-03 13:46:54 +0200

    feat(stacc-website): remove breadcrumbs from SectionHead

--- packages/apps/stacc-website/src/components/SectionHead/SectionHead.js
+++ packages/apps/stacc-website/src/components/SectionHead/SectionHead.js
@@ -1,33 +1,15 @@
 import React from "react"
 import PropTypes from "prop-types"
 import styled from "styled-components"
-import { Link } from "react-router-dom"
 import { Heading, Layout } from "@staccx/base"
-import { spacing, font, color } from "@staccx/theme"
+import { spacing, font } from "@staccx/theme"
 
-const SectionHead = ({
-  heading,
-  headingLevel,
-  lede,
-  illustration,
-  breadcrumb
-}) => {
+const SectionHead = ({ heading, headingLevel, lede, illustration }) => {
   return (
     <header>
       <Layout>
         <Body>
           <div>
-            <BreadcrumbContainer>
-              {breadcrumb &&
-                breadcrumb.length &&
-                breadcrumb.map(crumb => {
-                  return crumb.current ? (
-                    <CurrentCrumb>{crumb.label}</CurrentCrumb>
-                  ) : (
-                    <Breadcrumb to={crumb.url}>{crumb.label}</Breadcrumb>
-                  )
-                })}
-            </BreadcrumbContainer>
             <Heading level={headingLevel}>{heading}</Heading>
             <Lede>{lede}</Lede>
           </div>
@@ -40,30 +22,6 @@ const SectionHead = ({
     </header>
   )
 }
-
-const BreadcrumbContainer = styled.div`
-  display: block;
-  color: ${color.secondary};
-`
-
-const Breadcrumb = styled(Link)`
-  display: inline-block;
-  color: ${color.secondary};
-  text-decoration: none;
-
-  &:not(:last-child) {
-    &::after {
-      content: " > ";
-      color: ${color.gray};
-      display: inline-block;
-      margin: 0 ${spacing.tiny};
-    }
-  }
-`
-
-const CurrentCrumb = styled.span`
-  color: ${color.gray};
-`
 
 const Body = styled.div`
   display: flex;
