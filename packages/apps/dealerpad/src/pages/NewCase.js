94d2641a7de1176a191805496e70c777336d44dd
Marius Hauken <marius.hauken@gmail.com>
2018-09-06 16:09:07 +0200

    feat(dealerpad): add new case

--- packages/apps/dealerpad/src/pages/NewCase.js
+++ packages/apps/dealerpad/src/pages/NewCase.js
@@ -1,8 +1,8 @@
 import React from "react"
 import { Link } from "react-router-dom"
 import styled from "styled-components"
-import { Heading, Box, Wrapper, List, Portal, LayoutItem } from "@staccx/base"
-import { spacing } from "@staccx/theme"
+import { Heading, Box, Wrapper, List, Flag, LayoutItem } from "@staccx/base"
+import { spacing, ThemeComponent, color } from "@staccx/theme"
 import registerCase from "../data/registerCase"
 
 const Home = () => (
@@ -17,33 +17,54 @@ const Home = () => (
       </Wrapper>
     </Box>
     <Box variant="subtle">
-      <Wrapper>
-        <Grid>
+      <Grid>
+        <List>
           {registerCase.map((item, index) => (
             <li key={item.label}>
               <LayoutItem variant="fadeIn" delay={(index + 1) * 100 + 300}>
-                <Link to={item.path}>
-                  <Portal
-                    tagName={item.icon}
-                    label={item.label}
-                    variant="newCase"
-                  />
-                </Link>
+                <StyledLink to={item.path}>
+                  <Flag small img={<ThemeComponent tagName={item.icon} />}>
+                    {item.label}
+                  </Flag>
+                </StyledLink>
               </LayoutItem>
             </li>
           ))}
-        </Grid>
-      </Wrapper>
+        </List>
+        <Center>
+          <img src="/alfa1.png" alt="" />
+        </Center>
+      </Grid>
     </Box>
   </React.Fragment>
 )
 
-const Grid = styled(List)`
+const Grid = styled.div`
   display: grid;
   text-align: center;
-  grid-template-columns: repeat(auto-fill, minmax(100px, 1fr));
+  grid-template-columns: 200px 1fr;
   grid-column-gap: ${spacing.medium};
   grid-row-gap: ${spacing.medium};
+  > ul {
+    padding-left: ${spacing.medium};
+  }
+`
+
+const Center = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const StyledLink = styled(Link)`
+  text-align: left;
+  display: block;
+  padding: ${spacing.small} 0;
+  border-bottom: 1px solid ${color.line};
+  svg {
+    width: ${spacing.medium};
+    fill: ${color.primary};
+  }
 `
 
 export default Home
