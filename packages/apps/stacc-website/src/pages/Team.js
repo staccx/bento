2e1240b4b7e89e0dd6ead78b3f926c697b78664f
Espen Th Sæverud <espensaeverud@gmail.com>
2018-09-06 17:41:36 +0200

    feat(stacc-website): add Search for people

--- packages/apps/stacc-website/src/pages/Team.js
+++ packages/apps/stacc-website/src/pages/Team.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Heading, Wrapper, Input, Layout, Loading } from "@staccx/base"
+import { Heading, Wrapper, Input, Layout, Loading, Search } from "@staccx/base"
 import { SanityList } from "@staccx/sanity"
 import Person from "../components/Person/Person"
 import SectionHead from "../components/SectionHead/SectionHead"
@@ -8,52 +8,70 @@ import ContentLinksItem from "../components/ContentLinks/ContentLinks.Item"
 
 const Team = () => (
   <Wrapper>
-    <Layout paddingTop="grid" rowGap="grid">
-      <SectionHead
-        heading="Team Stacc"
-        headingLevel={1}
-        lede="Get all this content from Sanity please. Stacc AS is a software company group located in Bergen, Norway. The management has 25 years of experience with strategy, business development and technology in the financial services industry."
-      />
-      <ContentLinks>
-        <ContentLinksItem
-          heading="About the company"
-          url="/some/page"
-          body="Read more about Stacc, our mission, philosophy and goals"
-        />
-        <ContentLinksItem
-          heading="Join our team"
-          url="/careers"
-          body="Want to move finance forward? Help us create the next generation of finance technology."
-        />
-      </ContentLinks>
-      <Layout rowGap="medium">
-        <Heading level="2">Looking for someone?</Heading>
-        <Input id="peopleSearch" placeholder="Search employees" />
-      </Layout>
-      <ul>
-        <SanityList type={"person"} pick={"company->, ..."}>
-          {({ result }) => {
-            if (!result) {
-              return <Loading />
-            }
-            console.log(result)
-            return result.map(person => (
-              <Person
-                name={person.name}
-                image={person.image}
-                title={person.title}
-                company={person.company}
-                phone={person.phone}
-                email={person.email}
-                linkedin={person.linkedin}
-                twitter={person.twitter}
-                link={person.link}
+    <SanityList type={"person"} pick={"company->, ..."}>
+      {({ result }) => {
+        if (!result) {
+          return <Loading />
+        }
+        return (
+          <Layout paddingTop="grid" rowGap="grid">
+            <SectionHead
+              heading="Team Stacc"
+              headingLevel={1}
+              lede="Get all this content from Sanity please. Stacc AS is a software company group located in Bergen, Norway. The management has 25 years of experience with strategy, business development and technology in the financial services industry."
+            />
+            <ContentLinks>
+              <ContentLinksItem
+                heading="About the company"
+                url="/some/page"
+                body="Read more about Stacc, our mission, philosophy and goals"
               />
-            ))
-          }}
-        </SanityList>
-      </ul>
-    </Layout>
+              <ContentLinksItem
+                heading="Join our team"
+                url="/careers"
+                body="Want to move finance forward? Help us create the next generation of finance technology."
+              />
+            </ContentLinks>
+            <Search
+              indexer={"_id"}
+              documents={result}
+              indicises={["name", "title", ["company", "name"], "email"]}
+            >
+              {({ result: r, search }) => {
+                return (
+                  <React.Fragment>
+                    <Layout rowGap="medium">
+                      <Heading level="2">Looking for someone?</Heading>
+                      <Input
+                        id="peopleSearch"
+                        placeholder="Search employees"
+                        onChange={e => search(e.target.value)}
+                      />
+                    </Layout>
+                    <ul>
+                      {r &&
+                        r.map(person => (
+                          <Person
+                            name={person.name}
+                            image={person.image}
+                            title={person.title}
+                            company={person.company}
+                            phone={person.phone}
+                            email={person.email}
+                            linkedin={person.linkedin}
+                            twitter={person.twitter}
+                            link={person.link}
+                          />
+                        ))}
+                    </ul>
+                  </React.Fragment>
+                )
+              }}
+            </Search>
+          </Layout>
+        )
+      }}
+    </SanityList>
   </Wrapper>
 )
 
