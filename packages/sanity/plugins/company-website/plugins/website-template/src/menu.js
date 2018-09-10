6bfee8287e8af0dfd2d49bb819e3210fa1e20372
Espen Th Sæverud <espensaeverud@gmail.com>
2018-09-10 11:38:33 +0200

    fix(company-website-template): redo menu

--- packages/sanity/plugins/company-website/plugins/website-template/src/menu.js
+++ packages/sanity/plugins/company-website/plugins/website-template/src/menu.js
@@ -11,7 +11,7 @@ export default {
     {
       type: "array",
       name: "links",
-      title: "Links",
+      title: "MenuItems",
       of: [{ type: "menuItem" }]
     },
     {
--- packages/sanity/plugins/company-website/plugins/website-template/src/menuItem.js
+++ packages/sanity/plugins/company-website/plugins/website-template/src/menuItem.js
@@ -14,29 +14,37 @@ export default {
       title: "Is emphasized"
     },
     {
-      type: "array",
+      type: "reference",
       name: "link",
       title: "Link",
+      to: [
+        { type: "page" },
+        { type: "servicePage" },
+        { type: "productPage" },
+        { type: "homePage" },
+        { type: "clientsPage" },
+        { type: "teamPage" }
+      ]
+    },
+    {
+      type: "array",
+      name: "submenu",
+      title: "Submenu",
       of: [
-        {
-          type: "object",
-          title: "String link",
-          fields: [
-            {
-              type: "string",
-              name: "url",
-              title: "Url"
-            }
-          ]
-        },
         {
           type: "reference",
           name: "page",
           title: "Page",
-          to: [{ type: "page" }]
+          to: [
+            { type: "page" },
+            { type: "servicePage" },
+            { type: "productPage" },
+            { type: "homePage" },
+            { type: "clientsPage" },
+            { type: "teamPage" }
+          ]
         }
-      ],
-      validation: Rule => Rule.max(1).required()
+      ]
     }
   ]
 }
