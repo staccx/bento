d41d36df34bf4be941399812c016dfb50d7903c8
robinsandborg <robin.sandborg@gmail.com>
2018-09-12 15:43:47 +0200

    feat(stacc-website): style footer

---
+++ packages/apps/stacc-website/src/components/Footer/Footer.Navigation.js
@@ -0,0 +1,98 @@
+import React from "react"
+import styled from "styled-components"
+import { color, font, spacing } from "@staccx/theme"
+import { SanityDocument } from "@staccx/sanity"
+import { Layout, Heading } from "@staccx/base"
+import { NavLink } from "react-router-dom"
+
+const FooterNavigation = ({ heading, callToAction }) => (
+  <SanityDocument
+    id={"ffe2cd1d-2fed-4436-9942-ad9674dd80ea"}
+    pick={"links[]{link->,submenu[]->,...}, ..."}
+  >
+    {({ document: menu }) => {
+      if (!menu) {
+        return null
+      }
+
+      const mainMenu = menu.links.filter(menuItem => menuItem.link)
+      const subMenus = menu.links.filter(m => m.submenu && m.submenu.length > 0)
+
+      return (
+        <Layout rowGap="large">
+          <div>
+            <nav>
+              <FooterNav>
+                {mainMenu.map(menuItem => (
+                  <li key={menuItem._key}>
+                    <FooterNavLink
+                      to={menuItem.link.path.current}
+                      exact
+                      activeClassName="is-current"
+                      emphasized={
+                        menuItem.emphasized
+                          ? menuItem.emphasized.toString()
+                          : undefined
+                      }
+                    >
+                      {menuItem.title}
+                    </FooterNavLink>
+                  </li>
+                ))}
+              </FooterNav>
+            </nav>
+          </div>
+          <div>
+            <Layout>
+              {subMenus.map(m => (
+                <div>
+                  <SubNavHeading level={3}>{m.title}</SubNavHeading>
+                  <SubNav>
+                    {m.submenu.map(submenuItem => (
+                      <li key={submenuItem._id}>
+                        <FooterNavLink to={submenuItem.path.current}>
+                          {submenuItem.name}
+                        </FooterNavLink>
+                      </li>
+                    ))}
+                  </SubNav>
+                </div>
+              ))}
+            </Layout>
+          </div>
+        </Layout>
+      )
+    }}
+  </SanityDocument>
+)
+
+const SubNav = styled.ul`
+  display: grid;
+  grid-row-gap: ${spacing.tiny};
+  padding-top: ${spacing.tiny};
+`
+
+const SubNavHeading = styled(Heading)`
+  font-size: ${font.base};
+  color: ${color("g1")};
+  font-weight: normal;
+`
+
+const FooterNav = styled.ul`
+  display: flex;
+  justify-content: space-between;
+  padding-top: ${spacing.small};
+`
+
+const FooterNavLink = styled(NavLink)`
+  color: ${color.white};
+  text-decoration: none;
+
+  &:hover,
+  &:active,
+  &:focus {
+    color: ${color.primary};
+  }
+`
+
+export default FooterNavigation
--- packages/apps/stacc-website/src/components/Footer/Footer.js
+++ packages/apps/stacc-website/src/components/Footer/Footer.js
@@ -1,111 +1,20 @@
 import React from "react"
-import { createPortal } from "react-dom"
 import styled from "styled-components"
-import { Wrapper, fontSmoothing, Loading, Heading } from "@staccx/base"
-import { spacing, color } from "@staccx/theme"
-import { SanityDocument } from "@staccx/sanity"
-import Logo from "../Logo/Logo"
-import { NavLink, Link } from "react-router-dom"
+import { Wrapper, fontSmoothing } from "@staccx/base"
+import { spacing, color, wrapper } from "@staccx/theme"
+import FooterCompanyInfo from "./Footer.CompanyInfo"
+import FooterNavigation from "./Footer.Navigation"
 
 const Footer = ({ heading, callToAction }) => (
   <Container>
     <Wrapper>
       <Content>
-        <SanityDocument
-          id={"ffe2cd1d-2fed-4436-9942-ad9674dd80ea"}
-          pick={"links[]{link->,submenu[]->,...}, ..."}
-        >
-          {({ document: menu }) => {
-            if (!menu) {
-              return <Loading />
-            }
-
-            console.log(menu)
-            const mainMenu = menu.links.filter(menuItem => menuItem.link)
-            const subMenus = menu.links.filter(
-              m => m.submenu && m.submenu.length > 0
-            )
-            return (
-              <div>
-                <div>
-                  <Logo inverted />
-                </div>
-                <div>
-                  <SanityDocument id={"8af2f4f3-c1d2-4142-9509-b2c1636fe7a8"}>
-                    {staccGroup => {
-                      const { document: doc = null } = staccGroup
-                      if (!doc) {
-                        return null
-                      }
-
-                      console.log(staccGroup)
-                      const portal = createPortal(
-                        <div>Org.nr {doc.orgNr.orgNo}</div>,
-                        document.getElementById("orgNr")
-                      )
-                      return (
-                        <div>
-                          {doc.name}
-                          {doc.address}
-                          {portal}
-                        </div>
-                      )
-                    }}
-                  </SanityDocument>
-                  <SanityDocument id={"e96b6174-1228-4455-ab76-3d43597961b6"}>
-                    {insight => {
-                      const { document = null } = insight
-                      if (!document) {
-                        return null
-                      }
-
-                      // TODO: This is Stacc Insight document
-                      return <div>{document.address}</div>
-                    }}
-                  </SanityDocument>
-                  <div>
-                    {mainMenu.map(menuItem => (
-                      <li key={menuItem._key}>
-                        <NavLink
-                          to={menuItem.link.path.current}
-                          exact
-                          activeClassName="is-current"
-                          emphasized={
-                            menuItem.emphasized
-                              ? menuItem.emphasized.toString()
-                              : undefined
-                          }
-                        >
-                          {menuItem.title}
-                        </NavLink>
-                      </li>
-                    ))}
-                  </div>
-                  <div>
-                    {subMenus.map(m => (
-                      <div>
-                        <Heading level={3}>{m.title}</Heading>
-                        <ul>
-                          {m.submenu.map(submenuItem => (
-                            <li key={submenuItem._id}>
-                              <NavLink to={submenuItem.path.current}>
-                                {submenuItem.name}
-                              </NavLink>
-                            </li>
-                          ))}
-                        </ul>
-                      </div>
-                    ))}
-                  </div>
-                  <div id={"orgNr"} />
-                  <Link to={"/privacy-and-cookie-policy"}>
-                    Privacy and cookie policy
-                  </Link>
-                </div>
-              </div>
-            )
-          }}
-        </SanityDocument>
+        <div>
+          <FooterCompanyInfo />
+        </div>
+        <div>
+          <FooterNavigation />
+        </div>
       </Content>
     </Wrapper>
   </Container>
@@ -120,10 +29,25 @@ const Container = styled.footer`
 
 const Content = styled.div`
   display: flex;
-  justify-content: space-between;
+  flex-direction: column-reverse;
 
-  > div {
-    flex-basis: 25%;
+  @media only screen and (max-width: ${wrapper.medium}) {
+    > div:last-child {
+      margin-bottom: ${spacing.large};
+    }
+  }
+
+  @media only screen and (min-width: ${wrapper.medium}) {
+    flex-direction: row;
+    justify-content: space-between;
+
+    > div:first-child {
+      flex-basis: 25%;
+    }
+
+    > div:last-child {
+      flex-basis: 50%;
+    }
   }
 `
 
