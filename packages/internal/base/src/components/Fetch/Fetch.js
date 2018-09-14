9469c0c69179c31b0b6c3f9ea9857f637ad3fd2d
Espen Th Sæverud <espensaeverud@gmail.com>
2018-09-14 11:35:57 +0200

    feat(base): add Poll

--- packages/internal/base/src/components/Fetch/Fetch.js
+++ packages/internal/base/src/components/Fetch/Fetch.js
@@ -5,14 +5,35 @@ class Fetch extends React.Component {
   constructor(props, context) {
     super(props, context)
 
+    this.fetchData = this.fetchData.bind(this)
+    this.state = {
+      data: null
+    }
+    this.fetchData()
+  }
+
+  fetchData() {
+    clearTimeout(this.timeout)
+
     const get = this.props.get || window.fetch
 
     get(this.props.url, this.props.options)
       .then(this.props.mapData)
-      .then(data => this.setState({ data }))
+      .then(data => {
+        if (this.props.hasChanged) {
+          if (this.props.hasChanged(this.state.data, data)) {
+            this.setState({ data })
+          }
+        } else {
+          this.setState({ data })
+        }
+      })
 
-    this.state = {
-      data: null
+    if (this.props.poll) {
+      this.timeout = setTimeout(
+        () => this.fetchData(),
+        this.props.pollingInterval
+      )
     }
   }
 
@@ -26,6 +47,8 @@ Fetch.propTypes = {
   get: PropTypes.func,
   mapData: PropTypes.func,
   options: PropTypes.object,
+  poll: PropTypes.bool,
+  pollingInterval: PropTypes.number,
   url: PropTypes.string.isRequired
 }
 
@@ -33,5 +56,7 @@ export default Fetch
 
 Fetch.defaultProps = {
   mapData: item => item,
-  options: {}
+  options: {},
+  poll: false,
+  pollingInterval: 5000
 }
