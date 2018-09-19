edec77acfc006dcf41298575522da1b9988275a3
Espen Th Sæverud <espensaeverud@gmail.com>
2018-09-19 12:35:03 +0200

    feat(stacc-website): add gtm

---
+++ packages/internal/control-panel/src/commands/list.js
@@ -0,0 +1,11 @@
+var shell = require('shelljs');
+console.log(shell)
+//
+// if (!shell.which('git')) {
+//   shell.echo('Sorry, this script requires git');
+//   shell.exit(1);
+// }
+//
+//
+// shell.exec("git status")
+//
