64a892ffc24285da0e564181dd951a74c60e1499
robinsandborg <robin.sandborg@gmail.com>
2018-09-24 14:37:30 +0200

    feat(stacc-website): improve person layout

--- packages/apps/stacc-website/src/components/Icons/vCard.js
+++ packages/apps/stacc-website/src/components/Icons/vCard.js
@@ -5,8 +5,8 @@ const VCard = () => {
   return (
     <svg
       width="16"
-      height="16"
-      viewBox="0 0 16 16"
+      height="12"
+      viewBox="0 0 16 12"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
