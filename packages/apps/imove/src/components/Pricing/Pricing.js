bafe933d8cd1d6d1bd4579442ff97ea0ebd1b536
Joakim <joakimc@stacc.com>
2018-09-26 17:18:46 +0200

    feat(imove): add style to pricing

--- packages/apps/imove/src/components/Pricing/Pricing.js
+++ packages/apps/imove/src/components/Pricing/Pricing.js
@@ -5,32 +5,80 @@ import { Wrapper, Heading } from "@staccx/base"
 
 const Pricing = ({ heading, cars }) => (
   <Content>
-    <Wrapper>
-      <StyledHeading level={2}>{heading}</StyledHeading>
+    <Wrapper size="full">
+      <StyledMainHeading level={2}>{heading}</StyledMainHeading>
       <List>
         {cars.map(car => (
-          <li>
+          <Card>
             <img src={car.img} alt={car.name} />
-            <Heading level={3}>{car.name}</Heading>
-            <ul>
-              <li>{car.type}</li>
-              <li>{car.seats}</li>
-              <li>{car.doors}</li>
-              <li>{car.range}</li>
-            </ul>
-            <p>
+            <StyledHeading level={3}>{car.name}</StyledHeading>
+            <CarProperties>
+              <CarProperty icon="type">{car.type}</CarProperty>
+              <CarProperty icon="seats">{car.seats}</CarProperty>
+              <CarProperty icon="doors">{car.doors}</CarProperty>
+              <CarProperty icon="range">{car.range}</CarProperty>
+            </CarProperties>
+            <PriceBlob>
               fra
               <br />
               kr {car.price}
               <br />
               /mnd
-            </p>
-          </li>
+            </PriceBlob>
+          </Card>
         ))}
       </List>
     </Wrapper>
   </Content>
 )
+
+const PriceBlob = styled.div`
+  background-image: url(./img/brand/drop/green.svg);
+  width: 140px;
+  height: 140px;
+  margin: auto;
+  background-repeat: no-repeat;
+  background-size: contain;
+`
+
+const StyledMainHeading = styled(Heading)`
+  padding-bottom: calc(${spacing("medium")});
+`
+
+const StyledHeading = styled(Heading)`
+  color: ${color("primary")};
+`
+
+const CarProperties = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  font-size: ${font("large")};
+  text-align: left;
+  color: ${color("secondary")};
+`
+
+const CarProperty = styled.li`
+  min-width: 50%;
+  margin: ${spacing("small")} 0;
+  padding-left: calc(1em + 10px);
+  background-image: url(./img/icons/icon-car-${props => props.icon}.svg);
+  background-repeat: no-repeat;
+  background-position: left center;
+  }
+`
+
+const List = styled.ul`
+  display: flex;
+  justify-content: center;
+`
+
+const Card = styled.li`
+  box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.16);
+  border-radius: 10px;
+  max-width: 360px;
+  padding: 40px;
+  margin: 0 40px;
+`
 
 const Content = styled.div`
   font-size: ${font("largePlus")};
@@ -39,15 +87,6 @@ const Content = styled.div`
   background-image: url(${p => p.bg});
   background-size: contain;
   background-repeat: no-repeat;
-`
-
-const StyledHeading = styled(Heading)`
-  padding-bottom: calc(${spacing("medium")});
-`
-
-const List = styled.ul`
-  display: flex;
-  justify-content: space-around;
 `
 
 export default Pricing
