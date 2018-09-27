248b2f66791164bd1a513479da2eeb2593ce6958
Espen Th Sæverud <espensaeverud@gmail.com>
2018-09-27 10:23:11 +0200

    fix(stacc-website): add hack to scroll to top on window pop state

--- packages/apps/stacc-website/src/components/ScrollToTop.js
+++ packages/apps/stacc-website/src/components/ScrollToTop.js
@@ -2,6 +2,12 @@ import React from "react"
 import { withRouter } from "react-router-dom"
 
 class ScrollToTop extends React.Component {
+  componentWillMount() {
+    window.onpopstate = p => {
+      setTimeout(() => window.scrollTo(0, 0), 50)
+    }
+  }
+
   componentDidUpdate(prevProps) {
     if (this.props.location !== prevProps.location) {
       window.scrollTo(0, 0)
