1439de316f5f360a7a7a3f161c8e31a7da2c2b62
Torgeir Skjøtskift <torgeirs@stacc.com>
2018-09-27 13:54:46 +0200

    fix(imove-app): fix build

--- packages/apps/imove-app/rollup.config.js
+++ packages/apps/imove-app/rollup.config.js
@@ -1,2 +1,7 @@
-import generateConfig from "@staccx/rollup-config" import pkg from "./package.json" const config = generateConfig(pkg) //
-Config overrides go here export default config
+import generateConfig from "@staccx/rollup-config"
+import pkg from "./package.json"
+const config = generateConfig(pkg)
+
+// Config overrides go here
+
+export default config
