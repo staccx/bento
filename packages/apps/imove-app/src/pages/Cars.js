b5ba5fdadddbc6a405f740a08cdaff1a54d99e16
robinsandborg <robin.sandborg@gmail.com>
2018-10-02 11:28:40 +0200

    feat(imove-app): add functioning text-search for cars

--- packages/apps/imove-app/src/pages/Cars.js
+++ packages/apps/imove-app/src/pages/Cars.js
@@ -1,22 +1,31 @@
 import React from "react"
+import epitath from "epitath"
 import { Layout, Search } from "@staccx/base"
 import cars from "../data/cars"
 import CarsFilter from "../components/Cars/Cars.Filter"
 import CarsList from "../components/Cars/Cars.List"
 
-const Sell = ({ history }) => {
-  return (
-    <Search indicises={["923423", "123331133221"]} documents={cars}>
-      {({ change, isOpen = false }) => {
-        return (
-          <Layout paddingTop="medium" paddingBottom="huge" rowGap="large">
-            <CarsFilter cars={cars} />
-            <CarsList cars={cars} />
-          </Layout>
-        )
-      }}
-    </Search>
+const Cars = epitath(function*({ history }) {
+  const { result: makeAndModel, search: searchMakeAndModel } = yield (
+    <Search
+      indicises={["make", "model"]}
+      documents={cars}
+      indexer={"chassisNumber"}
+    />
   )
-}
 
-export default Sell
+  return (
+    <Layout paddingTop="medium" paddingBottom="huge" rowGap="large">
+      <CarsFilter
+        cars={cars}
+        result={makeAndModel}
+        handleSearchMake={searchMakeAndModel}
+        handleSearchPrice={console.log}
+        handleSearchRange={console.log}
+      />
+      <CarsList cars={makeAndModel} />
+    </Layout>
+  )
+})
+
+export default Cars
