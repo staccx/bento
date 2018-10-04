70d9a5be9bc9575a4c25d97246896de2d4da958e
Joakim <joakimc@stacc.com>
2018-10-04 16:10:28 +0200

    feat(imove-web): add css to header/nav

--- packages/apps/imove-web/src/components/Header/Header.js
+++ packages/apps/imove-web/src/components/Header/Header.js
@@ -1,28 +1,73 @@
 import React from "react"
+import styled from "styled-components"
+import { Wrapper } from "@staccx/base"
+import { font, color, spacing } from "@staccx/theme"
 
 const Header = () => (
-  <div>
-    <img src="" alt="" />
-    <nav>
-      <ul>
-        <li>
-          <a href="">Hva er inkludert?</a>
-        </li>
-        <li>
-          <a href="">Pris</a>
-        </li>
-        <li>
-          <a href="">Fordeler</a>
-        </li>
-        <li>
-          <a href="">Spørsmål og svar</a>
-        </li>
-        <li>
-          <a href="">Logg inn</a>
-        </li>
-      </ul>
-    </nav>
-  </div>
+  <Menu>
+    <StyledWrapper size="largePlus">
+      <img src="" alt="" />
+      <Navigation>
+        <Items>
+          <Item>
+            <a href="">Hva er inkludert?</a>
+          </Item>
+          <Item>
+            <a href="">Pris</a>
+          </Item>
+          <Item>
+            <a href="">Fordeler</a>
+          </Item>
+          <Item>
+            <a href="">Spørsmål og svar</a>
+          </Item>
+          <LoginButton>
+            <a href="/app">Logg inn</a>
+          </LoginButton>
+        </Items>
+      </Navigation>
+    </StyledWrapper>
+  </Menu>
 )
+
+const Menu = styled.div`
+  padding: ${spacing("large")};
+`
+
+const StyledWrapper = styled(Wrapper)`
+  display: flex;
+  justify-content: flex-end;
+  padding: ${spacing("medium")} $;
+  font-size: ${font("large")};
+`
+
+const Navigation = styled.nav`
+  display: block;
+`
+
+const Items = styled.ul`
+  display: flex;
+  justify-content: right;
+`
+
+const Item = styled.li`
+  a {
+    display: block;
+    color: ${color("primary")};
+    text-decoration: none;
+    padding: 0.4em 1em;
+  }
+`
+
+const LoginButton = styled.li`
+  a {
+    display: block;
+    color: ${color("primary")};
+    text-decoration: none;
+    padding: 0.4em 2em;
+    border: 3px solid ${color("primary")};
+  }
+  margin-left: 1em;
+`
 
 export default Header
