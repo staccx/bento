f478d1a2ba608991824eaa890a800c83309fae7e
robinsandborg <robin.sandborg@gmail.com>
2018-10-04 15:49:26 +0200

    refactor(imove-web): prepare exports for imove-cloud

--- packages/apps/imove-app/src/components/Carousel/Carousel.js
+++ packages/apps/imove-app/src/components/Carousel/Carousel.js
@@ -6,49 +6,63 @@ import { opacity } from "@staccx/color"
 import ArrowRight from "../Icons/ArrowRight"
 import ArrowLeft from "../Icons/ArrowLeft"
 
-const ImageCarousel = ({ images }) => (
-  <Outer>
-    <Carousel
-      initialSlideWidth={100}
-      heightMode="max"
-      wrapAround
-      renderCenterRightControls={({ nextSlide, slideCount, currentSlide }) => {
-        return (
-          <NextSlide onClick={nextSlide}>
-            <ArrowRight />
-          </NextSlide>
-        )
-      }}
-      renderCenterLeftControls={({
-        previousSlide,
-        slideCount,
-        currentSlide
-      }) => {
-        if (currentSlide === 0 || slideCount === 0) {
-          return null
-        }
-        return (
-          <PrevSlide onClick={previousSlide}>
-            <ArrowLeft />
-          </PrevSlide>
-        )
-      }}
-      renderBottomCenterControls={({ slideCount, currentSlide }) => {
-        return (
-          <DotsContainer>
-            {[...Array(slideCount)].map((e, index) => (
-              <Dot key={index} highlighted={currentSlide === index} />
-            ))}
-          </DotsContainer>
-        )
-      }}
-    >
-      {images.map(image => (
-        <img key={image} src={image} alt="" />
-      ))}
-    </Carousel>
-  </Outer>
-)
+class ImageCarousel extends React.Component {
+  componentDidUpdate() {
+    setTimeout(() => {
+      window.dispatchEvent(new Event("resize")) // Fix for some weird height issues
+    }, 0)
+  }
+  render() {
+    const { images } = this.props
+    return (
+      <Outer>
+        <Carousel
+          initialSlideWidth={100}
+          heightMode="max"
+          wrapAround
+          renderCenterRightControls={({
+            nextSlide,
+            slideCount,
+            currentSlide
+          }) => {
+            return (
+              <NextSlide onClick={nextSlide}>
+                <ArrowRight />
+              </NextSlide>
+            )
+          }}
+          renderCenterLeftControls={({
+            previousSlide,
+            slideCount,
+            currentSlide
+          }) => {
+            if (currentSlide === 0 || slideCount === 0) {
+              return null
+            }
+            return (
+              <PrevSlide onClick={previousSlide}>
+                <ArrowLeft />
+              </PrevSlide>
+            )
+          }}
+          renderBottomCenterControls={({ slideCount, currentSlide }) => {
+            return (
+              <DotsContainer>
+                {[...Array(slideCount)].map((e, index) => (
+                  <Dot key={index} highlighted={currentSlide === index} />
+                ))}
+              </DotsContainer>
+            )
+          }}
+        >
+          {images.map(image => (
+            <img key={image} src={image} alt="" />
+          ))}
+        </Carousel>
+      </Outer>
+    )
+  }
+}
 
 const Outer = styled.div`
   position: relative;
