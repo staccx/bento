8a48e7756f40da0ea6bcd71db4b31a1c492097e2
robinsandborg <robin.sandborg@gmail.com>
2018-10-04 15:56:35 +0200

    fix(imove-web): remove unused import

--- packages/apps/imove-web/src/App.js
+++ packages/apps/imove-web/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react"
 import { ThemeProxyProvider } from "@staccx/theme"
-import { BrowserRouter as Router } from "react-router-dom"
 import { imoveTheme } from "@staccx/imove-theme"
 import Page from "./Page"
 
