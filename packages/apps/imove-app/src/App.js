887c60ab4deff5c7f37c518a46a5ff842ae4ef7f
robinsandborg <robin.sandborg@gmail.com>
2018-10-05 15:19:19 +0200

    feat(imove-app): remove Wrapper

--- packages/apps/imove-app/src/App.js
+++ packages/apps/imove-app/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react"
-import { Wrapper } from "@staccx/base"
 import { ThemeProxyProvider } from "@staccx/theme"
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom"
 import Transitions from "./components/transitions/transitions"
@@ -34,83 +33,81 @@ class App extends Component {
                   {/* <Header /> */}
                   <Menu />
                   <Transitions pageKey={location.key} {...location.state}>
-                    <Wrapper size="small">
-                      <Switch location={location}>
-                        <Route path="/app/" exact component={LogIn} />
-                        <Route
-                          path="/app/onboarding"
-                          exact
-                          component={Onboarding}
-                        />
-                        <Route path="/app/my-car" exact component={MyCar} />
-                        <Route path="/app/sell" exact component={Sell} />
-                        <Route
-                          path="/app/sell/confirmation"
-                          exact
-                          component={SellConfirmation}
-                        />
-                        <Route
-                          path="/app/waiting-list"
-                          exact
-                          component={WaitingList}
-                        />
-                        <Route
-                          path="/app/waiting-list/confirmation"
-                          exact
-                          component={WaitingListConfirmation}
-                        />
-                        <Route path="/app/cars" exact component={Cars} />
-                        <Route
-                          path="/app/car/:chassisNumber"
-                          match={match}
-                          exact
-                          component={OrderCarDetail}
-                        />
-                        <Route
-                          path="/app/car/:chassisNumber/book"
-                          match={match}
-                          exact
-                          component={OrderBook}
-                        />
-                        <Route
-                          path="/app/car/:chassisNumber/confirm"
-                          match={match}
-                          exact
-                          component={OrderConfirm}
-                        />
-                        <Route path="/app/profile" exact component={Profile} />
-                        <Route
-                          path="/app/profile/edit"
-                          exact
-                          component={ProfileEdit}
-                        />
-                        <Route path="/app/end" exact component={End} />
-                        <Route
-                          path="/app/end/confirmation"
-                          exact
-                          component={EndConfirmation}
-                        />
-                        <Route
-                          path="/app"
-                          render={() => (
-                            <div>
-                              <h1>404</h1>
-                              <p>No content here (yet)</p>
-                              <Link to={"/app"}>Please log in to the app</Link>
-                            </div>
-                          )}
-                        />
-                        <Route
-                          path="/"
-                          render={() => (
-                            <div>
-                              This is where the landing pages lives.{" "}
-                              <Link to={"/app"}>Please log in to the app</Link>
-                            </div>
-                          )}
-                        />
-                      </Switch>
-                    </Wrapper>
+                    <Switch location={location}>
+                      <Route path="/app/" exact component={LogIn} />
+                      <Route
+                        path="/app/onboarding"
+                        exact
+                        component={Onboarding}
+                      />
+                      <Route path="/app/my-car" exact component={MyCar} />
+                      <Route path="/app/sell" exact component={Sell} />
+                      <Route
+                        path="/app/sell/confirmation"
+                        exact
+                        component={SellConfirmation}
+                      />
+                      <Route
+                        path="/app/waiting-list"
+                        exact
+                        component={WaitingList}
+                      />
+                      <Route
+                        path="/app/waiting-list/confirmation"
+                        exact
+                        component={WaitingListConfirmation}
+                      />
+                      <Route path="/app/cars" exact component={Cars} />
+                      <Route
+                        path="/app/car/:chassisNumber"
+                        match={match}
+                        exact
+                        component={OrderCarDetail}
+                      />
+                      <Route
+                        path="/app/car/:chassisNumber/book"
+                        match={match}
+                        exact
+                        component={OrderBook}
+                      />
+                      <Route
+                        path="/app/car/:chassisNumber/confirm"
+                        match={match}
+                        exact
+                        component={OrderConfirm}
+                      />
+                      <Route path="/app/profile" exact component={Profile} />
+                      <Route
+                        path="/app/profile/edit"
+                        exact
+                        component={ProfileEdit}
+                      />
+                      <Route path="/app/end" exact component={End} />
+                      <Route
+                        path="/app/end/confirmation"
+                        exact
+                        component={EndConfirmation}
+                      />
+                      <Route
+                        path="/app"
+                        render={() => (
+                          <div>
+                            <h1>404</h1>
+                            <p>No content here (yet)</p>
+                            <Link to={"/app"}>Please log in to the app</Link>
+                          </div>
+                        )}
+                      />
+                      <Route
+                        path="/"
+                        render={() => (
+                          <div>
+                            This is where the landing pages lives.{" "}
+                            <Link to={"/app"}>Please log in to the app</Link>
+                          </div>
+                        )}
+                      />
+                    </Switch>
                   </Transitions>
                 </div>
               )
