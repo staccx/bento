d9ff6aa31f8bdfd5a919f36388d2d366cf8bb3f2
Marius Hauken <marius.hauken@gmail.com>
2018-10-08 15:29:34 +0200

    feat(imove-app): menu now responsive

--- packages/clients/imove/theme/src/Theme/Theme.Box.js
+++ packages/clients/imove/theme/src/Theme/Theme.Box.js
@@ -6,15 +6,17 @@ export default registerStyle(
   {
     [VARIANT_DEFAULT]: css``,
     menu: css`
-      background: ${color.white};
-      position: fixed;
-      left: 0;
-      bottom: 0;
-      width: 100%;
       padding-top: ${spacing.small};
       padding-bottom: ${spacing.small};
-      box-shadow: 0px 4px 12px rgba(0, 0, 0, 0.15);
-      z-index: 100;
+      width: 100%;
+      @media screen and (max-width: 768px) {
+        background: ${color.white};
+        position: fixed;
+        left: 0;
+        bottom: 0;
+        box-shadow: 0px 4px 12px rgba(0, 0, 0, 0.15);
+        z-index: 100;
+      }
     `,
     myCarHeader: css`
       background-color: ${color("bgGray")};
