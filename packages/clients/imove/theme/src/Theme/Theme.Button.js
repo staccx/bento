97f4586b329c5cec38b86cbe5ffd000436c46a04
robinsandborg <robin.sandborg@gmail.com>
2018-10-12 10:11:44 +0200

    feat(imove-theme): lighten main enu

--- packages/clients/imove/theme/src/Theme/Theme.Button.js
+++ packages/clients/imove/theme/src/Theme/Theme.Button.js
@@ -23,20 +23,29 @@ export default registerStyle(
     `,
     mainNavigation: css`
       display: block;
-      background-color: ${color.primary};
-      color: ${color.white};
+      background-color: transparent;
+      color: ${color.primary};
       border-radius: 0;
       width: 100%;
-      background-image: url("data:image/svg+xml;charset=UTF-8,%3csvg width='6' height='10' viewBox='0 0 6 10' fill='none' xmlns='http://www.w3.org/2000/svg'%3e%3cpath d='M0 1.427L1.386 0 6 4.75 1.386 9.5 0 8.073 3.228 4.75 0 1.427z' fill='%23fff'/%3e%3c/svg%3e");
+      background-image: url("data:image/svg+xml;charset=UTF-8,%3csvg width='6' height='10' viewBox='0 0 6 10' fill='none' xmlns='http://www.w3.org/2000/svg'%3e%3cpath d='M0 1.427L1.386 0 6 4.75 1.386 9.5 0 8.073 3.228 4.75 0 1.427z' fill='%234FAF73'/%3e%3c/svg%3e");
       background-repeat: no-repeat;
       background-position: right ${spacing.small} center;
       text-align: left;
       min-height: 60px;
+      padding-left: 0;
+      border-bottom: 1px solid ${color.line};
+
+      &:hover,
+      &:active,
+      &:focus {
+        background-color: transparent;
+        color: ${color.secondary};
+      }
+
       @media only screen and (max-width: 430px) {
         margin-left: -${spacing.medium};
-      }
-      & + & {
-        margin-top: 1px;
+        margin-right: -${spacing.medium};
+        padding-left: ${spacing.medium};
       }
     `,
     back: css`
