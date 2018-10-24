1173d7926e2754a6faf6fbc3a601be697458e005
robinsandborg <robin.sandborg@gmail.com>
2018-10-24 11:40:05 +0200

    fix(wasteiq-web): fix build stuff

--- packages/apps/wasteiq-web/src/App.js
+++ packages/apps/wasteiq-web/src/App.js
@@ -1,21 +1,17 @@
 import React, { Component } from "react"
 import { Wrapper } from "@staccx/base"
-import { ThemeProxyProvider } from "@staccx/theme"
 import { BrowserRouter as Router } from "react-router-dom"
-import theme from "./theme/Theme"
 
 class App extends Component {
-render() {
-return (
-<ThemeProxyProvider theme={theme}>
-        <Router>
-            <Wrapper>
-                <div>Fresh app smell...</div>
-            </Wrapper>
-        </Router>
-</ThemeProxyProvider>
-)
-}
+  render() {
+    return (
+      <Router>
+        <Wrapper>
+          <div>Fresh app smell...</div>
+        </Wrapper>
+      </Router>
+    )
+  }
 }
 
 export default App
