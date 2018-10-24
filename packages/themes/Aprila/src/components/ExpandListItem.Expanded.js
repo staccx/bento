4c69899d916fca8e08ffb8d7622220e9ade44470
Espen Th Sæverud <espensaeverud@gmail.com>
2018-10-24 16:07:12 +0200

    fix(aprila-theme): remove extendDefault

--- packages/themes/Aprila/src/components/ExpandListItem.Expanded.js
+++ packages/themes/Aprila/src/components/ExpandListItem.Expanded.js
@@ -1,9 +1,11 @@
 import { css } from "styled-components"
 import { ExpandListItem, theming } from "@staccx/base"
 
-export default theming.extendDefault(
-  css`
-    padding: 0;
-  `,
+export default theming.createVariants(
+  {
+    [theming.VARIANT_DEFAULT]: css`
+      padding: 0;
+    `
+  },
   ExpandListItem.themeProps.expandedItem
 )
