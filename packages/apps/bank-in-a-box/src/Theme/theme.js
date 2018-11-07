eb4d23abacfc2f8aefd7d1ca18295f17048e6adc
Marius Hauken <marius.hauken@gmail.com>
2018-11-07 14:31:34 +0100

    feat(bank-in-a-box): removed unused code

--- packages/apps/bank-in-a-box/src/Theme/theme.js
+++ packages/apps/bank-in-a-box/src/Theme/theme.js
@@ -1,13 +1,9 @@
 import theme from "@staccx/norefjell-theme"
 import { backwardsStyle } from "../components/transitions/backwards"
 import { forwardsStyle } from "../components/transitions/forwards"
-import { TransactionListButtonStyle } from "./Shared/Transaction.ExpandButton"
-import { ExpandListItemButtonStyle } from "./Shared/ExpandListItem.Expanded"
 import { theming } from "@staccx/base"
 
 const BankTheme = theme.add([
-  TransactionListButtonStyle,
-  ExpandListItemButtonStyle,
   backwardsStyle,
   forwardsStyle,
   theming.createGlobal({ reset: theme.reset })
