89926f176902fdf761aa24753e3d72317b7665bf
Espen Th Sæverud <espensaeverud@gmail.com>
2018-11-09 13:42:28 +0100

    feat(developer-portal): change how module is exported

--- packages/apps/developer-portal/src/export.js
+++ packages/apps/developer-portal/src/export.js
@@ -1,1 +1,15 @@
-export { default } from "./App"
+import SwaggerDocs from "./pages/SwaggerDocs"
+import Introduction from "./pages/Introduction"
+import ApiReference from "./pages/ApiReference"
+import OpenApiProvider from "./components/OpenApiProvider"
+import OpenApiConsumer from "./components/OpenApiConsumer"
+import Header from "./components/Header/Header"
+
+export {
+  SwaggerDocs,
+  Introduction,
+  ApiReference,
+  OpenApiProvider,
+  OpenApiConsumer,
+  Header
+}
