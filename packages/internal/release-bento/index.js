440d3c83da3ecffa736d5ce61aafe9100ee72a2b
Espen Th Sæverud <espensaeverud@gmail.com>
2018-11-14 20:19:18 +0100

    fix(release-bento): add link to changelog in success output

--- packages/internal/release-bento/index.js
+++ packages/internal/release-bento/index.js
@@ -172,7 +172,7 @@ async function release(debug) {
     await postMessage(`@channel @${name}'s release failed. Let them know`)
     process.exit(1)
   }
-
+  
   /**
    * Post message to slack
    */
@@ -180,7 +180,7 @@ async function release(debug) {
     spinner.start("Alerting team on Slack")
     const name = await username()
     if (!debug) {
-      await postMessage(`@channel ${name} has released\n \`\`\`${text}\`\`\``)
+      await postMessage(`@channel ${name} has released\n \`\`\`${text}\`\`\`\nYou can find release notes here: https://bitbucket.org/stacc-flow/bento/src/master/CHANGELOG.md`)
     }
     spinner.succeed("Team alerted about the awesome")
   } catch (e) {
