9bcbcbec80f00ad45005b5137fe1fefe005d6a4d
Espen Th Sæverud <espensaeverud@gmail.com>
2018-11-16 15:20:13 +0100

    fix(utils): partition now returns accepted first

--- packages/internal/utils/src/export/partition.js
+++ packages/internal/utils/src/export/partition.js
@@ -1,7 +1,7 @@
 export default (list, condition) => {
   return list.reduce(
     (result, element) => {
-      result[condition(element) ? 1 : 0].push(element)
+      result[condition(element) ? 0 : 1].push(element)
       return result
     },
     [[], []]
