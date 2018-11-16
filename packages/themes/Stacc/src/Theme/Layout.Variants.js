d40e54f3c57a83768cbaaeea6fd56b90af6f26f2
Espen Th Sæverud <espensaeverud@gmail.com>
2018-11-16 15:19:23 +0100

    feat(stacc-theme): add grid variants for stories

---
+++ packages/themes/Stacc/src/Theme/Layout.Variants.js
@@ -0,0 +1,40 @@
+import { theming, Layout } from "@staccx/base"
+import { css } from "styled-components"
+
+export default theming.createVariants(
+  {
+    left: css`
+      grid-template-columns: 1fr 1fr;
+      grid-template-rows: auto;
+      grid-template-areas: "image content";
+      grid-column-gap: ${theming.spacing.large};
+      align-items: center;
+    `,
+    right: css`
+      grid-template-columns: 1fr 1fr;
+      grid-template-rows: auto;
+      grid-template-areas: "content image";
+      grid-column-gap: ${theming.spacing.large};
+      align-items: center;
+    `,
+    above: css`
+      grid-template-columns: 1fr;
+      grid-template-rows: auto auto;
+      grid-template-areas:
+        "image"
+        "content";
+      grid-row-gap: ${theming.spacing.large};
+      align-items: center;
+    `,
+    sideBySide: css`
+      grid-template-columns: 1fr;
+      grid-template-rows: auto auto;
+      grid-template-areas:
+        "image"
+        "content";
+      grid-row-gap: ${theming.spacing.large};
+      align-items: center;
+    `
+  },
+  Layout.themeProps.container
+)
