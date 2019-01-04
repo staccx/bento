90826d479d7c3fc228a9ca7f4615bc489661d3e2
Espen Th Sæverud <espensaeverud@gmail.com>
2019-01-04 13:02:30 +0100

    feat(base): add Form can now find granchildrend and setup validation

--- packages/internal/base/src/components/Forms/Form/Form.js
+++ packages/internal/base/src/components/Forms/Form/Form.js
@@ -3,63 +3,78 @@ import PropTypes from "prop-types"
 import { Formik } from "formik"
 import Button from "../../Button/Button"
 import { commonPropTypes } from "../../../constants/themeContants"
+import traverse from "react-traverse"
 const Yup = require("yup")
+
+const findFormFields = node => {
+  const formFields = []
+  const isFormField = name => name === "FormField"
+
+  traverse(node, {
+    ComponentElement(path) {
+      if (isFormField(path.node.type.name)) {
+        formFields.push(path.node)
+      }
+    }
+  })
+
+  return formFields
+}
 
 class Form extends Component {
   render() {
-    const validationShape = React.Children.toArray(this.props.children).reduce(
-      (validator, curr) => {
-        const { type, required, nullable, name, min, max, integer } = curr.props
+    const formFields = findFormFields(this.props.children)
+    console.log(formFields)
+    const validationShape = formFields.reduce((validator, curr) => {
+      const { type, required, nullable, name, min, max, integer } = curr.props
 
-        switch (type) {
-          case "string": {
-            validator[name] = Yup.string()
-            break
-          }
-          case "number": {
-            validator[name] = Yup.number()
-            if (min) {
-              validator[name] = validator[name].min(min)
-            }
-            if (max) {
-              validator[name] = validator[name].max(max)
-            }
-            if (integer) {
-              validator[name] = validator[name].integer()
-            }
-            break
-          }
-          case "bool": {
-            validator[name] = Yup.bool()
-            break
-          }
-          case "email": {
-            validator[name] = Yup.string().email()
-            break
-          }
-          case "date": {
-            validator[name] = Yup.date()
-            break
-          }
-          case "url": {
-            validator[name] = Yup.string().url()
-            break
-          }
-          default: {
-            validator[name] = Yup.string()
-            break
-          }
+      switch (type) {
+        case "string": {
+          validator[name] = Yup.string()
+          break
         }
-        if (nullable) {
-          validator[name] = validator[name].nullable()
+        case "number": {
+          validator[name] = Yup.number()
+          if (min) {
+            validator[name] = validator[name].min(min)
+          }
+          if (max) {
+            validator[name] = validator[name].max(max)
+          }
+          if (integer) {
+            validator[name] = validator[name].integer()
+          }
+          break
         }
-        if (required) {
-          validator[name] = validator[name].required("Denne må du ha")
+        case "bool": {
+          validator[name] = Yup.bool()
+          break
         }
-        return validator
-      },
-      {}
-    )
+        case "email": {
+          validator[name] = Yup.string().email()
+          break
+        }
+        case "date": {
+          validator[name] = Yup.date()
+          break
+        }
+        case "url": {
+          validator[name] = Yup.string().url()
+          break
+        }
+        default: {
+          validator[name] = Yup.string()
+          break
+        }
+      }
+      if (nullable) {
+        validator[name] = validator[name].nullable()
+      }
+      if (required) {
+        validator[name] = validator[name].required("Denne må du ha")
+      }
+      return validator
+    }, {})
     const validationSchema = Yup.object().shape(validationShape)
     const { Container } = this.props
     return (
