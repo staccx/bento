f58aca5b0fdf94365c0229b798803724a901ef12
Espen Th Sæverud <espensaeverud@gmail.com>
2019-01-31 13:15:39 +0100

    feat(sanity): add fallback to text if not array

--- packages/internal/sanity/src/components/SanityBlockContent.js
+++ packages/internal/sanity/src/components/SanityBlockContent.js
@@ -1,8 +1,11 @@
 import React from "react"
 import BlockContent from "@sanity/block-content-to-react"
 
-const SanityBlockContent = ({ blocks, serializers }) => (
-  <BlockContent blocks={blocks} serializers={serializers} />
-)
+const SanityBlockContent = ({ blocks, serializers }) => {
+  if (!Array.isArray(blocks)) {
+    return blocks
+  }
+  return <BlockContent blocks={blocks} serializers={serializers} />
+}
 
 export default SanityBlockContent
