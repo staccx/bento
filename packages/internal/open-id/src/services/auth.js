a11011fe0500f0a28d10c205ac17868e87e08f12
Espen Th Sæverud <espensaeverud@gmail.com>
2019-03-01 09:41:23 +0100

    feat(open-id): add signinSilentCallback to export

--- packages/internal/open-id/src/services/auth.js
+++ packages/internal/open-id/src/services/auth.js
@@ -2,11 +2,13 @@ import { Log, UserManager } from "oidc-client"
 
 class Auth {
   constructor(config, debug = false) {
+    Log.logger = console
+    Log.level = debug ? Log.DEBUG : Log.INFO
+
     console.log("creating a new usermanager")
     this.userManager = new UserManager(config)
 
-    Log.logger = console
-    Log.level = debug ? Log.DEBUG : Log.INFO
+
   }
 
   getUser() {
