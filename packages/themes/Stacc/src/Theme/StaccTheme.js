8199807c9948c9f775559aed69257c69e2a322a4
Espen Th Sæverud <espensaeverud@gmail.com>
2019-03-27 14:10:07 +0100

    feat(stacc-theme): add boxShadow

--- packages/themes/Stacc/src/Theme/StaccTheme.js
+++ packages/themes/Stacc/src/Theme/StaccTheme.js
@@ -111,6 +111,8 @@ const fontFamily = {
   mono: "IBM Plex Mono"
 }
 
+const boxShadow = `0px 3px 10px rgba(0,0,0,0.2)`
+
 const theme = new theming.Theme(
   {},
   {
@@ -124,7 +126,8 @@ const theme = new theming.Theme(
     borderRadius,
     headerHeight,
     fontFamily,
-    spacing
+    spacing,
+    boxShadow
   }
 )
   .add(Button)
