4bca3f762ba1146dc3692a59d42954f5b435b888
robinsandborg <robin.sandborg@gmail.com>
2019-04-11 12:48:46 +0200

    style(nordea-theme): add more styles

--- packages/themes/Nordea/src/Theme/theme.Headings.js
+++ packages/themes/Nordea/src/Theme/theme.Headings.js
@@ -36,6 +36,16 @@ export const HeadingStyling = theming.createVariants(
       color: ${theming.color("b4")};
       font-size: ${theming.font.h1};
       font-weight: ${theming.fontWeight.normal};
+    `,
+    newsList: css`
+      font-size: ${theming.font.base};
+      color: ${theming.color.text};
+      font-weight: normal;
+    `,
+    contact: css`
+      font-size: ${theming.font.h4};
+      font-weight: normal;
+      color: ${theming.color("b5")};
     `
   },
   Heading.themeProps.heading
