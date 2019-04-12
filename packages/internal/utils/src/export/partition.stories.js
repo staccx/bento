df72824c9001edd6d1e9916fe476f19fd2699472
Espen Th Sæverud <espensaeverud@gmail.com>
2019-04-12 14:07:07 +0200

    feat(utils): stories for partition

---
+++ packages/internal/utils/src/export/partition.stories.js
@@ -0,0 +1,23 @@
+import React from "react"
+import { storiesOf } from "@storybook/react"
+import partition from "./partition"
+import { Text } from "./__storyComponents"
+
+const data = new Array(10).fill(0).map(i => ({
+  isVisible: Math.random() > 0.5
+}))
+
+storiesOf("Functions|Utils/Partition", module).add("Simple", () => {
+  const [visible, notVisible] = partition(data, item => item.isVisible)
+  console.log(data)
+  return (
+    <div>
+      {data.map(item => (
+        <div>isVisible: {item.isVisible ? "yes" : "no"}</div>
+      ))}
+      <Text>Array length: {data.length}</Text>
+      <Text>Visible: {visible.length}</Text>
+      <Text>Not Visible: {notVisible.length}</Text>
+    </div>
+  )
+})
