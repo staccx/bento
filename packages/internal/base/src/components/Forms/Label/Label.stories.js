1807e900c794038e1a5ff9f439c496a368f8c7da
Thomase <Thomase@stacc.com>
2019-05-03 16:22:54 +0200

    feat(storybook): stories for AccountInput & Label

---
+++ packages/internal/base/src/components/Forms/Label/Label.stories.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { storiesOf } from "@storybook/react"
+import Label from "./Label"
+import docs from "./Label.md"
+
+storiesOf("components|base/Label", module)
+  .add("No props", () => <Label>This is a label</Label>, {
+    info: {
+      text: docs
+    }
+  })
+  .add("with props", () => <Label className={"a Label"}>This is label</Label>)
