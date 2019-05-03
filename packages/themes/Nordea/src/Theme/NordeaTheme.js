e89076f2595ad6537900f4aba122fabf320683dc
robinsandborg <robin.sandborg@gmail.com>
2019-05-03 13:04:04 +0200

    style(nordea-theme): add NordeSansLarge

--- packages/themes/Nordea/src/Theme/NordeaTheme.js
+++ packages/themes/Nordea/src/Theme/NordeaTheme.js
@@ -172,7 +172,7 @@ const fontWeight = {
 }
 
 const fontFamily = {
-  heading: "NordeaSansSmall",
+  heading: "NordeaSansLarge",
   body: "NordeaSansSmall"
 }
 
--- packages/themes/Nordea/src/Theme/reset.js
+++ packages/themes/Nordea/src/Theme/reset.js
@@ -6,13 +6,29 @@ export default theming.createGlobal({
   fonts: css`
     @font-face {
       font-family: "NordeaSansSmall";
-      src: url("/fonts/NordeaSansSmall-Regular.woff") format("woff");
+      src: url("/fonts/NordeaSansSmallWeb-Regular.woff2") format("woff2"),
+        url("/fonts/NordeaSansSmallWeb-Regular.woff2") format("woff");
       font-weight: 400;
     }
     @font-face {
       font-family: "NordeaSansSmall";
-      src: url("/fonts/NordeaSansSmall-Bold.woff") format("woff");
+      src: url("/fonts/NordeaSansSmallWeb-Bold.woff2") format("woff2"),
+        url("/fonts/NordeaSansSmallWeb-Bold.woff2") format("woff");
       font-weight: 700;
+    }
+
+    @font-face {
+      font-family: "NordeaSansLarge";
+      src: url("/fonts/NordeaSansLargeWeb-Bold.woff2") format("woff2"),
+        url("/fonts/NordeaSansLargeWeb-Bold.woff2") format("woff");
+      font-weight: 700;
+    }
+
+    @font-face {
+      font-family: "NordeaSansLarge";
+      src: url("/fonts/NordeaSansLargeWeb-Regular.woff2") format("woff2"),
+        url("/fonts/NordeaSansLargeWeb-Regular.woff2") format("woff");
+      font-weight: 400;
     }
 
     a {
