3d22be388e32d9ec5bf8c9a180d1b702f718d5ca
Espen Th Sæverud <espensaeverud@gmail.com>
2019-05-08 09:48:41 +0200

    fix(sanity): fix export

--- packages/internal/sanity/src/export.js
+++ packages/internal/sanity/src/export.js
@@ -1,4 +1,5 @@
-import { SanityProvider, useSanity } from "./components/SanityProvider"
+import { default as SanityProvider } from "./components/SanityProvider"
+import useSanity from "./components/useSanity"
 import { default as SanityImage } from "./components/SanityImage"
 import { default as SanityBlockContent } from "./components/SanityBlockContent"
 import { serializers, extractTextFromBlocks } from "./data/serializers"
