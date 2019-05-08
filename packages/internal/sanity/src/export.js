bc3f8590ea4edc4f5ca4dfc45fea86f9c5ac6f47
Espen Th Sæverud <espensaeverud@gmail.com>
2019-05-08 14:59:02 +0200

    feat(sanity): exporet list, query, doc

--- packages/internal/sanity/src/export.js
+++ packages/internal/sanity/src/export.js
@@ -1,6 +1,9 @@
 import { default as SanityProvider } from "./components/SanityProvider"
 import useSanity from "./components/useSanity"
 import { default as SanityImage } from "./components/SanityImage"
+import { default as SanityList } from "./components/SanityList"
+import { default as SanityQuery } from "./components/SanityQuery"
+import { default as SanityDocument } from "./components/SanityDocument"
 import { default as SanityBlockContent } from "./components/SanityBlockContent"
 import { serializers, extractTextFromBlocks } from "./data/serializers"
 
@@ -8,6 +11,9 @@ export {
   SanityProvider,
   useSanity,
   SanityImage,
+  SanityList,
+  SanityQuery,
+  SanityDocument,
   SanityBlockContent,
   serializers,
   extractTextFromBlocks
