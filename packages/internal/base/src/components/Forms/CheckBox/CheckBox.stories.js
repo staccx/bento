dd641c1804492677303e55ef1c52851bd70cb1ed
Thomase <Thomase@stacc.com>
2019-05-08 10:58:42 +0200

    feat(storybook): edit stories for Odometer & CheckBox

--- packages/internal/base/src/components/Forms/CheckBox/CheckBox.stories.js
+++ packages/internal/base/src/components/Forms/CheckBox/CheckBox.stories.js
@@ -6,43 +6,25 @@ storiesOf("Components|Base/Forms/Checkbox", module)
   .add("No props", () => <CheckBox>CheckBox</CheckBox>)
   .add("ID", () => <CheckBox id={"ID"}>Checkbox</CheckBox>)
   .add("Disabled true", () => (
-    <CheckBox id={"ID"} disabled={"true"}>
+    <CheckBox id={"ID"} disabled>
       CheckBox
     </CheckBox>
   ))
-  .add("With className", () => (
-    <CheckBox id={"ID"} className={"Name"}>
+  .add("DefaultChecked", () => (
+    <CheckBox id={"ID"} defaultChecked>
       CheckBox
     </CheckBox>
   ))
-  .add("Default Checked", () => (
-    <CheckBox id={"ID"} defaultChecked={"true"}>
-      CheckBox
-    </CheckBox>
-  ))
-  .add("Default Checked false", () => (
-    <CheckBox id={"3242343"} defaultChecked={"false"}>
-      CheckBox
-    </CheckBox>
-  ))
-  .add("Checked with id and children", () => (
-    <CheckBox id={"Something"} checkIcon={"true"}>
-      CheckBox
-    </CheckBox>
-  ))
-  .add("input", () => (
-    <CheckBox id={"ID"} input={"Some input"}>
+  .add("DefaultChecked false", () => (
+    <CheckBox id={"3242343"} defaultChecked={false}>
       CheckBox
     </CheckBox>
   ))
   .add("log", () => (
-    <CheckBox id={"ID"} onChange={() => console.log("test")}>
+    <CheckBox
+      id={"ID"}
+      onChange={e => console.log("test", e.target.value, e.target.id)}
+    >
       CheckBox
-    </CheckBox>
-  ))
-  .add("test", () => (
-    <CheckBox group="Signer" id={"1244123"} onChange={() => null}>
-      {" "}
-      Check off here{" "}
     </CheckBox>
   ))
