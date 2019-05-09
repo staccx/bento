a729b82dbf9cc35f46ca10935364dcc1832d69f1
Joakim Carlsen <joakimc@stacc.com>
2019-05-09 21:03:10 +0200

    style(nordea-theme): add/modify checkbox filter variant

--- packages/themes/Nordea/src/Theme/theme.Checkbox.js
+++ packages/themes/Nordea/src/Theme/theme.Checkbox.js
@@ -1,17 +1,64 @@
 import { css } from "styled-components"
 import { CheckBox, theming } from "@staccx/base"
 
+export const CheckBoxInputStyling = theming.createVariants(
+  {
+    filter: css`
+      :focus ~ label::before {
+        outline: 3px solid ${theming.color("b1")};
+        outline-offset: 0;
+      }
+    `
+  },
+  CheckBox.themeProps.input
+)
+
 export const CheckBoxLabelStyling = theming.createVariants(
   {
     filter: css`
+      min-height: 0;
       padding-top: 0;
       padding-bottom: 0;
-      min-height: 0;
-
-      &::before {
-        top: 0;
+      line-height: 24px;
+      ::after {
+        content: "";
+        display: none;
+        width: 14px;
+        height: 8px;
+        position: absolute;
+        bottom: 6px;
+        left: 10px;
+        border: 3px solid;
+        border-top: 0;
+        border-right: 0;
+        transform-origin: bottom left;
+        transform: rotate(-45deg) scale(1);
       }
     `
   },
   CheckBox.themeProps.label
+)
+
+export const CheckBoxLabelBeforeStyling = theming.createVariants(
+  {
+    filter: css`
+      top: 0;
+      border-color: ${theming.color("text")};
+    `
+  },
+  CheckBox.themeProps.labelBefore
+)
+
+export const CheckBoxLabelCheckedStyling = theming.createVariants(
+  {
+    filter: css`
+      > svg {
+        display: none;
+      }
+      ::after {
+        display: block;
+      }
+    `
+  },
+  CheckBox.themeProps.labelChecked
 )
