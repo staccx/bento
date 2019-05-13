840889b6b3e33aba40fbcdf5155d5d6126966fce
Joakim Carlsen <joakimc@stacc.com>
2019-05-13 15:03:52 +0200

    style(nordea-theme): add smallHeading prop

--- packages/themes/Nordea/src/Theme/theme.Headings.js
+++ packages/themes/Nordea/src/Theme/theme.Headings.js
@@ -43,14 +43,15 @@ export const HeadingStyling = theming.createVariants(
       font-weight: ${theming.fontWeight.normal};
     `,
     layoutBox: css`
-      font-size: ${theming.font.h4};
+      font-size: ${p => (p.smallHeading ? theming.font.h5 : theming.font.h4)};
       font-weight: ${theming.fontWeight.normal};
       color: ${theming.color("g3")};
       line-height: 1.3;
       margin-bottom: 0;
 
       @media (min-width: 720px) {
-        font-size: ${theming.font.h2};
+        font-size: ${p =>
+          p.smallHeading ? theming.font.input : theming.font.h2};
         line-height: 1;
         margin-top: -6px;
       }
