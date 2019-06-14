248129ea17287182689f58e4945a07349382f0b4
Joakim Carlsen <joakimc@stacc.com>
2019-06-14 14:28:01 +0200

    feat(component-testing): component testing → WidowFix

--- packages/apps/component-testing/src/theme/Theme.js
+++ packages/apps/component-testing/src/theme/Theme.js
@@ -1,5 +1,2 @@
-import { StaccTheme } from "@staccx/stacc-theme"
-import LoadingVariants from "./Loading.Variants"
-const theme = StaccTheme.add([LoadingVariants])
-console.log(theme)
-export default theme
+import { FunduTheme } from "@staccx/fundu-theme"
+export default FunduTheme
