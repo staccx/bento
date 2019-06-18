db706ef5cf95d21ef1b8856dfcfaf493b63d3f74
Joakim Carlsen <joakimc@stacc.com>
2019-06-18 09:29:22 +0200

    feat(component-testing): just the last changes

--- packages/internal/component-testing/src/App.js
+++ packages/internal/component-testing/src/App.js
@@ -76,8 +76,41 @@ const App = () => {
   )
 }
 
+// const StyledBreadcrumb = styled(Breadcrumb)`
+//   > li {
+//     background-color: #ddd;
+//     color: ${theming.color.primary};
+//     margin-right: 5px;
+//     flex-grow: 1;
+//     &:first-child {
+//       a {
+//         padding-left: 10px;
+//       }
+//     }
+//     &:last-child {
+//       background-color: transparent;
+//       color: ${theming.color.primary};
+//       > div {
+//         padding-right: 10px;
+//         border-color: transparent;
+//       }
+//     }
+//     a,
+//     &:last-item > div {
+//       border-color: #ddd;
+//     }
+//     &:hover a {
+//       background-color: ${theming.color.primary};
+//     }
+//     &:not(:last-child):hover {
+//       background-color: ${theming.color.primary};
+//       color: ${theming.color.white};
+//     }
+//   }
+// `
+
 const StyledBreadcrumb = styled(Breadcrumb)`
-  --item-distance: 20px;
+  --item-distance: 5px;
   --end-padding: 10px;
   --full-width: 1;
   --col-bg: #ddd;
