9211432e6b3b36a02920ad422c7480b0f9ca07cf
Espen Th Sæverud <espensaeverud@gmail.com>
2019-06-19 20:48:23 +0200

    feat(sanity-plugin-i18n): add support for namespacing

--- packages/internal/sanity-plugin-i18n/lib/supportedLanguages.js
+++ packages/internal/sanity-plugin-i18n/lib/supportedLanguages.js
@@ -1,22 +1,24 @@
-import languages from "part:@staccx/i18n/languages?";
+import languages from "part:@staccx/i18n/languages?"
 
 function _objectSpread(target) {
   for (var i = 1; i < arguments.length; i++) {
-    var source = arguments[i] != null ? arguments[i] : {};
-    var ownKeys = Object.keys(source);
+    var source = arguments[i] != null ? arguments[i] : {}
+    var ownKeys = Object.keys(source)
 
     if (typeof Object.getOwnPropertySymbols === "function") {
-      ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function (sym) {
-        return Object.getOwnPropertyDescriptor(source, sym).enumerable;
-      }));
+      ownKeys = ownKeys.concat(
+        Object.getOwnPropertySymbols(source).filter(function(sym) {
+          return Object.getOwnPropertyDescriptor(source, sym).enumerable
+        })
+      )
     }
 
-    ownKeys.forEach(function (key) {
-      _defineProperty(target, key, source[key]);
-    });
+    ownKeys.forEach(function(key) {
+      _defineProperty(target, key, source[key])
+    })
   }
 
-  return target;
+  return target
 }
 
 function _defineProperty(obj, key, value) {
@@ -26,50 +28,62 @@ function _defineProperty(obj, key, value) {
       enumerable: true,
       configurable: true,
       writable: true
-    });
+    })
   } else {
-    obj[key] = value;
+    obj[key] = value
   }
 
-  return obj;
+  return obj
 }
 
-const supportedLanguages = languages || [{
-  id: "nb",
-  title: "Norwegian",
-  isDefault: true
-}];
-export const getFields = (type, o = null) => supportedLanguages.map(lang => _objectSpread({
-  title: lang.title,
-  name: lang.id,
-  type: type
-}, o && {
-  of: [o]
-}, {
-  fieldset: lang.isDefault ? null : "translations"
-}));
-export const getPreview = () => ({
-  select: {
-    title: supportedLanguages[0].id
-  },
-
-  prepare(selection) {
-    const {
-      _type,
-      title
-    } = selection;
-    return {
-      title,
-      subtitle: _type
-    };
+var supportedLanguages = languages || [
+  {
+    id: "nb",
+    title: "Norwegian",
+    isDefault: true
   }
-
-});
-const fieldsets = [{
-  title: "Translations",
-  name: "translations",
-  options: {
-    collapsable: true
+]
+export var getFields = function getFields(type) {
+  var o =
+    arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : null
+  return supportedLanguages.map(function(lang) {
+    return _objectSpread(
+      {
+        title: lang.title,
+        name: lang.id,
+        type: type
+      },
+      o && {
+        of: [o]
+      },
+      {
+        fieldset: lang.isDefault ? null : "translations"
+      }
+    )
+  })
+}
+export var getPreview = function getPreview() {
+  return {
+    select: {
+      title: supportedLanguages[0].id
+    },
+    prepare: function prepare(selection) {
+      var _type = selection._type
+      var title = selection.title
+      return {
+        title: title,
+        subtitle: _type
+      }
+    }
   }
-}];
-export { supportedLanguages, fieldsets };
+}
+var fieldsets = [
+  {
+    title: "Translations",
+    name: "translations",
+    options: {
+      collapsable: true
+    }
+  }
+]
+export { supportedLanguages, fieldsets }
