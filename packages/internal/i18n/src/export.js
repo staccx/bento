8dea3b2dcc93c8cb41ce63fb2d97fb18d2a0c18c
Espen Th Sæverud <espensaeverud@gmail.com>
2019-06-19 20:41:49 +0200

    feat(base): add i18n to base. deleted i18n package

--- packages/internal/i18n/src/export.js
+++
@@ -1,7 +0,0 @@
-import { default as formatCurrency } from "./plugins/format.currency"
-export { default as translate } from "./translate"
-export { default as convert } from "./convert"
-
-export const plugins = {
-  formatCurrency
-}
