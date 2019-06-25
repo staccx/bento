19dc0917c9295590473b331ecb77da6763d37d75
Espen Th Sæverud <espensaeverud@gmail.com>
2019-06-25 11:20:59 +0200

    feat(json-schema): deleted

--- packages/babel.config.js
+++ packages/babel.config.js
@@ -1,18 +1,17 @@
 module.exports = {
-  presets: [
-    [
-      "@babel/preset-env",
-      {
-        modules: false
-      }
-    ],
-    "@babel/preset-react"
-  ],
+  presets: ["@babel/preset-env", "@babel/preset-react"],
   plugins: [
     "babel-plugin-styled-components",
     "@babel/plugin-proposal-export-default-from",
     "@babel/plugin-syntax-dynamic-import",
     ["@babel/plugin-proposal-decorators", { legacy: true }],
     ["@babel/plugin-proposal-class-properties", { loose: true }]
+  ],
+  ignore: [
+    "**/src/**/*.test.js",
+    "**/src/**/*.spec.js",
+    "**/src/**/*.stories.js",
+    "**/src/**/*.md",
+    "**/src/**/*.preview.js"
   ]
 }
--- packages/internal/json-schema/src/components/Custom.Errors.js
+++
@@ -1,9 +0,0 @@
-export default errors => {
-  return errors.map(error => {
-    console.log(error)
-    if (error.params.format && error.params.format === "email") {
-      error.message = "Email!!!"
-    }
-    return error
-  })
-}
