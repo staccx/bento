2e9d80f1d654bccbfb8dfde7e8fd4c921b306e58
Thomase <thomase@stacc.com>
2019-10-25 10:17:00 +0200

    feat(storybook): added .md documentation for formatCurrency

--- packages/internal/formatting/src/currency.stories.js
+++ packages/internal/formatting/src/currency.stories.js
@@ -2,22 +2,77 @@ import React from "react"
 import { storiesOf } from "@storybook/react"
 import { formatCurrency } from "./currency"
 import { Text as CurrencyText } from "./__storyComponents"
+import docs from "./formatCurrency.md"
 
 storiesOf("Functions/Formatting/Currency", module)
-  .add("No options", () => <CurrencyText>{formatCurrency(1000)}</CurrencyText>)
-  .add("Thousand separator", () => (
-    <CurrencyText>{formatCurrency(1000, { thousand: "." })}</CurrencyText>
-  ))
-  .add("Currency symbol", () => (
-    <CurrencyText>{formatCurrency(1000, { symbol: "$" })}</CurrencyText>
-  ))
-  .add("Format", () => (
-    <CurrencyText>
-      {formatCurrency(1000, { symbol: "$", format: "$s  $v" })}
-    </CurrencyText>
-  ))
-  .add("Norwegian", () => (
-    <CurrencyText>
-      {formatCurrency(100000, { symbol: "kr", format: "$s  $v", suffix: ",-" })}
-    </CurrencyText>
-  ))
+  .add(
+    "No options",
+    () => {
+      return <CurrencyText>{formatCurrency(1000)}</CurrencyText>
+    },
+    {
+      info: {
+        text: docs
+      }
+    }
+  )
+  .add(
+    "Thousand separator",
+    () => {
+      return (
+        <CurrencyText>{formatCurrency(1000, { thousand: "." })}</CurrencyText>
+      )
+    },
+    {
+      info: {
+        text: docs
+      }
+    }
+  )
+  .add(
+    "Currency symbol",
+    () => {
+      return (
+        <CurrencyText>{formatCurrency(1000, { symbol: "$" })}</CurrencyText>
+      )
+    },
+    {
+      info: {
+        text: docs
+      }
+    }
+  )
+  .add(
+    "Format",
+    () => {
+      return (
+        <CurrencyText>
+          {formatCurrency(1000, { symbol: "$", format: "$s  $v" })}
+        </CurrencyText>
+      )
+    },
+    {
+      info: {
+        text: docs
+      }
+    }
+  )
+  .add(
+    "Norwegian",
+    () => {
+      return (
+        <CurrencyText>
+          {formatCurrency(100000, {
+            symbol: "kr",
+            format: "$s  $v",
+            suffix: ",-"
+          })}
+        </CurrencyText>
+      )
+    },
+    {
+      info: {
+        text: docs
+      }
+    }
+  )
