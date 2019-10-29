aca2664d83fc3a42e6b35463fa64578c4d4db2b8
Thomase <thomase@stacc.com>
2019-10-29 14:56:28 +0100

    feat(storybook): better stories for select

--- packages/internal/base/src/components/Forms/Select/Select.stories.js
+++ packages/internal/base/src/components/Forms/Select/Select.stories.js
@@ -1,14 +1,32 @@
-import React from "react"
+import React, { useState } from "react"
 import { storiesOf } from "@storybook/react"
 import Select from "./Select"
 
-storiesOf("components|base/Forms/Select", module).add("default", () => {
-  return (
-    <Select
-      items={["Red", "Blue", "Orange"]}
-      onChange={() => null}
-      label={"Colors"}
-      selectedItem="Blue"
-    />
-  )
-})
+storiesOf("components|base/Forms/Select", module)
+  .add("console.log value", () => {
+    return (
+      <Select
+        items={["red", "blue", "orange"]}
+        onChange={value => console.log(value)}
+        label={"Colors"}
+        PlaceHolderLabel="Select some colors..."
+        SelectedItem="Blue"
+      />
+    )
+  })
+
+  .add("assign value to state", () => {
+    const [select, setSelect] = useState()
+    return (
+      <div>
+        <Select
+          items={["Red", "Blue", "Orange"]}
+          onChange={value => setSelect(value)}
+          label={"Colors"}
+          selectedItem="Blue"
+          PlaceHolderLabel="Select some colors..."
+        />
+        <p>You have selected:{select}</p>
+      </div>
+    )
+  })
