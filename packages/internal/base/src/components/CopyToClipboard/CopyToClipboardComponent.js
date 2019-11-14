e05696abc9901f7f52868006bfe87d4d35fb3ab7
Thomase <thomase@stacc.com>
2019-11-14 10:57:20 +0100

    feat(variant): add variant to CopyToClipboardComponent

--- packages/internal/base/src/components/CopyToClipboard/CopyToClipboardComponent.js
+++ packages/internal/base/src/components/CopyToClipboard/CopyToClipboardComponent.js
@@ -7,39 +7,40 @@ import Check from "../Icons/Check"
 import ThemeComponent from "../Theme/ThemeComponent"
 import { applyVariants } from "../../theming"
 import { themePropTypes, commonPropTypes } from "../../constants/themeContants"
-
-const CopyToClipboardComponent = ({ children, copyText }) => {
+const CopyToClipboardComponent = ({
+  children,
+  copyText,
+  variant,
+  ...restProps
+}) => {
   const [copied, setCopied] = useState(false)
-
   if (!copyText) {
     throw new Error("Copy text is required")
   }
-
   const handleClick = copy => {
     copy(copyText)
-
     setCopied(true)
     setTimeout(() => {
       setCopied(false)
     }, 1000)
   }
-
   return (
-    <CopyToClipboard children={children}>
+    <CopyToClipboard children={children} variant={variant}>
       {({ copy }) => (
         <ClipboardWrapper
+          variant={variant}
           onClick={() => {
             handleClick(copy)
           }}
+          {...restProps}
         >
           {children}
-          {copied ? <Copied /> : <Copy />}
+          {copied ? <Copied variant={variant} /> : <Copy variant={variant} />}
         </ClipboardWrapper>
       )}
     </CopyToClipboard>
   )
 }
-
 CopyToClipboardComponent.themeProps = {
   wrapper: {
     name: "CopyToClipboardComponent_Wrapper",
@@ -62,14 +63,12 @@ CopyToClipboardComponent.themeProps = {
     type: themePropTypes.component
   }
 }
-
 const ClipboardWrapper = styled.button`
   &:hover {
     outline: none;
   }
   ${applyVariants(CopyToClipboardComponent.themeProps.wrapper)};
 `
-
 const CopiedComponent = ({ ...props }) => (
   <ThemeComponent
     tagName={CopyToClipboardComponent.themeProps.copiedComponent}
@@ -77,7 +76,6 @@ const CopiedComponent = ({ ...props }) => (
     {...props}
   />
 )
-
 const CopyComponent = ({ ...props }) => (
   <ThemeComponent
     tagName={CopyToClipboardComponent.themeProps.copyComponent}
@@ -85,18 +83,14 @@ const CopyComponent = ({ ...props }) => (
     {...props}
   />
 )
-
 export const Copy = styled(CopyComponent)`
   ${applyVariants(CopyToClipboardComponent.themeProps.icon)};
 `
-
 export const Copied = styled(CopiedComponent)`
   ${applyVariants(CopyToClipboardComponent.themeProps.icon)};
 `
-
 CopyToClipboardComponent.propTypes = {
   children: commonPropTypes.children.isRequired,
   copyText: PropTypes.string.isRequired
 }
-
 export default CopyToClipboardComponent
--- packages/internal/base/src/components/Forms/CheckBox/CheckBox.md
+++ packages/internal/base/src/components/Forms/CheckBox/CheckBox.md
@@ -1,4 +1,4 @@
-
+```
 <div>
   <CheckBox
     group="signer"
