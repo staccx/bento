16b6cb881af7fe233b071c47c63dcfcd25c87d3d
Espen Th Sæverud <espensaeverud@gmail.com>
2019-12-18 18:09:01 +0100

    feat: updated and husky

---
+++ babel.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  presets: ["@babel/preset-env", "@babel/preset-react", "react-app"],
+  plugins: [],
+  ignore: [
+    "src/**/*.test.js",
+    "src/**/*.spec.js",
+    "src/**/*.stories.js",
+    "src/**/*.md",
+    "src/**/*.preview.js"
+  ]
+}
