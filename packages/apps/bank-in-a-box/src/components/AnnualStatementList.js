138172c368f31082fdaeb66010eb0eaea8595ee8
Torgeir Skjøtskift <torgeirs@stacc.com>
2020-02-11 13:30:45 +0100

    fix(bank-in-a-box): add missing translation

--- packages/apps/bank-in-a-box/src/components/AnnualStatementList.js
+++ packages/apps/bank-in-a-box/src/components/AnnualStatementList.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Heading, SplitListItem, Text, Alert } from "@staccx/base"
 import { TranslatedText } from "@staccx/i18n"
-import { formatCurrency, removeWhitespace } from "@staccx/formatting"
+import { formatCurrency, removeWhitespace, dashIt } from "@staccx/formatting"
 
 const AnnualStatementList = ({ statement }) => {
   if (!statement) {
@@ -13,18 +13,24 @@ const AnnualStatementList = ({ statement }) => {
       </Alert>
     )
   }
-  return statement.items.map(item => (
-    <React.Fragment key={item.name}>
-      <SplitListItem variant="annualStatementListItem" heading>
-        <Heading level="2" variant="annualStatmentHeading">
-          {item.name}
-        </Heading>
-      </SplitListItem>
-      <SplitListItem variant="annualStatementListItem" emphasize>
-        <Text>{item.description}</Text>
-        <div>{formatCurrency(parseInt(removeWhitespace(item.value), 10))}</div>
-      </SplitListItem>
-    </React.Fragment>
-  ))
+  return statement.items.map(item => {
+    return (
+      <React.Fragment key={item.name}>
+        <SplitListItem variant="annualStatementListItem" heading>
+          <Heading level="2" variant="annualStatmentHeading">
+            <TranslatedText i18nKey={dashIt(item.name)}>
+              {item.name}
+            </TranslatedText>
+          </Heading>
+        </SplitListItem>
+        <SplitListItem variant="annualStatementListItem" emphasize>
+          <Text>{item.description}</Text>
+          <div>
+            {formatCurrency(parseInt(removeWhitespace(item.value), 10))}
+          </div>
+        </SplitListItem>
+      </React.Fragment>
+    )
+  })
 }
 export default AnnualStatementList
