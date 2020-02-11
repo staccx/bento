89f856438cd2a1689559e696ad1534670cf48133
Thomase <thomase@stacc.com>
2020-02-11 14:56:11 +0100

    fix(storybook): something is wonky with hidden: true for canvas

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -5,7 +5,7 @@ addons.setConfig({
     // the order of the tabs is configured by the order here
     'storybook/docs/panel': 'Documentation', // the configuration is either an object or a title string
     canvas: {
-      hidden: true
+      hidden: false
     }, // canvas is here simply listed so its place as first tab
   },
 });
