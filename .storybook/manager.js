2347065bddf9a10f21ae2ed81a0158408fb9d76f
Thomase <thomase@stacc.com>
2020-02-13 12:09:07 +0100

    feat(storybook): intro|Bento now first page

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,5 +1,6 @@
 import { addons } from '@storybook/addons';
 
+
 addons.setConfig({
   previewTabs: {
     // the order of the tabs is configured by the order here
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,5 +2,11 @@ import React from "react";
 import { addDecorator, addParameters } from '@storybook/react';
 import {ThemeProvider} from "styled-components";
 import baseTheme from "../src/theming/baseTheme"
-
 addDecorator(story => <ThemeProvider theme={baseTheme}>{story()}</ThemeProvider>)
+addParameters({
+  options: {
+    storySort: (a, b) =>
+      a[1].kind === "Intro|Bento" ? 0 : a[1].id.localeCompare(b[1].id, undefined, {
+        numeric: true }),
+  },
+});
