fe20be73c2a6662ceede24992d097d3ca5376483
Thomase <thomase@stacc.com>
2020-02-13 13:18:35 +0100

    feat(storybook): added Storybook theme

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -8,5 +8,6 @@ addParameters({
     storySort: (a, b) =>
       a[1].kind === "Intro|Bento" ? 0 : a[1].id.localeCompare(b[1].id, undefined, {
         numeric: true }),
+    name: "Bento"
   },
 });
