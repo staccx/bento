10507414407736e20eefb931a010d7dabde1286f
Thomase <thomase@stacc.com>
2020-02-20 10:29:14 +0100

    fix(storybook): move register adddon functions to manager app

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,9 +1,37 @@
 import React from "react"
-import { addons } from "@storybook/addons"
+import { addons, types } from "@storybook/addons"
 import StoryBookTheme from "./bentoStorybookTheme"
 import "./withThemes"
 import "./tools/variants"
-import "./tools/rerender"
+import { ThemeSwitcher } from "./withThemes"
+import { Button } from "@storybook/components"
+import { FORCE_RE_RENDER } from "@storybook/core-events"
+import { Variants, VARIANTS_TOOL_NAME } from "./tools/variants"
+
+addons.register("storybook/theme-switcher", api => {
+  addons.addPanel("storybook/theme-switcher", {
+    title: "theme-switcher",
+    type: types.TOOL,
+    render: () => <ThemeSwitcher api={api} />
+  })
+})
+
+addons.register(VARIANTS_TOOL_NAME, api => {
+  addons.addPanel(VARIANTS_TOOL_NAME, {
+    title: "variant-switcher",
+    type: types.TOOL,
+    render: () => <Variants api={api}/>
+  });
+});
+
+addons.register("bento/rerender", api => {
+  addons.addPanel("bento/rerender", {
+    title: "rerender",
+    type: types.TOOL,
+    render: () => <Button primary onClick={() => api.emit(FORCE_RE_RENDER)}>Rerender</Button>
+  });
+});
+
 
 addons.setConfig({
   theme: StoryBookTheme,
