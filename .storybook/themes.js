fe33d84279328617d41caa4bf2c7d51ba0d13403
Robin Sandborg <robin.sandborg@gmail.com>
2020-02-20 12:54:25 +0100

    feat(storybook): remove test-theme

--- .storybook/themes.js
+++ .storybook/themes.js
@@ -1,6 +1,5 @@
 import baseTheme from "../src/theming/themes/baseTheme"
-import testTheme from "../src/theming/themes/testTheme"
 import violetTendencies from "../src/theming/themes/storybook/violet-tendencies/violetTendenciesTheme"
 import whiteCollar from "../src/theming/themes/storybook/white-collar/whiteCollarTheme"
 
-export default [baseTheme, testTheme, violetTendencies, whiteCollar]
+export default [baseTheme, violetTendencies, whiteCollar]
