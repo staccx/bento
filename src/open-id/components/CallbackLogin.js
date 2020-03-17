6328f25e98cc74c771340b3c33df4c504e17fe63
Espen Th Sæverud <espensaeverud@gmail.com>
2020-03-17 14:52:12 +0100

    feat(openid): add useCallbackLogin. refactor Callbacklogin

--- src/open-id/components/CallbackLogin.js
+++ src/open-id/components/CallbackLogin.js
@@ -1,8 +1,7 @@
-import React from "react"
 import PropTypes from "prop-types"
 import { useOpenId } from ".."
 import { logger } from "./OpenId"
-import { useTimer } from "../../hooks"
+import { useCallbackLogin } from "../hooks/useCallbackLogin"
 
 const CallbackLogin = ({
   minTimeBeforeRedirect,
@@ -10,44 +9,33 @@ const CallbackLogin = ({
   onFailure,
   element
 }) => {
-  const {
-    userManager,
-    afterLogin,
-    onError,
-    reloadParentAfterLogin
-  } = useOpenId()
+  const { afterLogin, onError, reloadParentAfterLogin } = useOpenId()
 
-  const ready = useTimer(minTimeBeforeRedirect)
   const _window = reloadParentAfterLogin ? window.parent : window
 
-  React.useEffect(() => {
-    logger.debug("CallbackLogin")
-    if (userManager && ready) {
-      logger.debug("SigningRedirectCallback being called")
-      userManager
-        .signinRedirectCallback()
-        .then(result => {
-          logger.info("SigninRedirectCallback successful", result)
-          if (onSuccess) {
-            logger.info("Using success callback", afterLogin)
-            onSuccess(result)
-          } else {
-            logger.info("Replacing location with", afterLogin)
-            _window.location.replace(afterLogin)
-          }
-        })
-        .catch(error => {
-          logger.info("SigninRedirectCallback unsuccessful", error)
-          if (onFailure) {
-            logger.info("Using failure callback", onError)
-            onFailure(error)
-          } else {
-            logger.error("Fallback to replace location", onError)
-            _window.location.replace(onError)
-          }
-        })
+  useCallbackLogin({
+    minTimeBeforeRedirect,
+    onSuccess: result => {
+      logger.info("SigninRedirectCallback successful", result)
+      if (onSuccess) {
+        logger.info("Using success callback", afterLogin)
+        onSuccess(result)
+      } else {
+        logger.info("Replacing location with", afterLogin)
+        _window.location.replace(afterLogin)
+      }
+    },
+    onFailure: error => {
+      logger.info("SigninRedirectCallback unsuccessful", error)
+      if (onFailure) {
+        logger.info("Using failure callback", onError)
+        onFailure(error)
+      } else {
+        logger.error("Fallback to replace location", onError)
+        _window.location.replace(onError)
+      }
     }
-  }, [userManager, ready])
+  })
 
   if (element) {
     return element
--- src/open-id/index.js
+++ src/open-id/index.js
@@ -7,3 +7,5 @@ export { OpenId, useOpenId, OpenIdContext } from "./components/OpenId"
 export { useOpenIdUser } from "./hooks/useOpenIdUser"
 export { useOtpLogin } from "./hooks/useOtpLogin"
 export { useUrlLogin } from "./hooks/useUrlLogin"
+
+export { useCallbackLogin } from "./hooks/useCallbackLogin"
