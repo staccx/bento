720e6df44cea6f96dd209d9cae128067db343e1f
Espen Th Sæverud <espensaeverud@gmail.com>
2020-04-20 13:30:50 +0200

    fix(hooks): let to const

--- src/hooks/useInterval.js
+++ src/hooks/useInterval.js
@@ -15,7 +15,7 @@ const useInterval = (callback, delay) => {
     }
 
     if (delay !== null) {
-      let id = setInterval(tick, delay)
+      const id = setInterval(tick, delay)
       return () => clearInterval(id)
     }
   }, [delay])
