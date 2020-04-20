ef9b36284c5c00aeb384eb856b570d0bd1f057b1
Espen Th Sæverud <espensaeverud@gmail.com>
2020-04-20 14:17:22 +0200

    test(components): add new tests to modal

--- src/components/Modal/Modal.spec.js
+++ src/components/Modal/Modal.spec.js
@@ -61,22 +61,33 @@ describe("Modal", () => {
         .toJSON()
       expect(tree).toMatchSnapshot()
     })
-    it("onClose", () => {
-      const tree = renderer
-        .create(
-          <ThemeProvider theme={baseTheme}>
-            <Modal
-              className="className"
-              isOpen
-              onClose={e => console.log(e.target.value)}
-            >
-              <p> Lorem ipsum dolor sit amet</p>
-              <p>Ut rutrum ex ante, ut posuere arcu pellentesque vel.</p>
-            </Modal>
-          </ThemeProvider>
-        )
-        .toJSON()
-      expect(tree).toMatchSnapshot()
+  })
+  describe("Functionality", () => {
+    it("Should open", () => {
+      const testRenderer = renderer.create(
+        <Modal isOpen>
+          <div className="testValue">Tester</div>
+        </Modal>
+      )
+
+      expect(testRenderer.root.props.isOpen).toBe(true)
+      expect(
+        testRenderer.root.findByProps({ className: "testValue" }).children
+      ).toStrictEqual(["Tester"])
+
+      console.log(testRenderer.root.children.length)
+      expect(testRenderer.root.children.length).toBe(2)
+    })
+
+    it("Should render null is not open", () => {
+      const testRenderer = renderer.create(
+        <Modal isOpen={false}>
+          <div className="testValue">Tester</div>
+        </Modal>
+      )
+
+      expect(testRenderer.root.props.isOpen).toBe(false)
+      expect(testRenderer.root.children).toStrictEqual([])
     })
   })
 })
