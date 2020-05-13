bce1a6c7ac885f87fdd7cd1f7e061865bab4aebf
Thomase <thomase@stacc.com>
2020-05-13 14:08:16 +0200

    test(hooks): render test for useBrRefSearch

--- src/hooks/useBrRegSearch/useBrRegSearch.spec.js
+++ src/hooks/useBrRegSearch/useBrRegSearch.spec.js
@@ -1,7 +1,10 @@
+import React from "react"
 import { renderHook } from "@testing-library/react-hooks"
+import { render, screen } from "@testing-library/react"
+import "@testing-library/jest-dom"
 import useBrRegSearch from "./useBrRegSearch"
 
-describe.skip("useBrregSearch", () => {
+describe("useBrregSearch", () => {
   it("search", async () => {
     const { result, waitForNextUpdate } = renderHook(() =>
       useBrRegSearch("Stacc")
@@ -18,5 +21,22 @@ describe.skip("useBrregSearch", () => {
     expect(result.current.results).not.toBeUndefined()
     expect(result.current.results.length).toBe(1)
     expect(result.current.results[0].navn).toBe("STACC X AS")
+  })
+  describe("Rendering", () => {
+    it("should render Results", async () => {
+      const { result, waitForNextUpdate } = renderHook(() =>
+        useBrRegSearch("Stacc")
+      )
+      await waitForNextUpdate()
+      render(
+        <div>
+          {result.current.results.map(result => {
+            return <p key={result.navn}>{result.navn}</p>
+          })}
+        </div>
+      )
+      expect(screen.getByText("STACC AS")).toBeInTheDocument()
+      expect(screen.getByText("STACC X AS")).toBeInTheDocument()
+    })
   })
 })
