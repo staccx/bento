8db514fe24f9fa9ae94972fcf5d7a9d7e77babec
Lene <lene.whiteley@evry.com>
2020-05-14 10:25:15 +0200

    test(components): rendering test for Form

--- src/components/Forms/Form/Form.spec.js
+++ src/components/Forms/Form/Form.spec.js
@@ -2,6 +2,7 @@ import React from "react"
 import renderer from "react-test-renderer"
 import { ThemeProvider } from "styled-components"
 import baseTheme from "../../../theming/themes/baseTheme"
+import { render, screen } from "@testing-library/react"
 import Form from "./Form"
 import FormField from "./FormField"
 import Input from "../Input/Input"
@@ -185,5 +186,34 @@ describe("Form", () => {
         .toJSON()
       expect(tree).toMatchSnapshot()
     })
+  })
+})
+
+describe("Rendering", () => {
+  it("Should render", () => {
+    render(
+      <ThemeProvider theme={baseTheme}>
+        <Form initialValues={{ epost: "" }} buttonText="Søk nå">
+          <FormField
+            name="epost"
+            type="string"
+            min={0}
+            max={100}
+            integer
+            required
+            renderError={({ error, touched }) => (
+              <span>
+                {error} {touched}
+              </span>
+            )}
+          >
+            {({ name, field, form }) => (
+              <Input {...field} placeholder="ola@nordmann.no" id={name} />
+            )}
+          </FormField>
+        </Form>
+      </ThemeProvider>
+    )
+    expect(screen.getByPlaceholderText("ola@nordmann.no")).toBeInTheDocument()
   })
 })
