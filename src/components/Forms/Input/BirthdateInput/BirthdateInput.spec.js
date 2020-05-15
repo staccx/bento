52e353000e357d60fd35bd3fd98633f496c4b776
Lene <lene.whiteley@evry.com>
2020-05-15 13:02:56 +0200

    test(components): rendering test for BirthdayInput

--- src/components/Forms/Input/BirthdateInput/BirthdateInput.spec.js
+++ src/components/Forms/Input/BirthdateInput/BirthdateInput.spec.js
@@ -4,6 +4,7 @@ import renderer from "react-test-renderer"
 import { ThemeProvider } from "styled-components"
 import baseTheme from "../../../../theming/themes/baseTheme"
 import BirthdateInput from "./BirthdateInput"
+import userEvent from "@testing-library/user-event"
 
 const setup = () => {
   const utils = render(
@@ -43,40 +44,29 @@ describe("BirthdateInput", () => {
         .toJSON()
       expect(tree).toMatchSnapshot()
     })
-    it("simulate input", () => {
-      const { day, month, year } = setup()
-      fireEvent.change(day, { target: { value: 12 } })
-      fireEvent.change(month, { target: { value: 1 } })
-      fireEvent.change(year, { target: { value: 1995 } })
-      expect(day.value).toBe("12")
-      expect(month.value).toBe("1")
-      expect(year.value).toBe("1995")
-    })
-    it("simulate input2", () => {
-      const { day, month, year } = setup()
-      fireEvent.change(day, { target: { value: 12 } })
-      fireEvent.change(month, { target: { value: 12 } })
-      fireEvent.change(year, { target: { value: 1995 } })
-      expect(day.value).toBe("12")
-      expect(month.value).toBe("12")
-      expect(year.value).toBe("1995")
-    })
   })
 })
 
 describe("Rendering", () => {
-  it("Should render", () => {
-    render(
-      <ThemeProvider theme={baseTheme}>
-        <BirthdateInput
-          data-testid="testID"
-          label="BirthdateInput"
-          ids={["Day1", "Month1", "Year1"]}
-          labels={["Day", "Month", "Year"]}
-          onChange={console.log}
-        />
-      </ThemeProvider>
-    )
+  it("Should render and be changed", () => {
+    const { day, month, year } = setup()
+    userEvent.type(day, "12")
+    userEvent.type(month, "1")
+    userEvent.type(year, "1337")
+
     expect(screen.getByPlaceholderText("dd")).toBeInTheDocument()
+    expect(day.value).toBe("12")
+    expect(month.value).toBe("1")
+    expect(year.value).toBe("1337")
+  })
+  it("Should follow date, month and year input lengths", () => {
+    const { day, month, year } = setup()
+    userEvent.type(day, "122095")
+    userEvent.type(month, "019045")
+    userEvent.type(year, "133337")
+    expect(screen.getByPlaceholderText("dd")).toBeInTheDocument()
+    expect(day.value).toBe("12")
+    expect(month.value).toBe("01")
+    expect(year.value).toBe("1333")
   })
 })
