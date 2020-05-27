9ce4f43b8bb2b8f12796f65469b7059e5b83d282
Thomase <thomase@stacc.com>
2020-05-27 15:18:04 +0200

    refactor(components): refactor codeRenderer to functional component

--- src/components/Code/CodeRenderer.js
+++ src/components/Code/CodeRenderer.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react"
+import React from "react"
 import PropTypes from "prop-types"
 import Prism from "prismjs"
 import CodeWrapper from "./CodeWrapper"
@@ -6,50 +6,47 @@ import "prismjs/components/prism-markup"
 import "prismjs/components/prism-java"
 import "prismjs/components/prism-csharp"
 
-class CodeRenderer extends Component {
-  render() {
-    const { code, language } = this.props
-    let grammar = null
-    let className = ""
+const CodeRenderer = ({ language, code }) => {
+  let grammar = null
+  let className = ""
 
-    switch (language) {
-      case "markup": {
-        className = "language-markup"
-        grammar = Prism.languages.html
-        break
-      }
-
-      case "javascript": {
-        className = "language-js"
-        grammar = Prism.languages.javascript
-        break
-      }
-
-      case "java": {
-        className = "language-java"
-        grammar = Prism.languages.java
-        break
-      }
-
-      case "csharp": {
-        className = "language-csharp"
-        grammar = Prism.languages.csharp
-        break
-      }
-
-      default:
-        return null
+  switch (language) {
+    case "markup": {
+      className = "language-markup"
+      grammar = Prism.languages.html
+      break
     }
 
-    const html = {
-      __html: Prism.highlight(code, grammar, language)
+    case "javascript": {
+      className = "language-js"
+      grammar = Prism.languages.javascript
+      break
     }
-    return (
-      <CodeWrapper className={className}>
-        <code className={className} dangerouslySetInnerHTML={html} />
-      </CodeWrapper>
-    )
+
+    case "java": {
+      className = "language-java"
+      grammar = Prism.languages.java
+      break
+    }
+
+    case "csharp": {
+      className = "language-csharp"
+      grammar = Prism.languages.csharp
+      break
+    }
+
+    default:
+      return null
   }
+
+  const html = {
+    __html: Prism.highlight(code, grammar, language)
+  }
+  return (
+    <CodeWrapper className={className}>
+      <code className={className} dangerouslySetInnerHTML={html} />
+    </CodeWrapper>
+  )
 }
 
 const propTypes = {
