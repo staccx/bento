ac593a162c668c2b6cdfcbb734b7384be8d18856
Guro Prestegard <gurop@stacc.com>
2020-08-18 16:55:23 +0200

    docs(storybook): stories for Lists/LinkList

---
+++ src/components/Lists/LinkList/LinkList.stories.js
@@ -0,0 +1,34 @@
+import React from "react"
+import LinkListItem from "./LinkListItem"
+import List from "../List"
+
+export default {
+  title: "New/Lists/LinkList",
+  component: LinkListItem
+}
+
+export const LinkListStandard = args => (
+  <List>
+    <LinkListItem {...args}>One ListElement</LinkListItem>
+    <LinkListItem {...args}>Another List Element</LinkListItem>
+  </List>
+)
+LinkListStandard.args = { to: "/" }
+
+export const LinkListLongInlineList = args => (
+  <List>
+    <LinkListItem {...args}>One ListElement</LinkListItem>
+    <LinkListItem {...args}>Another List Element</LinkListItem>
+    <LinkListItem {...args}>One ListElement</LinkListItem>
+    <LinkListItem {...args}>Another List Element</LinkListItem>
+    <LinkListItem {...args}>One ListElement</LinkListItem>
+    <LinkListItem {...args}>Another List Element</LinkListItem>
+    <LinkListItem {...args}>One ListElement</LinkListItem>
+    <LinkListItem {...args}>Another List Element</LinkListItem>
+    <LinkListItem {...args}>One ListElement</LinkListItem>
+    <LinkListItem {...args}>Another List Element</LinkListItem>
+    <LinkListItem {...args}>One ListElement</LinkListItem>
+    <LinkListItem {...args}>Another List Element</LinkListItem>
+  </List>
+)
+LinkListLongInlineList.args = { to: "/" }
