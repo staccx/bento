d0bba0a5c303c20d510230e0e4eb6866170e5f23
Guro Prestegard <gurop@stacc.com>
2020-08-18 15:23:27 +0200

    docs(storybook): stories for Layout:ItemGroup

---
+++ src/components/Layout/ItemGroup/ItemGroup.stories.js
@@ -0,0 +1,19 @@
+import React from "react"
+import ItemGroup from "./ItemGroup"
+import Button from "../../Button/Button"
+
+export default {
+  title: "New/Layout/ItemGroup",
+  component: ItemGroup
+}
+
+export const ItemGroupStandard = args => (
+  <ItemGroup {...args}>
+    <Button>Loan</Button>
+    <Button>Credit</Button>
+    <Button>Saving</Button>
+    <Button>About this bank</Button>
+    <Button>Contact</Button>
+  </ItemGroup>
+)
+ItemGroupStandard.args = { spacing: "medium" }
