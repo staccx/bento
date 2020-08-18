889fc24ef90e67ffa35fe74c5f5907c222e82983
Guro Prestegard <gurop@stacc.com>
2020-08-18 15:14:44 +0200

    docs(storybook): stories for Layout:Flag

---
+++ src/components/Layout/Flag/Flag.stories.js
@@ -0,0 +1,43 @@
+import React from "react"
+import Flag from "./Flag"
+
+export default {
+  title: "New/Layout/Flag",
+  component: Flag
+}
+
+export const FlagStandard = args => (
+  <Flag
+    {...args}
+    img={
+      <img src="http://via.placeholder.com/150x150" widt="100" height="100" />
+    }
+  >
+    Text for flag-object, the standard way.
+  </Flag>
+)
+FlagStandard.args = { responsive: 800 }
+
+export const FlagReversed = args => (
+  <Flag
+    {...args}
+    img={
+      <img src="http://via.placeholder.com/150x150" widt="100" height="100" />
+    }
+  >
+    Text for flag-object when the image is reversed
+  </Flag>
+)
+FlagReversed.args = { responsive: 800, reverse: true }
+
+export const FlagTop = args => (
+  <Flag
+    {...args}
+    img={
+      <img src="http://via.placeholder.com/150x150" widt="100" height="100" />
+    }
+  >
+    Text for flag-object positioned to the top
+  </Flag>
+)
+FlagTop.args = { responsive: 800, top: true }
