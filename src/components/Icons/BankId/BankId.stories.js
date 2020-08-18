5e4072e73f7e50838721c0e56d79722f887c03cc
Guro Prestegard <gurop@stacc.com>
2020-08-18 13:47:00 +0200

    docs(storybook): stories for Icons:BankId

---
+++ src/components/Icons/BankId/BankId.stories.js
@@ -0,0 +1,9 @@
+import React from "react"
+import BankId from "./BankId"
+
+export default {
+  title: "New/Icons/BankId",
+  component: BankId
+}
+
+export const BankIdStandard = args => <BankId {...args} />
