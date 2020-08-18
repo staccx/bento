16c45f10889cdf6ad40e984adffd6fcb0d24d1d0
Guro Prestegard <gurop@stacc.com>
2020-08-18 16:28:45 +0200

    docs(storybook): stories for Lists/ExpandListr - additions

--- src/components/Lists/ExpandList/ExpandList.stories.js
+++ src/components/Lists/ExpandList/ExpandList.stories.js
@@ -19,3 +19,60 @@ export const ExpandListStandard = args => (
     </ExpandListItem>
   </List>
 )
+
+export const ExpandListLongList = args => (
+  <List>
+    <ExpandListItem {...args}>
+      This is hidden until it has been expanded
+    </ExpandListItem>
+    <ExpandListItem {...args}>This is hidden</ExpandListItem>
+    <ExpandListItem {...args}>This is hidden</ExpandListItem>
+    <ExpandListItem {...args}>This is hidden</ExpandListItem>
+    <ExpandListItem {...args}>This is hidden</ExpandListItem>
+    <ExpandListItem {...args}>This is hidden</ExpandListItem>
+    <ExpandListItem {...args}>This is hidden</ExpandListItem>
+    <ExpandListItem {...args}>This is hidden</ExpandListItem>
+    <ExpandListItem {...args}>This is hidden</ExpandListItem>
+    <ExpandListItem {...args}>This is hidden</ExpandListItem>
+  </List>
+)
+ExpandListLongList.args = {
+  variant: "variant",
+  title: "This is an ExpandListItem"
+}
+
+export const ExpandListLongTitle = args => (
+  <List>
+    <ExpandListItem {...args}>
+      This is hidden untill it has been expanded
+    </ExpandListItem>
+  </List>
+)
+ExpandListLongTitle.args = {
+  variant: "variant",
+  title:
+    "This expand title is very long, does that matter? it might do, but i dont know, lets find out. i am now just typing random words to make this title longer, so far it seemes fine"
+}
+
+export const ExpandListDefaultExpanded = args => (
+  <List>
+    <ExpandListItem {...args}>
+      Since i am default open, you can see me on render, click to hide me
+    </ExpandListItem>
+  </List>
+)
+ExpandListDefaultExpanded.args = {
+  expanded: true,
+  title: "A Title"
+}
+export const ExpandListFlush = args => (
+  <List>
+    <ExpandListItem {...args}>
+      This container is flush, so i am closer to the header
+    </ExpandListItem>
+  </List>
+)
+ExpandListFlush.args = {
+  flush: true,
+  title: "A Title"
+}
