afc2a9ebb50b9ee128f0f05730cf31c15bf4af57
Guro Prestegard <gurop@stacc.com>
2020-08-18 14:05:06 +0200

    docs(storybook): stories for Icons:BankIdMobile

---
+++ src/components/Icons/BankIdMobile/BankIdMobile.stories.js
@@ -0,0 +1,9 @@
+import React from "react"
+import BankIdMobile from "./BankIdMobile"
+
+export default {
+  title: "New/Icons/BankIdMobile",
+  component: BankIdMobile
+}
+
+export const BankIdMobileStandard = args => <BankIdMobile {...args} />
