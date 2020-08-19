a202af88d6af64079d7c61e4d7cff8b5305beeb9
Guro Prestegard <gurop@stacc.com>
2020-08-19 13:32:39 +0200

    docs(storybook): block comment components NewBadge fix

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -2,7 +2,7 @@ import PropTypes from "prop-types"
 import React from "react"
 
 /**
- * Forklaring på hva det er og hvordan den kan brukes
+ * Formatting for an img tag
  */
 const Logo = ({ brand, height, width, className }) => (
   <img
