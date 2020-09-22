3bcbc416728a9d07ad91954fe81752f60d73f46a
Espen Th Sæverud <espensaeverud@gmail.com>
2020-09-22 17:58:23 +0200

    chore(bento): add module resolver for better paths

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,12 @@
+const path = require("path")
+
+const disableEsLint = (e) => {
+  return e.module.rules.filter(e =>
+    e.use && e.use.some(e => e.options && void 0 !== e.options.useEslintrc)).forEach(s => {
+    e.module.rules = e.module.rules.filter(e => e !== s)
+  }), e
+}
+
 module.exports = {
   stories: ['../src/**/*.stories.@(mdx|js)'],
   addons: [
@@ -7,5 +16,22 @@ module.exports = {
     "@storybook/addon-links",
     '@storybook/addon-controls',
     "@storybook/core-events"
-  ]
+  ],
+  webpackFinal: async (config, { configType }) => {
+    config = disableEsLint(config)
+    config.resolve.alias = {
+        "animations": path.resolve(__dirname, "..", "src", "animations"),
+        "color": path.resolve(__dirname, "..", "src", "color"),
+        "components": path.resolve(__dirname, "..", "src", "components"),
+        "formatting": path.resolve(__dirname, "..", "src", "formatting"),
+        "hooks": path.resolve(__dirname, "..", "src", "hooks"),
+        "i18n": path.resolve(__dirname, "..", "src", "i18n"),
+        "math": path.resolve(__dirname, "..", "src", "math"),
+        "open-id": path.resolve(__dirname, "..", "src", "open-id"),
+        "sanity": path.resolve(__dirname, "..", "src", "sanity"),
+        "theming": path.resolve(__dirname, "..", "src", "theming"),
+        "utils": path.resolve(__dirname, "..", "src", "utils")
+    }
+    return config;
+  },
 }
