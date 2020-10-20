f61e2144a686e5c4cb0d04a8bb94de8cc345ae9b
Espen Th Sæverud <espensaeverud@gmail.com>
2020-10-20 15:33:26 +0200

    fix(i18n): value is null

--- src/i18n/backend.utils.js
+++ src/i18n/backend.utils.js
@@ -1,5 +1,5 @@
 export const resourceReducer = language => (acc, current) => {
-  acc[current.key] = current.value.map(
+  acc[current.key] = current?.value?.map(
     item =>
       item[language] ??
       item[language.toUpperCase()] ??
