f35b6e775274ac8a4a8793fd9a0ff05a571c1139
Espen Th Sæverud <espensaeverud@gmail.com>
2020-10-31 21:30:04 +0100

    test: fix tests

--- src/components/Forms/Slider/SliderKeyboardInput/SliderKeyboardInput.spec.js
+++ src/components/Forms/Slider/SliderKeyboardInput/SliderKeyboardInput.spec.js
@@ -4,7 +4,7 @@ import renderer from "react-test-renderer"
 import { ThemeProvider } from "styled-components"
 import baseTheme from "../../../../theming/themes/baseTheme"
 import SliderKeyboardInput from "./SliderKeyboardInput"
-import { replaceWhiteSpaceForMatcher } from "../../../../utils/_testReplaceWhitespace";
+import { replaceWhiteSpaceForMatcher } from "../../../../utils/_testReplaceWhitespace"
 
 const setup = (props = {}) => {
   const utils = render(
@@ -154,7 +154,9 @@ describe("SliderKeyboardInput", () => {
     })
     it("simulated keyboard with default value", () => {
       const { sliderKeyboardLabel } = setup({ defaultValue: 1337 })
-      expect(sliderKeyboardLabel.value).toBe(replaceWhiteSpaceForMatcher("1,337"))
+      expect(sliderKeyboardLabel.value).toBe(
+        replaceWhiteSpaceForMatcher("1 337")
+      )
     })
   })
 })
