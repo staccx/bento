8d9c373a1b15a3b4888ffded76f5c8d2972b2670
Espen Th Sæverud <espensaeverud@gmail.com>
2021-03-11 15:22:33 +0100

    docs(i18n): fix transform story

--- src/i18n/Transform/Transform.stories.js
+++ src/i18n/Transform/Transform.stories.js
@@ -1,18 +1,29 @@
 import React from "react"
 import Transform from "./Transform"
+import { languages } from "../index"
 
 export default {
   title: "Components/i18n/Transform",
-  component: Transform
+  component: Transform,
+  parameters: {
+    docs: {
+      description: {
+        component:
+          "Note that this renders before the language has been properly changed. This is a bug"
+      }
+    }
+  }
 }
-
+/*
+This is the description
+ */
 export const Key = args => <Transform {...args} />
 Key.args = {
   data: {
-    no: "Tester",
-    dk: "Tester på dansk",
-    se: "tjenare",
-    fi: "Ei saa peitaa",
-    us: "America first"
+    [languages.Norwegian]: "Tester",
+    [languages.Danish]: "Tester på dansk",
+    [languages.Swedish]: "tjenare",
+    [languages.Finnish]: "Ei saa peitaa",
+    [languages.English]: "America first"
   }
 }
