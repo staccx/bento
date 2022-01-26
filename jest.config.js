c844ec783b6ece378697b737697224999f7a6455
Espen Sæverud <espensaeverud@gmail.com>
2022-01-26 09:02:12 +0100

    test: result now created prepush

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,7 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
-  setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"]
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
+  coverageReporters: [["lcov", { projectRoot: "." }], "text-summary", "json"],
+  roots: ["<rootDir>/src/"]
 }
