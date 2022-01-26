cd8d58abd5613ea27713436fec6407132316b443
Espen Sæverud <espensaeverud@gmail.com>
2022-01-26 09:01:55 +0100

    test: fix mock window

--- src/hooks/useCopyToClipboard/useCopyToClipboard.spec.tsx
+++ src/hooks/useCopyToClipboard/useCopyToClipboard.spec.tsx
@@ -5,10 +5,18 @@ import "@testing-library/jest-dom"
 import { useCopyToClipboard } from "./useCopyToClipboard"
 // TODO: Ser ut som copy-to-clipboard er vanskelig å jest teste, se: https://github.com/nkbt/react-copy-to-clipboard/issues/106
 
+let prompt
 describe("useCopyToClipboard", () => {
   // jest.mock("copy-to-clipboard", () => {
   //   return jest.fn()
   // })
+  beforeAll(() => {
+    prompt = window.prompt
+    window.prompt = jest.fn(() => null)
+  })
+  afterAll(() => {
+    window.prompt = prompt
+  })
 
   it("should copyContent", async () => {
     const testing = "test"
